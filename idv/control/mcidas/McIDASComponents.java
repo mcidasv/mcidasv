d7c0b05484b729d6866ffb7be5df546176db7445
Gail Dengel <gad@ssec.wisc.edu>
2006-03-17 16:26:51 +0000

    Move here from .../data/imagery/mcidas/

---
+++ idv/control/mcidas/McIDASComponents.java
@@ -0,0 +1,18 @@
+package ucar.unidata.idv.control.mcidas;
+
+
+
+/**
+ * Holds a set of definitions concerning McIDAS data.
+ */
+public interface McIDASComponents {
+
+    /** Image frame component */
+    public static final String IMAGE = "McIDASComponents.image";
+
+    /** Graphics frame component */
+    public static final String GRAPHICS = "McIDASComponents.graphics";
+
+    /** Color Table frame component */
+    public static final String COLORTABLE = "McIDASComponents.colortable";
+}
