a6406dc222448f6a787f043f82f3d9878231806c
Gail Dengel <gad@ssec.wisc.edu>
2006-12-12 20:48:22 +0000

    First blocking.

---
+++ idv/chooser/mcidas/McCommandChooser.java
@@ -0,0 +1,122 @@
+package ucar.unidata.idv.chooser.mcidas;
+
+import ucar.unidata.idv.*;
+
+import ucar.unidata.data.imagery.mcidas.McIDASDataset;
+
+import ucar.unidata.idv.chooser.IdvChooser;
+import ucar.unidata.idv.chooser.IdvChooserManager;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.util.List;
+import java.util.Hashtable;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+import ucar.unidata.util.PreferenceList;
+
+import ucar.unidata.xml.XmlResourceCollection;
+
+import org.w3c.dom.Element;
+
+import ucar.unidata.ui.imagery.mcidas.McCommandLineChooser;
+//import ucar.unidata.data.imagery.mcidas.McIDASDataset;
+
+
+public class McCommandChooser extends IdvChooser {
+
+    private McCommandLineChooser mcCommandLineChooser;
+
+    /**
+     * Create the chooser with the given manager and xml
+     *
+     * @param mgr The manager
+     * @param root The xml
+     *
+     */
+    public McCommandChooser(IdvChooserManager mgr, Element root) {
+        super(mgr, root);
+    }
+
+
+    /**
+     * Handle the update event. Just pass it through to the mcCommandLineChooser
+     */
+    public void doUpdate() {
+        mcCommandLineChooser.doUpdate();
+    }
+
+    /**
+     * Make the GUI
+     *
+     * @return The GUI
+     */
+    protected JComponent doMakeContents() {
+        mcCommandLineChooser = doMakeMcCommandLineChooser();
+        initChooserPanel(mcCommandLineChooser);
+        mcCommandLineChooser.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent e) {
+                if (e.getPropertyName().equals(
+                        McCommandLineChooser.NEW_SELECTION)) {
+                    loadMcIDASDataSet(e);
+                }
+
+            }
+        });
+        return mcCommandLineChooser.getContents();
+    }
+
+    /**
+     * Make the chooser. This is a hook so a derived class
+     * can make its own chooser
+     *
+     * @return The {@link ucar.unidata.ui.imagery.McCommandLineChooser} to pass
+     * to the mcCommandLineChooser.
+     */
+    protected McCommandLineChooser doMakeMcCommandLineChooser() {
+        return new McCommandLineChooser(this, getPreferenceList(McIDASIdvChooser.PREF_FRAMEDESCLIST)) {
+            public void doCancel() {
+                closeChooser();
+            }
+        };
+    }
+
+
+    /**
+     * Get the xml resource collection that defines the frame default xml
+     *
+     * @return Frame defaults resources
+     */
+    protected XmlResourceCollection getFrameDefaults() {
+        return getIdv().getResourceManager().getXmlResources(
+            McIDASIdvResourceManager.RSC_FRAMEDEFAULTS);
+    }
+
+    /**
+     * User said go, we go. Simply get the list of frames
+     * from the mcCommandLineChooser and create the MCIDAS
+     * DataSource
+     *
+     * @param e The event
+     */
+    protected void loadMcIDASDataSet(PropertyChangeEvent e) {
+        McCommandLineChooser mxc = (McCommandLineChooser) e.getSource();
+        McIDASDataset mds = new McIDASDataset( mxc.getDatasetName(),
+                                            (List) e.getNewValue());
+        // make properties Hashtable 
+        Hashtable ht = new Hashtable();
+        ht.put(mxc.FRAME_NUMBERS_KEY, mds.frameNumbers);
+        if (mds.frameNumbers.size() > 1) {
+           ht.put(mxc.DATA_NAME_KEY,"Frame Sequence");
+        } else {
+           ht.put(mxc.DATA_NAME_KEY,"Frame");
+        }
+        makeDataSource("", "MCIDAS", ht);
+    }
+}
---
+++ ui/imagery/mcidas/McCommandLineChooser.java
@@ -0,0 +1,421 @@
+package ucar.unidata.ui.imagery.mcidas;
+
+import ucar.unidata.data.imagery.mcidas.FrameDirectory;
+import ucar.unidata.data.imagery.mcidas.McIDASXFrameDescriptor;
+
+import edu.wisc.ssec.mcidas.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.border.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Vector;
+
+import ucar.unidata.idv.chooser.IdvChooser;
+
+import ucar.unidata.ui.ChooserPanel;
+
+import ucar.unidata.util.Defaults;
+import ucar.unidata.util.GuiUtils;
+import ucar.unidata.util.LogUtil;
+import ucar.unidata.util.Misc;
+import ucar.unidata.util.StringUtil;
+
+import ucar.unidata.util.PreferenceList;
+
+/**
+ * Widget to select frames from McIDAS-X shared memory
+ * Displays a list of the descriptors (names) of the frame datasets
+ */
+public class McCommandLineChooser extends FrameChooser {
+
+
+    /** A widget for the command line text */
+    protected JTextField keyLine;
+    protected JTextField encodingLine;
+    protected JTextField commandLine;
+    private JButton sendBtn;
+    private JTextArea textArea;
+ 
+    /**
+     *  The list of currently loaded frame Descriptor-s
+     */
+    private Vector frameDescriptors;
+
+    /**
+     *  Keep track of which image load we are on.
+     */
+    private int currentFrame = 0;
+
+    /**
+     * Construct an Adde image selection widget
+     *
+     * @param imageDefaults The xml resources for the image defaults
+     * @param descList Holds the preferences for the image descriptors
+     * @param serverList Holds the preferences for the adde servers
+     */
+    public McCommandLineChooser(IdvChooser idvChooser,
+                               PreferenceList descList) {
+    }
+
+
+
+    /**
+     * Update labels, enable widgets, etc.
+     */
+    protected void updateStatus() {
+        super.updateStatus();
+        enableWidgets();
+        return;
+    }
+
+    /**
+     * Do we have frames selected. Either we are doing a frame 
+     * loop and there are some selected in the list. Or we
+     * are refreshing the current frame
+     *
+     * @return Do we have frames?
+     */
+    protected boolean timesOk() {
+        if (getDoFrameLoop() && !haveFrameSelected()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Convenience method for lazy people who don't want to call
+     * {@link ucar.unidata.util.LogUtil#logException(String, Throwable)}.
+     *
+     * @param msg    log message
+     * @param exc    Exception to log
+     */
+    public void logException(String msg, Exception exc) {
+        LogUtil.logException(msg, exc);
+    }
+
+    /**
+     * This allows derived classes to provide their own name for labeling, etc.
+     *
+     * @return  the dataset name
+     */
+    public String getDataName() {
+        return "McIDAS-X Frame Data";
+    }
+
+    /**
+     * Get the name of the dataset.
+     *
+     * @return descriptive name of the dataset.
+     */
+    public String getDatasetName() {
+        String temp = null;
+        return temp;
+    }
+
+    /**
+     * Check if we are ready to read times
+     *
+     * @return  true if times can be read
+     */
+    protected boolean canReadFrames() {
+        return true;
+    }
+
+    /**
+     * Check if a descriptor (image type) has been chosen
+     *
+     * @return  true if an image type has been chosen
+     */
+    protected boolean haveDescriptorSelected() {
+        return true;
+    }
+
+
+    /**
+     * Handle when the user presses the update button
+     *
+     * @throws Exception On badness
+     */
+    public void handleUpdate()  {
+    }
+
+
+    /**
+     * Make the UI for this selector.
+     *
+     * @return The gui
+     */
+    protected JComponent doMakeContents() {
+        List allComps = new ArrayList();
+        getComponents(allComps);
+        JPanel imagePanel = GuiUtils.doLayout(allComps, 2, GuiUtils.WT_NN,
+                                              GuiUtils.WT_N);
+        List textComps = new ArrayList();
+        textArea = new JTextArea("", 20, 60);
+        textComps.add(textArea);
+        textComps.add(new JLabel(" "));
+        textComps.add(new JLabel(" "));
+        JPanel textPanel = GuiUtils.doLayout(textComps, 2, GuiUtils.WT_NN,
+                                              GuiUtils.WT_N);
+        return GuiUtils.topCenterBottom(imagePanel, textPanel, getDefaultButtons(this));
+    }
+
+    /**
+     * Make the components (label/widget) and return them
+     *
+     *
+     * @param comps The list to add components to
+     */
+    protected void getComponents(List comps) {
+        JLabel keyLabel = GuiUtils.rLabel("Key:  ");
+        comps.add(keyLabel);
+        keyLine = new JTextField("", 10);
+        keyLine.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                 String keyString = (keyLine.getText()).trim();
+                 sendKeyLine(keyString);
+            }
+        });
+        keyLine.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+            }
+        });
+        comps.add(keyLine);
+
+        JLabel encodingLabel = GuiUtils.rLabel("Encoding:  ");
+        comps.add(encodingLabel);
+        encodingLine = new JTextField("", 20);
+        encodingLine.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                 String encodingString = (encodingLine.getText()).trim();
+                 sendEncodingLine(encodingString);
+            }
+        });
+        encodingLine.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+            }
+        });
+        comps.add(encodingLine);
+        comps.add(new JLabel(" "));
+        comps.add(new JLabel(" "));
+
+        JLabel commandLineLabel = GuiUtils.rLabel("Command Line:  ");
+        comps.add(commandLineLabel);
+        commandLine = new JTextField("", 40);
+        commandLine.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                 String saveCommand = (commandLine.getText()).trim();
+                 sendCommandLine(saveCommand);
+            }
+        });
+        commandLine.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+                sendBtn.setEnabled(true);
+            }
+        });
+        List sendList = new ArrayList();
+        sendList.add(commandLine);
+        sendList.add(new JLabel(" "));
+        sendBtn = getSendButton();
+        sendList.add(sendBtn);
+        JPanel commandPanel = GuiUtils.doLayout(sendList, 3, GuiUtils.WT_NNN,
+                                              GuiUtils.WT_N);
+        comps.add(GuiUtils.left(commandPanel));
+        comps.add(new JLabel(" "));
+        comps.add(new JLabel(" "));
+    }
+
+     protected JButton getSendButton() {
+         sendBtn = new JButton("Send");
+         sendBtn.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent ae) {
+                 String line = (commandLine.getText()).trim();
+                 sendCommandLine(line);
+             }
+         });
+         sendBtn.setEnabled(false);
+         return sendBtn;
+     }
+
+     private void sendKeyLine(String keyLine) {
+         textArea.setText(keyLine);
+     }
+
+     private void sendEncodingLine(String encodingLine) {
+         textArea.setText(encodingLine);
+     }
+
+     private void sendCommandLine(String commandLine) {
+         textArea.setText(commandLine);
+     }
+
+    /**
+     * Get one of the selected frames.
+     *
+     * @return One of the selected frames.
+     */
+    protected FrameDirectory getASelectedFrame() {
+        if (haveFrameSelected()) {
+            McIDASXFrameDescriptor fd =
+                (McIDASXFrameDescriptor) getTimesList().getSelectedValue();
+            if (fd != null) {
+                return fd.getDirectory();
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Enable or disable the GUI widgets based on what has been
+     * selected.
+     */
+    protected void enableWidgets() {
+        boolean descriptorState = (canReadFrames());
+
+        boolean timesOk = timesOk();
+        getTimesList().setEnabled(getDoFrameLoop() && descriptorState);
+    }
+
+    /**
+     *  Read the set of image times available for the current server/group/type
+     *  This method is a wrapper, setting the wait cursor and wrapping the
+     *  call to {@link #readFramesInner(int)}; in a try/catch block
+     */
+    protected void readFrames() {
+        clearFramesList();
+        if ( !canReadFrames()) {
+            return;
+        }
+        Misc.run(new Runnable() {
+            public void run() {
+                updateStatus();
+                showWaitCursor();
+                try {
+                    readFramesInner();
+                } catch (Exception e) {
+                }
+                showNormalCursor();
+                updateStatus();
+            }
+        });
+    }
+
+
+    /**
+     * Set the list of dates/times based on the image selection
+     *
+     * @param framestep    the framestep for the image selection
+     */
+    protected void readFramesInner() {
+        loadFrames();
+    }
+
+
+    /** locking mutex */
+    private Object MUTEX = new Object();
+
+
+
+    /**
+     * Load the frames
+     *
+     */
+    protected void loadFrames() {
+            synchronized (MUTEX) {
+/*
+                frameDescriptors = new Vector();
+                int numFrames = 0;
+                int nF = fsi.getNumberOfFrames();
+                if (nF < 1) {
+                  System.out.println("McCommandLineChooser: loadFrames  nF=" + nF);
+                  return;
+                }
+
+                for (int i=0; i<nF; i++) {
+                  if (fsi.getFrameDirectory(i+1) < 0) {
+                    System.out.println("McCommandLineChooser: loadFrames  unable to get frame directory");
+                    return;
+                  }
+                  int[] frmdir = fsi.myFrmdir;
+                  FrameDirectory fd = new FrameDirectory(frmdir);
+                  if (fd.sensorNumber != -1) {
+                    numFrames++;
+//                    String fdir=fd.toString();
+                    McIDASXFrameDescriptor fdir = new McIDASXFrameDescriptor(fd, i+1);
+                    frameDescriptors.add(fdir);
+                  }
+                }
+                if (getDoFrameLoop()) {
+                    getTimesList().setListData(frameDescriptors);
+                }
+                getTimesList().setVisibleRowCount(
+                    Math.min(numFrames, getTimesListSize()));
+                getTimesList().setSelectedIndex(0);
+                getTimesList().ensureIndexIsVisible(
+                    getTimesList().getSelectedIndex());
+                revalidate();
+*/
+            }
+    }
+
+    /**
+     * Does this selector have all of its state set to load in data
+     *
+     * @return Has the user chosen everything they need to choose to load data
+     */
+    protected boolean getGoodToGo() {
+        return false;
+    }
+
+    /**
+     * Returns a list of the images to load or null if none have been
+     * selected.
+     *
+     * @return  list  get the list of image descriptors
+     */
+    public List getFrameList() {
+        if ( !timesOk()) {
+            return null;
+        }
+        if (getDoFrameLoop()) {
+            List frames = new ArrayList();
+            Object[] selectedTimes = getTimesList().getSelectedValues();
+            for (int i = 0; i < selectedTimes.length; i++) {
+              McIDASXFrameDescriptor fd = new McIDASXFrameDescriptor(
+                                      (McIDASXFrameDescriptor) selectedTimes[i]);
+              frames.add(fd);
+            }
+            return frames;
+        }
+        return null;
+    }
+
+
+    /**
+     * Method to do the work of loading the data
+     */
+    public void doLoad() {
+        try {
+            if ( !getGoodToGo()) {
+                updateStatus();
+                return;
+            }
+            firePropertyChange(NEW_SELECTION, null, getFrameList());
+        } catch (Exception exc) {
+            logException("doLoad", exc);
+        }
+    }
+
+}
