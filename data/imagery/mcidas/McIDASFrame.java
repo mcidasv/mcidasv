0b5a2b10bfdbe92ae8e282eaa6c5bf1ca44cea3e
Gail Dengel <gad@ssec.wisc.edu>
2007-03-28 18:57:13 +0000

    Use inputStream, not memory mapped files.

--- data/imagery/mcidas/McIDASFrame.java
+++ data/imagery/mcidas/McIDASFrame.java
@@ -1,10 +1,19 @@
 package ucar.unidata.data.imagery.mcidas;
 
+import edu.wisc.ssec.mcidas.McIDASUtil;
+
+import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
-import java.nio.*;
-import java.nio.channels.*;
+import java.lang.Class;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import ucar.unidata.util.Misc;
 
@@ -14,143 +23,129 @@ import ucar.unidata.util.Misc;
  */
 public class McIDASFrame {
 
+    int testFlag = 0;
+
     /** source frame number */
-    int myNumber;
+    private int myNumber;
+    private String myRequest;
 
     /** frame descriptor */
-    McIDASXFrameDescriptor myDescriptor;
+    private McIDASFrameDescriptor myDescriptor;
+
+    private FrameSubs myFs;
 
 
     /** frame data */
-    protected byte[]         myImg;       /* image pixel values */
-    protected float[][]      myEnhTable;  /* enhancement table  */
-    protected int[]          myGraphics;  /* graphics           */
-    protected int[][]        myGraLocs;   /* graphics locations */
-    protected FrameDirectory myFrameDir;  /* frame directory    */
+    private byte[]         myImg;       /* image pixel values */
+    private float[][]      myEnhTable;  /* enhancement table  */
+    private int[]          myGraphics;  /* graphics           */
+    private int[][]        myGraLocs;   /* graphics locations */
+    private FrameDirectory myFrameDir;  /* frame directory    */
 
     /** frame dimensions */
-    protected int lines;
-    protected int elems;
+    private int myLines;
+    private int myElems;
 
     /** magnification factors */
-    protected int lMag;
-    protected int eMag;
-
-/* ???
-    private FrmsubsImpl fsi = new FrmsubsImpl();
-*/
-    private static FrmsubsMM fsi;
-    static {
-      try {
-        fsi = new FrmsubsMM();
-        fsi.getMemoryMappedUC();
-      } catch (Exception e) { }
-    }
+    private int lMag;
+    private int eMag;
 
     private int status;
     private int success = 0;
+
+    private DataInputStream inputStream;
  
     /**
      *  Empty constructor for XML encoding
      */
     public McIDASFrame() {}
 
+//    public McIDASFrame(int i) {}
 
     /**
      * Construct a new McIDASFrame from the given frame number
      *
      * @param frameNumber       frame number
      */
-    public McIDASFrame(int frameNumber) {
-      if (frameNumber < 1) {
-        frameNumber = fsi.getCurrentFrame();
-        if (frameNumber < 0) {
-          System.out.println("McIDASFrame: unable to get current frame number");
-        }
-      }
-      this.myNumber = frameNumber;
+    public McIDASFrame(int frameNumber, String request) {
+        FrameSubs fs = new FrameSubs(request);
+        this.myFs = fs;
+        this.myElems = fs.getEleSize(frameNumber);
+        this.myLines = fs.getLineSize(frameNumber);
+        int[] stab = fs.stretchTable;
+        int[] ctab = fs.colorTable;
+        this.myEnhTable = getEnhTable(stab, ctab);
+        this.myRequest = request;
+        this.myNumber = frameNumber;
+        this.myImg = fs.getImg(frameNumber);
+        int[] dir = fs.getFrameDir(frameNumber);
+        this.myFrameDir = new FrameDirectory(dir);
     }
 
 
+    private float[][] getEnhTable(int[]stab, int[] ctab) {
+        float[][] enhTable = new float[3][256];
+        for (int i=1; i<18; i++) {
+            enhTable[0][i] = (float)((ctab[i]/0x10000)&0xff);
+            enhTable[1][i] = (float)((ctab[i]/0x100)&0xff);
+            enhTable[2][i] = (float)(ctab[i]&0xff);
+            //System.out.println(i + ": red=" + enhTable[0][i] + " green=" + enhTable[1][i]
+            //                     + " blue=" + enhTable[2][i]);
+        }
+        for (int i=18; i<256; i++) {
+            enhTable[0][i] = (float)((ctab[stab[i]]/0x10000)&0xff);
+            enhTable[1][i] = (float)((ctab[stab[i]]/0x100)&0xff);
+            enhTable[2][i] = (float)(ctab[stab[i]]&0xff);
+            //System.out.println(i + ": red=" + enhTable[0][i] + " green=" + enhTable[1][i]
+            //                     + " blue=" + enhTable[2][i]);
+        }
+        for (int i=0; i<256; i++) {
+            enhTable[0][i] /= 0xff;
+            enhTable[1][i] /= 0xff;
+            enhTable[2][i] /= 0xff;
+        }
+        return enhTable;
+    }
+
+        
+    /** Get current frame number */
+    public int getCurrentFrame() {
+        return this.myFs.getCurrentFrame();
+    }
+
+
+    /** Get frame number */
+    public int getFrameNumber() {
+        return this.myNumber;
+    }
+
     /** Get frame data */
-    protected int getFrameData(boolean infoData, boolean infoEnh) {
- 
-     int frm = this.myNumber;
+    public byte[] getFrameData() {
+    return this.myImg;
+  }
 
-     FrameDirectory fd = this.myFrameDir;
-     if (fd == null) {
-       getFrameDirectory(frm);
-       fd = this.myFrameDir;
-     }
 
-     int linsize = 0;
-     int elesize = 0;
-     int lineMag = fd.lineMag;
-     int eleMag = fd.eleMag;
+    /** Get frame data */
+    public int getLineSize() {
+    return this.myLines;
+  }
 
-     linsize = fsi.getLineSize(frm)/lineMag;
-     elesize = fsi.getEleSize(frm)/eleMag;
 
-     if (status<0) {
-       System.out.println("McIDASFrame: unable to get frame size myNumber=" + this.myNumber);
-       return status;
-     }
+    /** Get frame data */
+    public int getElementSize() {
+    return this.myElems;
+  }
 
-     this.lines = linsize;
-     this.elems = elesize;
-     this.lMag = lineMag;
-     this.eMag = eleMag;
-     //System.out.println("current frame: "+this.myNumber+"\n"+"linsize: "+this.lines+"\n"+"elesize: "+this.elems);
 
-     int imgSize = 1;
-     if (infoData)
-       imgSize = this.lines*this.elems;
-     byte[] img = new byte[imgSize];
+    /** Get Enhancement Table */
+    public float[][] getEnhancementTable() {
+    return this.myEnhTable;
+  }
 
-     int enhSize = 1;
-     if (infoEnh)
-       enhSize = 256;
-     int[] stab = new int[enhSize];
-     int[] ctab = new int[enhSize];
-     int[] gtab = new int[enhSize];
 
-     //System.out.println("myNumber=" + this.myNumber + " lines=" + this.lines + " elems=" + this.elems);
-     status = fsi.getFrameData(infoData, infoEnh, this.myNumber,
-                               img, stab, ctab, gtab);
-     if (status<0) {
-       System.out.println("McIDASFrame: unable to get frame data");
-       return status;
-     }
-     if (infoData)
-       this.myImg = img;
-
-     if (infoEnh) {
-       //System.out.println("McIDASFrame: getFrameData  making enhTable");
-       float[][] enhTable = new float[3][256];
-       for (int i=1; i<18; i++) {
-         enhTable[0][i] = (float)((ctab[i]/0x10000)&0xff);
-         enhTable[1][i] = (float)((ctab[i]/0x100)&0xff);
-         enhTable[2][i] = (float)(ctab[i]&0xff);
-         // System.out.println(i + ": red=" + enhTable[0][i] + " green=" + enhTable[1][i]
-         //                      + " blue=" + enhTable[2][i]);
-       }
-       for (int i=18; i<256; i++) {
-         enhTable[0][i] = (float)((ctab[stab[i]]/0x10000)&0xff);
-         enhTable[1][i] = (float)((ctab[stab[i]]/0x100)&0xff);
-         enhTable[2][i] = (float)(ctab[stab[i]]&0xff);
-         // System.out.println(i + ": red=" + enhTable[0][i] + " green=" + enhTable[1][i]
-         //                      + " blue=" + enhTable[2][i]);
-       }
-       for (int i=0; i<256; i++) {
-         enhTable[0][i] /= 0xff;
-         enhTable[1][i] /= 0xff;
-         enhTable[2][i] /= 0xff;
-       }
-       this.myEnhTable = enhTable;
-       //System.out.println("McIDASFrame: done");
-     }
-
-     return success;
+    /** Get Frame Directory */
+    public FrameDirectory getFrameDirectory() {
+    return this.myFrameDir;
   }
 
 
@@ -160,7 +155,7 @@ public class McIDASFrame {
     status = 0;
     int npts=0;
     try {
-      npts = fsi.getGraphicsSize(this.myNumber);
+//      npts = fsi.getGraphicsSize(this.myNumber);
     } catch (Exception e) {
       status = -1;
       System.out.println("McIDASFrame: unable to detemine graphics size");
@@ -173,7 +168,7 @@ public class McIDASFrame {
 
     if (npts > 0) {
       try {
-        status = fsi.getGraphics(this.myNumber, npts, gra);
+//        status = fsi.getGraphics(this.myNumber, npts, gra);
         //System.out.println("McIDASFrame getGraphicsData: getGraphics status=" + status);
       } catch (Exception e) {
           System.out.println("McIDASFrame: Graphics not read");
@@ -184,7 +179,7 @@ public class McIDASFrame {
 
       int loc,lin;
       int gpts = 0;
-      int elems = this.elems * this.eMag;
+      int elems = this.myElems * this.eMag;
 
       for (int i=0; i<npts; i++) {
         loc = gra[i]/0x100;
@@ -208,25 +203,5 @@ public class McIDASFrame {
 
     //System.out.println("McIDASFrame: getGraphicsData done");
     return success;
-  }
-
-
-  /** Get frame directory */
-  protected int getFrameDirectory(int frm) {
-    int istat = 0;
-
-    istat = fsi.getFrameDirectory(frm);
-    if (istat < 0) {
-      System.out.println("McIDASFrame getFrameDirectory: can't read directory frame=" + frm);
-      return istat;
-    }
-      
-    int[] frmdir = fsi.myFrmdir;
-
-    FrameDirectory fd = new FrameDirectory(frmdir);
-    this.myFrameDir = fd;
-
-    istat = 0;
-    return istat;
   }
 }
