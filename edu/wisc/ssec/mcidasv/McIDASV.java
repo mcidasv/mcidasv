76f475f4189b50f742b3ab7c71892b6174e414b6
Bruce Flynn <brucef@ssec.wisc.edu>
2007-06-13 19:56:38 +0000

    Initial revision for skeleton application.
    Made sure what we have compiles

---
+++ edu/wisc/ssec/mcidasv/McIDASV.java
@@ -0,0 +1,94 @@
+package edu.wisc.ssec.mcidasv;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+import ucar.unidata.idv.IntegratedDataViewer;
+import ucar.unidata.idv.ui.IdvUIManager;
+import ucar.unidata.util.LogUtil;
+import visad.VisADException;
+import edu.wisc.ssec.mcidasv.ui.UIManager;
+
+public class McIDASV extends IntegratedDataViewer {
+
+    /**
+     * Create the McIdasV with the given command line arguments.
+     * This constructor calls {@link IntegratedDataViewer#init()}
+     *
+     * @param args Command line arguments
+     * @exception VisADException  from construction of VisAd objects
+     * @exception RemoteException from construction of VisAD objects
+     *
+     */
+    public McIDASV(String[] args) throws VisADException, RemoteException {
+        super(args);
+        init();
+    }
+
+
+    /**
+     *  Add in our properties. This is the first part  of the bootstrap
+     * initializatio process.  The properties file contains a property
+     * that defines any other property files to be loaded in
+     * Then the idv looks at the property:<pre>
+     * idv.resourcefiles </pre>
+     * to find out where the rbi files are located. These  rbi files
+     * define where all of the various and sundry resources exist. In this
+     * example we use our own rbi file: example.rbi
+     *
+     * @param files List of property files
+     */
+    public void initPropertyFiles(List files) {
+        //The files list contains the default system 
+        //properties (idv.properties)
+        //We want to completely clobber it to use our own
+        //If we just wanted to add ours on top of the system we just
+        //don't clear the list. Note the path here is a java resource, i.e.,
+        //it is found from the classpath (either on dsk or in a jar file).
+        //In  general whenever we specify some path (e.g., for properties,
+        //for resources) the path can either be a java resource, a 
+        //file system path or a url
+
+
+        /*
+          files.clear();
+          files.add("/ucar/unidata/apps/example/example.properties");
+        */
+    }
+
+
+
+    /**
+     * Factory method to create the
+     * {@link IdvUIManager}. Here we create our own ui manager
+     * so it can do McV specific things.
+     *
+     * @return The UI manager
+     */
+    protected IdvUIManager doMakeIdvUIManager() {
+        return new UIManager(getIdv());
+    }
+
+
+
+    /**
+     * The main. Configure the logging and create the McIdasV
+     *
+     * @param args Command line arguments
+     *
+     * @throws Exception When something untoward happens
+     */
+    public static void main(String[] args) throws Exception {
+        LogUtil.configure();
+        new McIDASV(args);
+    }
+
+
+}
+
+
+
+
+
+
+
---
+++ edu/wisc/ssec/mcidasv/ui/UIManager.java
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ *
+ * Copyright  1997-2004 Unidata Program Center/University Corporation for
+ * Atmospheric Research, P.O. Box 3000, Boulder, CO 80307,
+ * support@unidata.ucar.edu.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package edu.wisc.ssec.mcidasv.ui;
+
+import ucar.unidata.idv.IntegratedDataViewer;
+import ucar.unidata.idv.ui.IdvUIManager;
+
+
+/**
+ * Derive our own ui manager to do some  example specific things.
+ */
+
+public class UIManager extends IdvUIManager {
+
+    /** The tag in the xml ui for creating the special example chooser */
+    public static final String TAG_EXAMPLECHOOSER = "examplechooser";
+
+
+    /**
+     * The ctor. Just pass along the reference to the idv.
+     *
+     * @param idv The idv
+     */
+    public UIManager(IntegratedDataViewer idv) {
+        super(idv);
+    }
+
+}
+
+
+
+
+
+
+
