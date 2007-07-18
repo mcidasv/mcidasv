7f359445027787de71ea5b3c68a307e1015b5676
Bruce Flynn <brucef@ssec.wisc.edu>
2007-07-18 00:49:13 +0000

    To support custom version and about screen

---
+++ edu/wisc/ssec/mcidasv/Constants.java
@@ -0,0 +1,21 @@
+package edu.wisc.ssec.mcidasv;
+
+/**
+ * Application wide constants.
+ * @version $Id$
+ */
+public interface Constants {
+
+	/** Path to about dialog template. */
+	public static String PROP_ABOUTTEXT = "mcidasv.about.text";
+	
+	/** Path to version file. */
+	public static String PROP_VERSIONFILE = "mcidasv.version.file";
+	/** Major version number. */
+	public static String PROP_VERSION_MAJOR = "mcidasv.version.major";
+	/** Minor version number. */
+	public static String PROP_VERSION_MINOR = "mcidasv.version.minor";
+	/** Version release number. */
+	public static String PROP_VERSION_RELEASE = "mcidasv.version.release";
+
+}
