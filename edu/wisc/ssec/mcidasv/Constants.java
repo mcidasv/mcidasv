3d2d62f8af0e170098aad88825456b2963a94949
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2007-07-30 16:26:11 +0000

    Added in some preference strings for the toolbar menu.

--- edu/wisc/ssec/mcidasv/Constants.java
+++ edu/wisc/ssec/mcidasv/Constants.java
@@ -7,9 +7,10 @@ package edu.wisc.ssec.mcidasv;
 public interface Constants {
 
 	/** Application property file name. */
-	public static final String PROPERTIES_FILE = "/edu/wisc/ssec/mcidasv/resources/mcidasv.properties";
+	public static final String PROPERTIES_FILE = 
+		"/edu/wisc/ssec/mcidasv/resources/mcidasv.properties";
 	
-	/** Specifies use of the {@link edu.wisc.ssec.mcidasv.ui.TabbedUIManager}. */
+	/** Specifies use of {@link edu.wisc.ssec.mcidasv.ui.TabbedUIManager}. */
 	public static final String PROP_TABBED_UI = "mcidasv.tabbedDisplay";
 	
 	/** Property name for for the path to about dialog template. */
@@ -27,5 +28,22 @@ public interface Constants {
 	/** Macro for the version in the about HTML file. */
 	public static String MACRO_VERSION = "%MCVERSION%";
 	
+	/** Enabled the toolbar manipulation JPopupMenu. */
+	public static final String PREF_TBM_ENABLED ="tbm.enabled";
+	
+	/** 
+	 * Show large or small icons. If PREF_TBM_ICONS is disabled, this pref
+	 * has no meaning.
+	 */
+	public static final String PREF_TBM_SIZE = "tbm.icon.size";
+	
+	/** Whether or not icons should be shown in the toolbar. */
+	public static final String PREF_TBM_ICONS = "tbm.icon.enabled";
+	
+	/** Whether or not labels should be shown in the toolbar. */
+	public static final String PREF_TBM_LABELS = "tbm.label.enabled";
+	
+	/** The toolbar display option that was chosen. */
+	public static final String PREF_TBM_SELOPT = "tbm.bg.selected";
 	
 }
