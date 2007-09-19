d655da8392fdfd1564608b5179c1564ed82d52e3
Dave Parker <davep@ssec.wisc.edu>
2007-09-19 21:39:14 +0000

    A little closer on having a single window to control frame display... just a little

--- edu/wisc/ssec/mcidasv/ui/McIdasFrameDisplay.java
+++ edu/wisc/ssec/mcidasv/ui/McIdasFrameDisplay.java
@@ -1,67 +1,95 @@
 package edu.wisc.ssec.mcidasv.ui;
 
-import java.*;
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import java.awt.Panel;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 
-import ucar.unidata.util.GuiUtils;
-import ucar.unidata.util.Misc;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class McIdasFrameDisplay extends JFrame {
 
-	private Integer frameNumber;
-	private Image img;
-	private Canvas c;
-
-	public McIdasFrameDisplay(int frame, Image i) {
-		
-		frameNumber = (Integer)frame;
-		img = i;
-		
-		super.setTitle("McIDAS-X Frame " + frameNumber);
-		paintImage();
-		
-	    super.setVisible(true);
+	private Integer frameNumber = 1;
+	private List frameNumbers;
+	private Hashtable images;
+	private Image theImage;
+	private JPanelImage p;
+	
+	public McIdasFrameDisplay(String title, List frameNumbers) {
+		if (frameNumbers.size()<1) return;
+		this.frameNumbers = frameNumbers;
+		this.frameNumber = (Integer)frameNumbers.get(0);
+		this.images = new Hashtable(frameNumbers.size());
+		this.p = new JPanelImage();
+		setTitle(title);
+		getContentPane().add(p);
 	}
 	
-	private void paintImage() {
-		MediaTracker mediaTracker = new MediaTracker(this);
-		mediaTracker.addImage(img, 0);
-		try
-		{
-			mediaTracker.waitForID(0);
-		}
-		catch (InterruptedException ie)
-		{
-			System.err.println(ie);
-			System.exit(1);
-		}
-
-		int width = img.getWidth(null);
-		int height = img.getHeight(null);
-		c = new Canvas() {
-			public void update(Graphics g) {paint(g);}
-			public void paint(Graphics g) {
-				g.drawImage(img,0,0,this);
+	public void setFrameImage(int inFrame, Image inImage) {
+		images.put("Frame " + inFrame, inImage);
+	}
+	
+	public void showFrameNumber(int inFrame) {
+		boolean inList = false;
+		for (int i=0; i<frameNumbers.size(); i++) {
+			Integer frameInt = (Integer)frameNumbers.get(i);
+			if (frameInt.intValue() == inFrame) {
+				inList = true;
+				break;
 			}
-		};
-		c.setSize(width, height);
-	    add(c);
-	    
-		pack();
+		}
+		if (!inList) {
+			System.err.println("showFrameNumber: " + inFrame + " is not a valid frame");
+			return;
+		}
+		frameNumber = (Integer)inFrame;
+		paintFrame();
+		this.setSize(p.getWidth() + 20, p.getHeight() + 40);
+//		pack();
+		if (!isVisible()) {
+			setVisible(true);
+			toFront();
+		}
 	}
 	
 	public int getFrameNumber() {
 		return frameNumber.intValue();
 	}
 	
-	public Image getImage() {
-		return img;
+	private void paintFrame() {
+		theImage = (Image)images.get("Frame " + frameNumber);
+		if (theImage == null) {
+			System.err.println("paintFrame: Got a null image for frame " + frameNumber);
+			return;
+		}
+		
+		MediaTracker mediaTracker = new MediaTracker(this);
+		mediaTracker.addImage(theImage, frameNumber);
+		try {
+			mediaTracker.waitForID(frameNumber);
+		} catch (InterruptedException ie) {
+			System.err.println("MediaTracker exception: " + ie);
+		}
+
+		int width = theImage.getWidth(null);
+		int height = theImage.getHeight(null);
+		this.p.setSize(width, height);
+		this.p.repaint();
 	}
 	
-	public void setImage(Image i) {
-		img = i;
-		paintImage();
+	public class JPanelImage extends JPanel {
+		public JPanelImage() { }
+		public void update(Graphics g) {
+			paint(g);
+		}
+		public void paint(Graphics g) {
+			g.drawImage(theImage, 0, 0, null);
+		}
 	}
   
 }
