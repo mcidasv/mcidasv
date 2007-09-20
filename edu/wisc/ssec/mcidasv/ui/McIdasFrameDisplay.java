1bd43a50b328af69e2350af46bf212288bc6b98d
Dave Parker <davep@ssec.wisc.edu>
2007-09-20 21:27:20 +0000

    Working towards a prettier GUI

--- edu/wisc/ssec/mcidasv/ui/McIdasFrameDisplay.java
+++ edu/wisc/ssec/mcidasv/ui/McIdasFrameDisplay.java
@@ -1,60 +1,150 @@
 package edu.wisc.ssec.mcidasv.ui;
 
-import java.awt.Canvas;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
-import java.awt.Panel;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Hashtable;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class McIdasFrameDisplay extends JFrame {
 
 	private Integer frameNumber = 1;
+	private Integer frameIndex = 0;
 	private List frameNumbers;
 	private Hashtable images;
 	private Image theImage;
-	private JPanelImage p;
+	private JPanelImage pi;
+	private JComboBox cb;
 	
 	public McIdasFrameDisplay(String title, List frameNumbers) {
 		if (frameNumbers.size()<1) return;
+		this.frameIndex = 0;
 		this.frameNumbers = frameNumbers;
-		this.frameNumber = (Integer)frameNumbers.get(0);
+		this.frameNumber = (Integer)frameNumbers.get(this.frameIndex);
 		this.images = new Hashtable(frameNumbers.size());
-		this.p = new JPanelImage();
+		this.pi = new JPanelImage();
+		this.pi.setFocusable(true);
+		
+		String[] frameNames = new String[frameNumbers.size()];
+		for (int i=0; i<frameNumbers.size(); i++) {
+			frameNames[i] = "Frame " + (Integer)frameNumbers.get(i);
+		}
+		JComboBox newCb = new JComboBox(frameNames);
+		newCb.setEditable(false);
+    	newCb.addItemListener(new ItemListener() {
+        	public void itemStateChanged(ItemEvent e) {
+        		if (e.getStateChange() == e.SELECTED) {
+	        		JComboBox myself = (JComboBox)e.getItemSelectable();
+	        		showIndexNumber(myself.getSelectedIndex());
+        		}
+        	}
+        });
+		newCb.addKeyListener(new KeyAdapter() {
+			public void keyTyped(KeyEvent ke) {
+				char keyChar = ke.getKeyChar();
+				if (ke.isAltDown()) {
+					if (keyChar == (char)'a') showFrameNext();
+					else if (keyChar == (char)'b') showFramePrevious();
+				}
+            }
+        });
+    	this.cb = newCb;
+    	
+    	Dimension d = new Dimension(100, 50);
+    	
+        JButton buttonPrevious = new JButton("Previous");
+        buttonPrevious.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	showFramePrevious();
+            }
+        });
+        buttonPrevious.setPreferredSize(d);
+    	
+        JButton buttonNext = new JButton("Next");
+        buttonNext.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	showFrameNext();
+            }
+        });
+        buttonNext.setPreferredSize(d);
+		
 		setTitle(title);
-		getContentPane().add(p);
+		setLayout(new BorderLayout());
+		getContentPane().add(buttonPrevious, BorderLayout.WEST);
+		getContentPane().add(cb, BorderLayout.NORTH);
+		getContentPane().add(buttonNext, BorderLayout.EAST);
+		getContentPane().add(pi);
 	}
 	
 	public void setFrameImage(int inFrame, Image inImage) {
 		images.put("Frame " + inFrame, inImage);
 	}
 	
-	public void showFrameNumber(int inFrame) {
-		boolean inList = false;
-		for (int i=0; i<frameNumbers.size(); i++) {
-			Integer frameInt = (Integer)frameNumbers.get(i);
-			if (frameInt.intValue() == inFrame) {
-				inList = true;
-				break;
-			}
-		}
-		if (!inList) {
-			System.err.println("showFrameNumber: " + inFrame + " is not a valid frame");
-			return;
-		}
-		frameNumber = (Integer)inFrame;
+	private int getIndexPrevious() {
+		int thisIndex = frameIndex.intValue();
+		if (thisIndex > 0)
+			thisIndex--;
+		else
+			thisIndex = frameNumbers.size() - 1;
+		return thisIndex;
+	}
+	
+	private int getIndexNext() {
+		int thisIndex = frameIndex.intValue();
+		if (thisIndex < frameNumbers.size() - 1)
+			thisIndex++;
+		else
+			thisIndex = 0;
+		return thisIndex;
+	}
+	
+	public void showFramePrevious() {
+		showIndexNumber(getIndexPrevious());
+	}
+	
+	public void showFrameNext() {
+		showIndexNumber(getIndexNext());
+	}
+	
+	private void showIndexNumber(int inIndex) {
+		if (inIndex < 0 || inIndex >= frameNumbers.size()) return;
+		frameIndex = (Integer)inIndex;
+		frameNumber = (Integer)frameNumbers.get(inIndex);
+		cb.setSelectedIndex(frameIndex);
 		paintFrame();
-		this.setSize(p.getWidth() + 20, p.getHeight() + 40);
-//		pack();
+		pack();
 		if (!isVisible()) {
 			setVisible(true);
 			toFront();
 		}
+	}
+	
+	public void showFrameNumber(int inFrame) {
+		int inIndex = -1;
+		for (int i=0; i<frameNumbers.size(); i++) {
+			Integer frameInt = (Integer)frameNumbers.get(i);
+			if (frameInt.intValue() == inFrame) {
+				inIndex = (Integer)i;
+				break;
+			}
+		}
+		if (inIndex >= 0)
+			showIndexNumber(inIndex);
+		else
+			System.err.println("showFrameNumber: " + inFrame + " is not a valid frame");
 	}
 	
 	public int getFrameNumber() {
@@ -78,8 +168,10 @@ public class McIdasFrameDisplay extends JFrame {
 
 		int width = theImage.getWidth(null);
 		int height = theImage.getHeight(null);
-		this.p.setSize(width, height);
-		this.p.repaint();
+		Dimension d = new Dimension(width, height);
+		this.pi.setSize(d);
+		this.pi.setPreferredSize(d);
+		this.pi.repaint();
 	}
 	
 	public class JPanelImage extends JPanel {
