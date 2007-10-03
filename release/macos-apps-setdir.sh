5db5223624434f1aaf17d915915e51bfe9c4aff7
Dave Parker <davep@ssec.wisc.edu>
2007-10-03 21:21:50 +0000

    Trial and error on Mac tester--fixed a few issues

--- release/macos-apps-setdir.sh
+++ release/macos-apps-setdir.sh
@@ -3,13 +3,14 @@
 if [ -n "$1" ]; then
 
 	FILE="Contents/MacOS/run_script"
-	export NEW_DIR=`echo "$1" |/usr/bin/sed -e 's/\//\\\\\//g'`
+	TOP_DIR=`echo "$1" |/usr/bin/sed -e 's/\//\\\\\//g'`
+	export APP_DIR="${TOP_DIR}\/McIDAS-V"
 
 	# Run shortcut
 	APP="Run McIDAS-V.app"
 	/bin/ed "${APP}/${FILE}" <<-EOF >/dev/null 2>&1
 		/^INSTALL_DIR=/
-		s/INSTALL_DIR=.*/INSTALL_DIR="${NEW_DIR}"/
+		s/INSTALL_DIR=.*/INSTALL_DIR="${APP_DIR}"/
 		.
 		w
 		q
@@ -19,7 +20,7 @@ if [ -n "$1" ]; then
 	APP="Configure McIDAS-V.app"
 	/bin/ed "${APP}/${FILE}" <<-EOF >/dev/null 2>&1
 		/^INSTALL_DIR=/
-		s/INSTALL_DIR=.*/INSTALL_DIR="${NEW_DIR}"/
+		s/INSTALL_DIR=.*/INSTALL_DIR="${APP_DIR}"/
 		.
 		w
 		q
