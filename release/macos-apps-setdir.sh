d8c4613b5728e9ac0c1a48a95e93e2ba280a6c41
Dave Parker <davep@ssec.wisc.edu>
2007-10-03 21:38:58 +0000

    Working shortcuts created in /Applications if requested

--- release/macos-apps-setdir.sh
+++ release/macos-apps-setdir.sh
@@ -4,10 +4,10 @@ if [ -n "$1" ]; then
 
 	FILE="Contents/MacOS/run_script"
 	TOP_DIR=`echo "$1" |/usr/bin/sed -e 's/\//\\\\\//g'`
-	export APP_DIR="${TOP_DIR}\/McIDAS-V"
+	export APP_DIR="${TOP_DIR}"
 
 	# Run shortcut
-	APP="Run McIDAS-V.app"
+	APP="/Applications/McIDAS-V Launcher.app"
 	/bin/ed "${APP}/${FILE}" <<-EOF >/dev/null 2>&1
 		/^INSTALL_DIR=/
 		s/INSTALL_DIR=.*/INSTALL_DIR="${APP_DIR}"/
@@ -17,7 +17,7 @@ if [ -n "$1" ]; then
 	EOF
 
 	# Configure shortcut
-	APP="Configure McIDAS-V.app"
+	APP="/Applications/McIDAS-V Configuration.app"
 	/bin/ed "${APP}/${FILE}" <<-EOF >/dev/null 2>&1
 		/^INSTALL_DIR=/
 		s/INSTALL_DIR=.*/INSTALL_DIR="${APP_DIR}"/
