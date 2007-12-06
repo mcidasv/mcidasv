524ca300c4a8274a6884d2fafbcd654cbeba3d4b
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2007-12-06 21:20:18 +0000

    Added heap size variable for easy parsing.

--- release/runMcV.bat
+++ release/runMcV.bat
@@ -1,3 +1,4 @@
-jre\bin\java -Xmx512m -cp idv.jar -jar mcidasv.jar %*
+SET HEAP_SIZE=512m
+jre\bin\java -Xmx%HEAP_SIZE% -cp idv.jar -jar mcidasv.jar %*
 REM Use the line below instead if you want to use the D3D version of Java 3D
-REM jre\bin\java -Xmx512m -Dj3d.rend=d3d -cp idv.jar -jar mcidasv.jar %*
+REM jre\bin\java -Xmx%HEAP_SIZE% -Dj3d.rend=d3d -cp idv.jar -jar mcidasv.jar %*
