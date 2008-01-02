3d86b603bbb84414e6acd655f05710c7ceae4ece
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2008-01-02 18:00:12 +0000

    Plugin Manager RSS feeds now have some conception of plugin versioning. We'll use this to allow users to roll back plugin updates if desired.

--- tools/external/pluginfeed/plugins.py
+++ tools/external/pluginfeed/plugins.py
@@ -2,32 +2,36 @@ import os
 import datetime
 import PyRSS2Gen as RSS2
 
+# Please see plugin_desc_example.txt for an example plugin description file.
+
 # Points to the directory where all plugin description files live.
 # It is a good idea to keep the description files and plugins separate.
+#DESCRIPTION_DIRECTORY = '/Users/jbeavers/rsstest/descs'
 DESCRIPTION_DIRECTORY = ''
 
 # Points to the directory that contains the actual plugins.
 # Again, keep the description directory and plugin directory separate.
+#PLUGIN_DIRECTORY = '/Users/jbeavers/rsstest/plugins'
 PLUGIN_DIRECTORY = ''
 
-# A file that will store the list of plugins that have been seen so far. It'll
-# also be used to determine if a plugin has been updated since the last run
-# of this script.
-KNOWN_PLUGINS = ''
-
 # The filename that will eventually store our RSS data.
+#RSS_FILE = '/Users/jbeavers/rsstest/test.rss'
 RSS_FILE = ''
 
 # The URL to the directory where the plugins live. Leave off the trailing '/'.
+#WWW_PREFIX = 'http://www/mcidas/v'
 WWW_PREFIX = ''
 
 # The title of the feed. This is used so that we still [kinda] conform to RSS2
+#FEED_TITLE = 'SSEC McIDAS-V Plugins'
 FEED_TITLE = ''
 
 # A link back to the feed. Again, for conformance.
+#FEED_LINK = 'http://www/mcidas/v/%s' % (RSS_FILE)
 FEED_LINK = ''
 
-# Umm...
+# A brief description of the feed.
+#FEED_DESCRIPTION = 'Default list of SSEC McV plugins'
 FEED_DESCRIPTION = ''
 
 class McVRSSItem(RSS2.RSSItem):
@@ -39,188 +43,114 @@ class McVRSSItem(RSS2.RSSItem):
                  category,
                  guid,
 
-                 updated = None):
+                 version = None):
 
 		RSS2.RSSItem.__init__(self, title=title, link=link, description=description, pubDate=pubDate, categories=[category], guid=guid)
-		self.updated = updated
+		self.version = version
 		
 	def publish_extensions(self, handler):
 		"""
 		Overrides RSSItem.publish_extension so that I can provide a 
 		mcv:updated tag if needed.
 		"""
-		if self.updated != None:
-			RSS2._opt_element(handler, "mcv:updated", self.updated)
+		if self.version != None:
+			RSS2._opt_element(handler, "mcv:version", self.version)
 
-"""
-Current item structure:
-<item>
-  <title>Workshop McV</title>
-  <link>http://www.ssec.wisc.edu/mcidas/software/v/plugins/workshop.blah</link>
-  <description>Workshop-specific configuration for McV</description>
-  <category>Misc</category>
-  <guid isPermaLink="false">http://www.ssec.wisc.edu/mcidas/software/v/plugins/workshop.blah</guid>
-  <mcv:updated>1337</mcv:updated>
-  <pubDate>blah blah</pubDate>
-</item>
-
-Issue: picking up new plugins or modifications:
-so I work thru plugin dir and find mtimes.
-then I work through a text file that has mtimes and plugins
-
-if plugin from directory isn't in the text file, it's added to the feed
-if plugin from directory has a newer mtime, it's added to the feed
-
-mcv will have to be smart enough to signal new or updated plugins
-I can just provide an extra tag similar to what LJ does: <mcv:updated>timestamp</mcv:updated>
-mcv:updated will merely be the timestamp of the last update. there will never be more than
-one mcv:updated per item. pubDate will always match the latest mtime.
-
-Issue: what if a plugin is removed?
-if a plugin wasn't found in the directory, but was in the file, remove from feed
-mcv will have to be smart enough to know to remove it from the list of plugins
-
-Issue: what if the feed gets huge?
-the feed should contain all available plugins--this is the only approach
-"""
-
-
-
-def read_description(file):
+def read_descriptions(directory):
 	"""
-	Reads a given description file and returns a dictionary containing the relevant data.
+	Iterate through all of the files in the given directory and attempt to
+	read plugin descriptions from each. This will return a dictionary of lists
+	with the key being the name of the plugin. Each list in a dictionary entry
+	will correspond to different versions of a given plugin.
 	"""
-	description = {}
-	for line in open(file).readlines():
-		(key, value) = line.split('=')
-		description[key] = value.strip()
-	return description
+	descriptions = {}
+	for description in os.listdir(directory):
+		fname = '%s/%s' % (directory, description)
+		current = {}
+		for line in open(fname).readlines():
+			# ignore comments
+			if line[0] == '#':
+				print 'si!'
+				continue
 
-def list_descriptions(directory):
-	"""
-	Return a filtered list of description files. Basically just filters out '.' and '..'.
-	"""
-	newlist = []
-	for file in os.listdir(directory):
-		if file != '.' and file != '..':
-			newlist.append(file)
-			
-	return newlist
+			(key, value) = line.split('=')
+			current[key] = value.strip()
+		
+		try:
+			descriptions[current['title']].append(current)
+		except KeyError:
+			descriptions[current['title']] = [current]
 
-def poll_plugins(directory):
+	return descriptions
+
+def poll_mtimes(directory):
 	"""
 	Work through the plugin directory and accumulate mtime timestamps for each plugin.
 	"""
 	plugins = {}
 	for plugin in os.listdir(directory):
-		if plugin != '.' and plugin != '..':
-			statinfo = os.stat('%s/%s' % (directory, plugin))
-			plugins[plugin] = statinfo.st_mtime
-			
+		statinfo = os.stat('%s/%s' % (directory, plugin))
+		plugins[plugin] = statinfo.st_mtime
+
 	return plugins
 
-def read_plugin_list(file):
+def create_feed_items(feed):
 	"""
-	Read the plugins that have already been placed in the RSS feed. 
-	Return the plugins and their last-observed mtimes.
+	Iterates through all feed items and eventually returns a list of 
+	McVRSSItem objects that correspond to each plugin.
 	"""
-	plugins = {}
-	try:
-		for line in open(file).readlines():
-			(plugin, mtime) = line.strip().split(':')
-			plugins[plugin] = float(mtime)
-	except IOError:
-		pass
-		
-	return plugins
-
-def write_plugin_list(file, plugin_feed):
-	try:
-		f = open(file, 'w+')
-		for plugin in plugin_feed.keys():
-			f.write('%s:%s\n' % (plugin, plugin_feed[plugin]['time']))
-			#print 'Plugin: %s mtime=%s' % (plugin, plugin_feed[plugin]['time'])
-		f.close()
-	except:
-		print 'Error writing plugins!!'
-
-def create_feed_items(data):
 	items = []
-	for plugin in data.keys():
-		attrs = data[plugin]
-		if attrs['updated'] == True:
-			item = McVRSSItem(
-			    title = attrs['title'],
-			    link = '%s/%s' % (WWW_PREFIX, attrs['plugin']),
-			    description = attrs['desc'],
-			    pubDate = str(datetime.datetime.utcfromtimestamp(attrs['time'])),
-			    updated = str(datetime.datetime.utcfromtimestamp(attrs['time'])),
-			    category = attrs['category'],
-			    guid = '%s/%s' % (WWW_PREFIX, attrs['plugin']),
-			)
-		else:
-			item = McVRSSItem(
-			    title = attrs['title'],
-			    link = '%s/%s' % (WWW_PREFIX, attrs['plugin']),
-			    description = attrs['desc'],
-			    pubDate = str(datetime.datetime.utcfromtimestamp(attrs['time'])),
-			    category = attrs['category'],
-			    guid = '%s/%s' % (WWW_PREFIX, attrs['plugin']),
+	for plugin in feed.keys():
+		attrs = feed[plugin]
+		item = McVRSSItem(
+		    title = attrs['title'],
+		    link = '%s/%s' % (WWW_PREFIX, attrs['link']),
+		    description = attrs['description'],
+		    pubDate = str(datetime.datetime.utcfromtimestamp(attrs['time'])),
+		    category = attrs['category'],
+		    guid = '%s/%s' % (WWW_PREFIX, attrs['link']),
+			version = attrs['version']
 			)
 		items.append(item)
-	return items
+	return items	
 
-def create_rss(file, feed):
+def create_rss(fname, feed):
+	"""
+	Uses PyRSS2Gen to generate a RSS feed based upon the information passed
+	in the feed parameter. Writes out the RSS to the file name specified by 
+	fname.
+	"""
 	items = create_feed_items(feed)
 	rss = RSS2.RSS2(
 	    title = FEED_TITLE,
 	    link = FEED_LINK,
 	    description = FEED_DESCRIPTION,
 	    lastBuildDate = datetime.datetime.now(),
-	
 	    items = items
 	)
 
-	rss.write_xml(open(file, 'w+'))
-
+	rss.write_xml(open(fname, 'w+'))
 
 def main():
-	"""
-	The main function sifts through all of the various files and information to build
-	the RSS file containing all of our plugins.
-	"""
-	descriptions = {}
-	for file in list_descriptions(DESCRIPTION_DIRECTORY):
-		desc = read_description('%s/%s' % (DESCRIPTION_DIRECTORY, file))
-		descriptions[desc['plugin']] = desc
+	# first read all of the plugin description files
+	descriptions = read_descriptions(DESCRIPTION_DIRECTORY)
 	
-	old_plugins = read_plugin_list(KNOWN_PLUGINS)
-	current_mtimes = poll_plugins(PLUGIN_DIRECTORY)
+	# now figure out when each plugin was last updated (for RSS conformance)
+	current_mtimes = poll_mtimes(PLUGIN_DIRECTORY)
 	
+	# loop through all versions of each plugin and add it to the feed data
 	feed = {}
 	for plugin in descriptions.keys():
-		# if the plugin has already been observed we need to determine if it 
-		# has been updated.
-		if plugin in old_plugins.keys():
-			feed[plugin] = descriptions[plugin]
+		for version in descriptions[plugin]:
+			plugin_file = version['link']
+
+			feed[plugin_file] = version
 			
-			if current_mtimes[plugin] > old_plugins[plugin]:
-				# looks like there's been an update
-				feed[plugin]['updated'] = True
-				feed[plugin]['time'] = current_mtimes[plugin]
-			else:
-				# no update, just use "old" timestamp
-				feed[plugin]['updated'] = False
-				feed[plugin]['time'] = old_plugins[plugin]
-		else:
-			# haven't seen this plugin, add it to the feed!
-			feed[plugin] = descriptions[plugin]
-			feed[plugin]['time'] = current_mtimes[plugin]
-			feed[plugin]['updated'] = False
-	
+			# be sure to store the mtimes for the RSS pubDate tag
+			feed[plugin_file]['time'] = current_mtimes[plugin_file]
+
+	# write out the RSS representation of the feed data
 	create_rss(RSS_FILE, feed)
-	
-	write_plugin_list(KNOWN_PLUGINS, feed)
 
 if __name__ == '__main__':
 	main()
