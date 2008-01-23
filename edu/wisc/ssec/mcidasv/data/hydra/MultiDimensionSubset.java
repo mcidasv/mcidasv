abb5091fe305648c568ea712e7c8036bd4b97de2
Tom Rink <rink@ssec.wisc.edu>
2008-01-23 16:37:26 +0000

    Handle persistence of enclosed HashMap directly.

--- edu/wisc/ssec/mcidasv/data/hydra/MultiDimensionSubset.java
+++ edu/wisc/ssec/mcidasv/data/hydra/MultiDimensionSubset.java
@@ -2,23 +2,64 @@ package edu.wisc.ssec.mcidasv.data.hydra;
 
 import ucar.unidata.data.DataSelection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class MultiDimensionSubset extends DataSelection {
 
-  HashMap subset;
+  private double[][] coords = null;
+  private String[] keys = null;
+
+  public MultiDimensionSubset() {
+    super();
+  }
 
   public MultiDimensionSubset(HashMap subset) {
     super();
-    this.subset = subset;
+    coords = new double[subset.size()][];
+    keys = new String[subset.size()];
+    Iterator iter = subset.keySet().iterator();
+    int cnt =0;
+    while (iter.hasNext()) {
+       String key = (String) iter.next();
+       keys[cnt] = key;
+       coords[cnt] = (double[]) subset.get(key);
+       cnt++;
+    }
   }
 
-  HashMap getSubset() {
-    return subset;
+  public MultiDimensionSubset(double[][] coords, String[] keys) {
+    this.coords = coords;
+    this.keys = keys;
+  }
+
+  public HashMap getSubset() {
+    HashMap hmap = new HashMap();
+    for (int k=0; k<keys.length; k++) {
+      hmap.put(keys[k], coords[k]);
+    }
+    return hmap;
+  }
+
+  public double[][] getCoords() {
+    return coords;
+  }
+
+  public void setCoords(double[][] coords) {
+    this.coords = coords;
+  }
+
+  public String[] getKeys() {
+    return keys;
+  }
+
+  public void setKeys(String[] keys) {
+    this.keys = keys;
   }
 
   public MultiDimensionSubset cloneMe() {
-     return new MultiDimensionSubset(this.subset);
+    return new MultiDimensionSubset(coords, keys);
   }
 
 }
