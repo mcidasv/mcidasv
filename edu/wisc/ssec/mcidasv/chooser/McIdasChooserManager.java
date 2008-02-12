15c1ac5103c0b6644cc7702bd21801fa7535290b
Dave Parker <davep@ssec.wisc.edu>
2008-02-12 21:55:25 +0000

    Added licence and copyright header to all .java files

--- edu/wisc/ssec/mcidasv/chooser/McIdasChooserManager.java
+++ edu/wisc/ssec/mcidasv/chooser/McIdasChooserManager.java
@@ -1,3 +1,29 @@
+/*
+ * $Id$
+ *
+ * Copyright 2007-2008
+ * Space Science and Engineering Center (SSEC)
+ * University of Wisconsin - Madison,
+ * 1225 W. Dayton Street, Madison, WI 53706, USA
+ *
+ * http://www.ssec.wisc.edu/mcidas
+ *
+ * This file is part of McIDAS-V.
+ * 
+ * McIDAS-V is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * McIDAS-V is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses
+ */
+
 package edu.wisc.ssec.mcidasv.chooser;
 
 
@@ -80,7 +106,7 @@ import javax.swing.event.*;
  * This piece has always been a bit flaky
  *
  * @author IDV development team
- * @version $Revision$Date: 2007/11/27 20:17:06 $
+ * @version $Revision$Date: 2007/12/03 21:33:52 $
  */
 
 public class McIdasChooserManager extends IdvChooserManager {
--- edu/wisc/ssec/mcidasv/control/HydraPlanViewControl.java
+++ edu/wisc/ssec/mcidasv/control/HydraPlanViewControl.java
@@ -1,3 +1,29 @@
+/*
+ * $Id$
+ *
+ * Copyright 2007-2008
+ * Space Science and Engineering Center (SSEC)
+ * University of Wisconsin - Madison,
+ * 1225 W. Dayton Street, Madison, WI 53706, USA
+ *
+ * http://www.ssec.wisc.edu/mcidas
+ *
+ * This file is part of McIDAS-V.
+ * 
+ * McIDAS-V is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * McIDAS-V is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses
+ */
+
 package edu.wisc.ssec.mcidasv.control;
 
 
