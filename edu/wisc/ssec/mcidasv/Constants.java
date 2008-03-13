c676b523215d1106a01cc44e42adb7c3b43d7c8f
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2008-03-13 23:20:01 +0000

    Added a few constants for incoming view panel changes. [0]

--- edu/wisc/ssec/mcidasv/Constants.java
+++ edu/wisc/ssec/mcidasv/Constants.java
@@ -39,14 +39,14 @@ public interface Constants {
 	 * field selector
 	 */
 	public static final String DATASELECTOR_NAME = "Data Explorer";
-	
+
 	/**
 	 * A thing that contains one or more of the things named
 	 * <tt>PANEL_NAME</tt>. One of these can be either in a tab
 	 * or in it's own window.
 	 */
 	public static final String DISPLAY_NAME = "Display";
-	
+
 	/**
 	 * The name of a thing that contains the display/layer controls
 	 */
@@ -59,7 +59,7 @@ public interface Constants {
 	public static String MACRO_IDV_VERSION = "%IDVVERSION%";
 	/** Macro for the version in the about HTML file. */
 	public static String MACRO_VERSION = "%MCVERSION%";
-	
+
 	/** 
 	 * Java OS descriptor for the Max OSX operating system. This should be 
 	 * constant for any machine running java on OSX.
@@ -71,81 +71,92 @@ public interface Constants {
 	 * toolbar.
 	 */
 	public static final String PANEL_NAME = "Panel";
-	
+
 	/** Gail's server preference manager. */
 	public static final String PREF_LIST_ADDE_SERVERS = "ADDE Servers";
-	
+
 	/** Advanced prefs for IDV, Java, and McIDAS-X. */
 	public static final String PREF_LIST_ADVANCED = "Advanced";
-	
+
 	/** Prefs for which display types to allow. */
 	public static final String PREF_LIST_AVAILABLE_DISPLAYS = "Available Displays";
-	
+
 	/** Prefs for which data choosers should show up. */
 	public static final String PREF_LIST_DATA_CHOOSERS = "Data Sources";
-	
+
 	/** Name of panel containing prefs related to data formatting. */
 	public static final String PREF_LIST_FORMATS_DATA = "Formats & Data";
-	
+
 	/** Name of the panel that holds the "general" sorts of user prefs. */
 	public static final String PREF_LIST_GENERAL = "General";
-	
+
 	/** Panel name for the different nav control scheme prefs. */
 	public static final String PREF_LIST_NAV_CONTROLS = "Navigation Controls";
-	
+
 	/** Prefs for configuring what appears in the toolbar. */
 	public static final String PREF_LIST_TOOLBAR = "Toolbar Options";
-	
+
 	/** Name of the different prefs for configuring how tabs/windows look. */
 	public static final String PREF_LIST_VIEW = "Display Window";
-	
-	/** The name of the version check user preference. */	
-	public static final String PREF_VERSION_CHECK = "mcidasv.doversioncheck";	
-	
+
+	/** The name of the version check user preference. */
+	public static final String PREF_VERSION_CHECK = "mcidasv.doversioncheck";
+
 	/** 
 	 * Show large or small icons. If PREF_TBM_ICONS is disabled, this pref
 	 * has no meaning.
 	 */
 	public static final String PREF_TBM_SIZE = "tbm.icon.size";
-	
+
 	/** Property name for for the path to about dialog template. */
 	public static String PROP_ABOUTTEXT = "mcidasv.about.text";
-	
+
 	public static String PROP_BUILD_DATE = "mcidasv.build.date";
-	
+
 	/** Property name for the copyright year. */
 	public static String PROP_COPYRIGHT_YEAR = "mcidasv.copyright.year";
-	
+
 	/** Property name for the McIdas-V homepage URL. */
 	public static String PROP_HOMEPAGE = "mcidasv.homepage";
-	
+
 	/** Specifies use of {@link edu.wisc.ssec.mcidasv.ui.TabbedUIManager}. */
 	public static final String PROP_TABBED_UI = "mcidasv.tabbedDisplay";
-		
+
 	/** Property name for the major version number. */
 	public static String PROP_VERSION_MAJOR = "mcidasv.version.major";
-	
+
 	/** Property name for the minor version number. */
 	public static String PROP_VERSION_MINOR = "mcidasv.version.minor";
-	
+
 	/** Property name for the version release number. */
 	public static String PROP_VERSION_RELEASE = "mcidasv.version.release";
-	
+
 	/** Property name for the path to version file. */
 	public static String PROP_VERSIONFILE = "mcidasv.version.file";
-	
+
+	/** Property that determines whether the view panel should pop up. */
+	public static final String PROP_VP_SHOWPOPUP = 
+		"idv.ui.viewpanel.showpopup";
+
+	/** Property for whether view panel categories will be shown. */
+	public static final String PROP_VP_SHOWCATS = 
+		"idv.ui.viewpanel.showcategories";
+
+	/** typo was found in IDV code. */
+	public static final String PROP_VP_CATOPEN = "viewpanel.catgegory.open";
+
 	/** Application property file name. */
 	public static final String PROPERTIES_FILE = 
 		"/edu/wisc/ssec/mcidasv/resources/mcidasv.properties";
-	
+
 	/** McIDAS-V webpage base url */
 	public static final String HOMEPAGE_URL = "http://www.ssec.wisc.edu/mcidas/software/v";
-	
+
 	/** Location of latest version file under base url */
 	public static final String VERSION_URL = "stable/version.txt";
-	
+
 	public static final String SCRUB_STRINGS_FILE = "/edu/wisc/ssec/mcidasv/resources/scrubstrings.xml";
-	
+
     /** Where to look for javahelp */
     public static final String DEFAULT_DOCPATH = "/docs/userguide";
     
@@ -154,7 +165,7 @@ public interface Constants {
 
     /** File suffix for compressed bundle files */
     public static final String SUFFIX_ZIDV = ".mcvz";
-    
+
     /** File filter used for bundle files */
     public static final PatternFileFilter FILTER_XIDV =
         new PatternFileFilter("(.+\\.mcv$)", "McIDAS-V Bundles (*.mcv)",
@@ -169,6 +180,4 @@ public interface Constants {
     public static final PatternFileFilter FILTER_XIDVZIDV =
         new PatternFileFilter("(.+\\.mcv$|.+\\.mcvz$)",
                               "All McIDAS-V Bundles (*.mcv,*.mcvz)", SUFFIX_XIDV);
-
-    
 }
