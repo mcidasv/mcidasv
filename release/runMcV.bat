d0c2a73af91f9e50b92cdd43f7d6c1caf86b8178
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2008-07-29 18:43:32 +0000

    Updated Windows startup script to work with the 3d toggle in the startup prefs. [128]

--- release/runMcV.bat
+++ release/runMcV.bat
@@ -12,5 +12,10 @@ IF NOT EXIST "%USERPROFILE%\.mcidasv\runMcV-Prefs.bat" copy "runMcV-Prefs.bat" "
 REM If .mcidas\runMcV-Prefs.bat exists, call it to populate the current environment
 IF EXIST "%USERPROFILE%\.mcidasv\runMcV-Prefs.bat" CALL "%USERPROFILE%\.mcidasv\runMcV-Prefs.bat"
 
+SET enable_3d=true
+IF %USE_3DSTUFF%==0 SET enable_3d=false
+
+SET MCV_FLAGS=-Didv.3d=%enable_3d%
+
 REM Start McIDAS-V
-jre\bin\java -Xmx%HEAP_SIZE% %D3DREND% -cp idv.jar -jar mcidasv.jar %*
+jre\bin\java -Xmx%HEAP_SIZE% %D3DREND% -cp idv.jar -jar mcidasv.jar %MCV_FLAGS% %*
