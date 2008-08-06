9c2c467d932a73f160b0159f3409e705cfecda8c
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2008-08-06 20:36:16 +0000

    Using the combine function from the interpreter now populate the field selector's combo slot. [126]

--- edu/wisc/ssec/mcidasv/resources/test.py
+++ edu/wisc/ssec/mcidasv/resources/test.py
@@ -1,9 +1,10 @@
 from java.awt import Color
 from edu.wisc.ssec.mcidasv.control.LinearCombo import Selector
+from edu.wisc.ssec.mcidasv.data.hydra import MultiSpectralData
 
-def selector(channel=919.5, color=Color.GREEN):
+def selector(channel=MultiSpectralData.init_wavenumber, color=Color.GREEN):
     sel = Selector(float(channel), color, _linearCombo, _jythonConsole)
     return sel
 
-def combine():
-    pass
+def combine(combination):
+    _linearCombo.setComboChoice(combination.getData())
