5bfe862be4f8a5ff883fb32995aac24a482e2f41
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2008-08-07 20:10:39 +0000

    Python support for adding combinations to the field selector. [126]

--- edu/wisc/ssec/mcidasv/resources/test.py
+++ edu/wisc/ssec/mcidasv/resources/test.py
@@ -6,5 +6,6 @@ def selector(channel=MultiSpectralData.init_wavenumber, color=Color.GREEN):
     sel = Selector(float(channel), color, _linearCombo, _jythonConsole)
     return sel
 
-def combine(combination):
-    _linearCombo.setComboChoice(combination.getData())
+def combine(combination=None, name='Linear Combination'):
+    if combination != None:
+        _linearCombo.addCombination(name, combination.getData())
