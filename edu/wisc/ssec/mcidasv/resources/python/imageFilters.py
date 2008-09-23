0cde04a30e1ff1315fea0de2963cf45d25b3f13a
Kevin Baggett <kevinb@ssec.wisc.edu>
2008-09-23 21:20:37 +0000

    Change of Low Pass 2D filter to more closely match what is seen in X Also some clean up of print statements and comments [135]

--- edu/wisc/ssec/mcidasv/resources/python/imageFilters.py
+++ edu/wisc/ssec/mcidasv/resources/python/imageFilters.py
@@ -1,4 +1,5 @@
 from visad.python.JPythonMethods import *
+
 def cloudFilter(sdataset1,sdataset2,replace=0,default=0):
   """ cloud filter from McIDAS-X """
   newData1=sdataset1.clone()
@@ -95,75 +96,6 @@ def shotFilter(sdataset,bline=0,eline=999999,filter_diff=15):
    
    return newData
 
-def shotFilter2(sdataset,bline=0,eline=999999,filter_diff=15, radius=5):
-   """ shot filter #2 from McIDAS-X - called from clean filter with radius argument """  
-   
-   newData = sdataset.clone()
-      
-   for t in range(newData.getDomainSet().getLength()):
-     rangeObject = newData.getSample(t)
-     vals = rangeObject.getFloats(0)
-     domain=GridUtil.getSpatialDomain(rangeObject)
-     x=domain.getLinear1DComponent(0)
-     y=domain.getLinear1DComponent(1)
-              
-     [element_size,line_size]=domain.getLengths()
-     
-     for i in range(len(y)):
-       print 'bob'
-       if (i > 1):
-          return newData 
-       for j in range(len(x))[1:-1]:
-         getNextElement=0
-         next = i + 1
-         value = vals[0][j*line_size + i]
-                  
-         if (value == 0):
-            for k in range(radius):
-               next = i + k
-               if (next > element_size):
-                 getNextElement=1
-                 break
-               newVal = vals[0][j*line_size + next]
-               if (newVal != 0 ):
-                 break
-            if (k == radius):
-              print 'Here 2'
-              getNextElement = 1
-         
-         if (getNextElement):
-            continue
-           
-         left = vals[0][j*line_size + i - 1]         
-         right = vals[0][j*line_size + next]
-                                 
-         left_diff = value - left
-         right_diff = value - right
-         
-         ave_sum = 0
-         ave_div = 0
-
-         sign = left_diff *right_diff
-         if (sign < 0):
-           continue
-         
-         left_diff = abs(left_diff)
-         if (left_diff > filter_diff):
-	   ave_div = ave_div+1
-           ave_sum = ave_sum + left
-
-         right_diff = abs(right_diff)
-         if (right_diff > filter_diff):
-	   ave_div = ave_div+1
-           ave_sum = ave_sum + right
-         
-         print value, left, right, left_diff, right_diff, ave_div, ave_sum
-         if (ave_div > 0):
-           vals[0][i*line_size + i] = ave_sum / ave_div
-            
-     rangeObject.setSamples(vals,1)
-   
-   return newData
 
 def spotFilter(sdataset,omcon=0,oacon=0,imcon=0,iacon=0,cmin=0,cmax=0):
    """ spot filter from McIDAS-X """
@@ -184,7 +116,6 @@ def gradientFilter(sdataset):
      vals = rangeObject.getFloats(0)
      in_hi = int(max(max(vals)))
      in_low = int(min(min(vals))) 
-     print 'min/max=', in_low, in_hi
      domain=GridUtil.getSpatialDomain(rangeObject)
                    
      [element_size,line_size]=domain.getLengths()
@@ -198,7 +129,7 @@ def gradientFilter(sdataset):
      
      post_hi = int(max(max(vals)))
      post_low = int(min(min(vals))) 
-     print 'post-processed min/max=',post_low,post_hi  
+      
      lookup=contrast(post_low,post_hi,post_low,post_hi)
      vals=modify(vals,element_size,line_size,post_low,lookup) 
      rangeObject.setSamples(vals)
@@ -219,11 +150,8 @@ def passFilter(sdataset,user_passname,user_radius=50,user_leak=100):
      in_hi = int(max(max(vals)))
      in_low = int(min(min(vals))) 
      midpoint = (in_hi - in_low)/2 + in_low
-     print 'minimum=',in_low,in_hi
      domain=GridUtil.getSpatialDomain(rangeObject)
-            
      [element_size,line_size]=domain.getLengths()
-     print element_size,line_size
      radiusArray=range(nmod)
      
      for i in xrange(line_size):
@@ -259,7 +187,7 @@ def passFilter(sdataset,user_passname,user_radius=50,user_leak=100):
             
      post_hi = int(max(max(vals)))
      post_low = int(min(min(vals))) 
-     print 'post-processed min/max=',post_low,post_hi  
+     
      lookup=contrast(post_low,post_hi,post_low,post_hi)
      vals=modify(vals,element_size,line_size,post_low,lookup) 
      rangeObject.setSamples(vals)
@@ -279,8 +207,7 @@ def lowPass2DFilter(sdataset,user_linecoef=0.5,user_elecoef=0.5):
      vals = rangeObject.getFloats(0)
      domain=GridUtil.getSpatialDomain(rangeObject)
      [element_size,line_size]=domain.getLengths()
-     print element_size,line_size   
-
+     
      """ save the first line """
      firstLine = vals[0][0:element_size].tolist()
      
@@ -291,37 +218,29 @@ def lowPass2DFilter(sdataset,user_linecoef=0.5,user_elecoef=0.5):
           if (vals[0][i*element_size + j] > 0):
             val = ecoef*val + e1 * vals[0][i*element_size + j]
           vals[0][i*element_size + j] = round(val)
-          if ( i == 5):
-            print round(val), j
+     
        """ right to left filter along line """
        val = vals[0][i*element_size + (element_size - 1)]
-       if (i == 5):
-          print 'Initial val=', val
+     
        for j in xrange(element_size - 1, 0, -1):
          val=ecoef*val + e1 * vals[0][i*element_size + j]
          vals[0][i*element_size + j] = round(val)
-         if (i == 5):
-           print 'Return val=', val, j 
+      
        """ filter along the elements """
        for j in xrange(element_size):
          val = lcoef * firstLine[j] + l1 * vals[0][i*element_size + j]
-         if (i == 5):
-           print 'Element val=', val, j, firstLine[j], vals[0][i*element_size + j]
          vals[0][i*element_size + j] = round(val) 
 
      """ filter along the lines going through the image up the elements """
-     """ save the last line """
-     """ print (line_size -1) *element_size, (line_size -1)*element_size + (element_size -1)
-     lastLine = vals[0][(line_size)*element_size : (line_size)* (element_size) + (element_size)].tolist()
-     print lastLine
-     for i in xrange(line_size , 0, -1):
+     """ get the modified first line """
+     firstLine = vals[0][0:element_size].tolist()
+     for i in xrange(line_size - 1, 0, -1):
         for j in range(element_size):
-          val = lcoef * lastLine[j] + l1 * vals[0][i*element_size + j]
-          vals[0][i*element_size + j] = round(val) """
+          val = lcoef * firstLine[j] + l1 * vals[0][i*element_size + j]
+          vals[0][i*element_size + j] = round(val) 
 
      post_hi = int(max(max(vals)))
      post_low = int(min(min(vals))) 
-     print 'post-processed min/max=',post_low,post_hi  
      lookup=contrast(post_low,post_hi,post_low,post_hi)
      vals=modify(vals,element_size,line_size,post_low,lookup) 
      rangeObject.setSamples(vals)
@@ -352,15 +271,12 @@ def highPass2DFilter(sdataset):
             vals[0][i * element_size + j] = midValue - val
          else:
             vals[0][i * element_size + j] = 0
-         if (i == 405):
-            print 'values are ', i, j, vals[0][i*element_size + j]
-
+     
      vals[0] = firstLine + vals[0][0:(line_size-1)*element_size]
      post_hi = int(max(max(vals)))
      post_low = int(min(min(vals))) 
-     print 'post-processed min/max=',post_low,post_hi  
-     """ lookup=contrast(post_low,post_hi,post_low,post_hi)
-     vals=modify(vals,element_size,line_size,post_low,lookup) """
+     lookup=contrast(post_low,post_hi,post_low,post_hi)
+     vals=modify(vals,element_size,line_size,post_low,lookup)
      rangeObject.setSamples(vals)
 
    return newData
@@ -384,8 +300,7 @@ def contrast(in_low,in_hi,minimum,maximum,out_low=0,out_hi=255,inc=1):
       slope=0
    else:
       slope=float(out_hi - out_low)/float(in_hi - in_low)
-   print 'slope =', out_hi, out_low, in_hi, in_low, slope, maximum, minimum
-
+ 
    lookup = []
    for input_value in xrange(minimum, maximum + 1, inc):
       out_value = out_low + int(round((input_value - in_low)*slope))
@@ -395,20 +310,16 @@ def contrast(in_low,in_hi,minimum,maximum,out_low=0,out_hi=255,inc=1):
         out_value=min(max(out_value,out_low),out_hi)
       
       indx=input_value - minimum
-      print indx, input_value, inc, out_value
       for i in xrange(inc):
          lookup.insert(indx, out_value)
          indx = indx + 1
    
-   print lookup
    return lookup
 
 def modify(vals,element_size,line_size,minimum,lookup):
   """ modifies an image with the lookup table generated by a stretch function """
-  print line_size, element_size
   for i in xrange(line_size):
      for j in xrange(element_size):
-        """ print i, j, minimum, vals[0][i*element_size+j] - minimum + 1, lookup[int(vals[0][i*element_size+j]-minimum -1)], len(lookup) """
         vals[0][i*element_size + j] = lookup[int(vals[0][i*element_size+j] - minimum)]
 
   return vals
