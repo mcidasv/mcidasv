a2ed44dcc9badd0d913c83e2863d03f30757049b
Dave Parker <davep@ssec.wisc.edu>
2008-11-21 09:00:07 +0000

    [0] Dreamweaver updates synced from web server

--- docs/userguide/processed/mcv-top.php
+++ docs/userguide/processed/mcv-top.php
@@ -1,6 +1,6 @@
 <table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
   <tr>
     <td width=300 align=center valign=middle><img src="../../../images/mcidas-logos/mcidasv_200x100.png"></td>
-    <td align=center valign=middle><h1>McIDAS-V User's Guide</h1></td>
+    <td align=left valign=middle><h1>McIDAS-V User's Guide</h1></td>
   </tr>
 </table>
