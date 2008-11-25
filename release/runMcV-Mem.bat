89ebb2a7228f361c808da120740a44b49a43b14d
Dave Parker <davep@ssec.wisc.edu>
2008-11-25 17:47:52 +0000

    Move system memory detection to external script [0]

---
+++ release/runMcV-Mem.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+for /f "tokens=4,5" %%i in ('systeminfo 2^>NUL ^|find "Total Physical Memory"') do set SYS_MEM_IN=x%%ix && set SYS_MEM_TYPE=%%j
+
+:toint
+set SYS_MEM_IN=%SYS_MEM_IN:~1%
+set SYS_MEM_CHAR=%SYS_MEM_IN:~0,1%
+if "%SYS_MEM_CHAR%" == "x" goto gotint
+if "%SYS_MEM_CHAR%" == "," goto toint
+set /a SYS_MEM=%SYS_MEM%%SYS_MEM_CHAR%
+goto toint
+
+:gotint
+IF %SYS_MEM_TYPE% == GB SET /a SYS_MEM=%SYS_MEM% * 1024
--- release/runMcV.bat
+++ release/runMcV.bat
@@ -41,23 +41,11 @@ IF EXIST "%MCV_USERPATH%\runMcV-Prefs.bat" CALL "%MCV_USERPATH%\runMcV-Prefs.bat
 SET ENABLE_3D=true
 IF %USE_3DSTUFF%==0 SET ENABLE_3D=false
 
-SET MCV_FLAGS=-Didv.3d=%ENABLE_3D%
-
 REM Get the amount of system memory
-for /f "tokens=4,5" %%i in ('systeminfo 2^>NUL ^|find "Total Physical Memory"') do set SYS_MEM_IN=x%%ix && set SYS_MEM_TYPE=%%j
 set /a SYS_MEM=0
+IF EXIST runMcV-Mem.bat CALL runMcV-Mem.bat
 
-:toint
-set SYS_MEM_IN=%SYS_MEM_IN:~1%
-set SYS_MEM_CHAR=%SYS_MEM_IN:~0,1%
-if "%SYS_MEM_CHAR%" == "x" goto gotint
-if "%SYS_MEM_CHAR%" == "," goto toint
-set /a SYS_MEM=%SYS_MEM%%SYS_MEM_CHAR%
-goto toint
-
-:gotint
-IF %SYS_MEM_TYPE% == GB SET /a SYS_MEM=%SYS_MEM% * 1024
-SET MCV_FLAGS=%MCV_FLAGS% -Didv.sysmem=%SYS_MEM%
+SET MCV_FLAGS=-Didv.3d=%ENABLE_3D% -Didv.sysmem=%SYS_MEM%
 IF %SYS_MEM% LEQ 0 SET HEAP_SIZE=%HEAP_DEFAULT%
 
 REM Check for valid HEAP_SIZE
