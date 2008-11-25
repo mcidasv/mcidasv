adab3d4b33bca6552aec4774a73eb3a8d7b9e9b8
Dave Parker <davep@ssec.wisc.edu>
2008-11-25 22:19:18 +0000

    Incorporate available memory script into startup manager scripts [0]

--- release/runMcV-Config.bat
+++ release/runMcV-Config.bat
@@ -1,1 +1,7 @@
-jre\bin\java -jar startupmanager.jar %*
+@ECHO OFF
+
+REM Get the amount of system memory
+set /a SYS_MEM=0
+IF EXIST runMcV-Mem.bat CALL runMcV-Mem.bat
+
+jre\bin\java -jar startupmanager.jar -Didv.sysmem=%SYS_MEM% %*
