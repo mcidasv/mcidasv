3c461b89aa7e714f5f7d21a1088976a214f5ace2
Dave Parker <davep@ssec.wisc.edu>
2008-12-22 16:45:57 +0000

    Add SSEC copyright [0]

--- edu/wisc/ssec/mcidasv/control/Grid2DReadoutProbe.java
+++ edu/wisc/ssec/mcidasv/control/Grid2DReadoutProbe.java
@@ -1,3 +1,29 @@
+/*
+ * $Id$
+ *
+ * Copyright 2007-2008
+ * Space Science and Engineering Center (SSEC)
+ * University of Wisconsin - Madison,
+ * 1225 W. Dayton Street, Madison, WI 53706, USA
+ *
+ * http://www.ssec.wisc.edu/mcidas
+ *
+ * This file is part of McIDAS-V.
+ * 
+ * McIDAS-V is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * McIDAS-V is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses
+ */
+
 package edu.wisc.ssec.mcidasv.control;
 
 
@@ -56,7 +82,7 @@ import javax.swing.event.*;
  * void probePositionChanged (double x, double y);
  *
  * @author IDV development team
- * @version $Revision$Date: 2008/11/04 02:57:06 $
+ * @version $Revision$Date: 2008/12/05 21:48:52 $
  */
 public class Grid2DReadoutProbe extends GridDisplayControl {
 
