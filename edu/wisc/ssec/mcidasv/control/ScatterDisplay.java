1e812d5312634accaf83bc4e4ab06fc9cfdda672
Dave Parker <davep@ssec.wisc.edu>
2008-12-22 17:45:22 +0000

    Fixed badly formatted copyright [0]

--- edu/wisc/ssec/mcidasv/control/ScatterDisplay.java
+++ edu/wisc/ssec/mcidasv/control/ScatterDisplay.java
@@ -24,6 +24,7 @@
  * along with this program.  If not, see http://www.gnu.org/licenses
  */
 
+
 package edu.wisc.ssec.mcidasv.control;
 
 import edu.wisc.ssec.mcidasv.data.hydra.HydraRGBDisplayable;
