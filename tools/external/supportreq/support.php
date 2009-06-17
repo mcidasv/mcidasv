224e5890733cef691d8307c7ee4f8029cfc037d8
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2009-06-17 19:38:31 +0000

    some fixies to make the new web server happy (perhaps more, later) [0]

--- tools/external/supportreq/support.php
+++ tools/external/supportreq/support.php
@@ -1,8 +1,8 @@
 <?php
 require('class.phpmailer.php');
 
-$data = $GLOBALS['HTTP_POST_VARS']['form_data'];
-$files = $GLOBALS['HTTP_POST_FILES']['form_data'];
+$data = $_POST['form_data'];
+$files = $_FILES['form_data'];
 
 $mail = new PHPMailer();
 
@@ -19,26 +19,29 @@ if (($sendcc == false) || ($data['cc_user'] == "true")) {
   $mail->AddAddress($data['email'], $data['fromName']);
 }
 
-if ($files['name']['att_one'] != '')
+if (isset($files['name']['att_one']) && $files['name']['att_one'] != '') {
   $mail->AddAttachment($files['tmp_name']['att_one'], $files['name']['att_one'], 'base64', $files['type']['att_one']);
+}
 
-if ($files['name']['att_two'] != '')
+if (isset($files['name']['att_two']) && $files['name']['att_two'] != '') {
   $mail->AddAttachment($files['tmp_name']['att_two'], $files['name']['att_two'], 'base64', $files['type']['att_two']);
+}
 
-if ($files['name']['att_three'] != '')
+if (isset($files['name']['att_three']) && $files['name']['att_three'] != '') {
   $mail->AddAttachment($files['tmp_name']['att_three'], $files['name']['att_three'], 'base64', $files['type']['att_three']);
+}
 
-if ($files['name']['att_extra'] != '')
+if (isset($files['name']['att_extra']) && $files['name']['att_extra'] != '') {
   $mail->AddAttachment($files['tmp_name']['att_extra'], $files['name']['att_extra'], 'base64', $files['type']['att_extra']);
+}
 
-if ($files['name']['att_state'] != '')
+if (isset($files['name']['att_state']) && $files['name']['att_state'] != '') {
   $mail->AddAttachment($files['tmp_name']['att_state'], $files['name']['att_state'], 'base64', $files['type']['att_state']);
+}
 
-if ($files['name']['att_log'] != '')
+if (isset($files['name']['att_log']) && $files['name']['att_log'] != '') {
   $mail->AddAttachment($files['tmp_name']['att_log'], $files['name']['att_log'], 'base64', $files['type']['att_log']);
-
-// print_r($mail);
-// var_dump(isset($data['cc_user']));
+}
 
 if (!$mail->Send()) {
   echo "Message was not sent. :(<br/>\n";
