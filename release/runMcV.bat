00955451996e5a4088737f5bac5183421cf5c9ef
Dave Parker <davep@ssec.wisc.edu>
2009-07-16 14:49:35 +0000

    Remove double quotes around system name and memory in log file [0]

--- release/runMcV.bat
+++ release/runMcV.bat
@@ -117,8 +117,8 @@ REM Start McIDAS-V
 @echo ################ >>"%MCV_LOG%"
 date /t >>"%MCV_LOG%"
 time /t >>"%MCV_LOG%"
-@echo "%SYS_VER%" >>"%MCV_LOG%"
-@echo "%SYS_MEM% MB system memory" >>"%MCV_LOG%"
+@echo %SYS_VER% >>"%MCV_LOG%"
+@echo %SYS_MEM% MB system memory >>"%MCV_LOG%"
 @echo Command line: jre\bin\javaw.exe -Xmx%HEAP_SIZE% %D3D_FLAG% -cp idv.jar -jar mcidasv.jar %MCV_FLAGS% %MCV_PARAMS%
 
 start /B jre\bin\javaw.exe -Xmx%HEAP_SIZE% %D3D_FLAG% -cp idv.jar -jar mcidasv.jar %MCV_FLAGS% %MCV_PARAMS% >>"%MCV_LOG%" 2>&1
