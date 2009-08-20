beb41afe65322914703801f741b0e044d4ff76ee
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2009-08-20 18:30:10 +0000

    Partly understands Unidata's concept of resource collections. I'll start documenting soon, but here's an example of looking for all "action" tags that have id, image, description, and action attributes:
    import static edu.wisc.ssec.mcidasv.util.XPathUtils.elements;
    String query = "//action[@id and @image and @description and @action]";
    for (Element e : elements(getIdv(), IdvResourceManager.RSC_ACTIONS, query)) {
        System.err.println("y helo thar: "+e);
    }
    
    [0]

--- edu/wisc/ssec/mcidasv/util/XPathUtils.java
+++ edu/wisc/ssec/mcidasv/util/XPathUtils.java
@@ -1,8 +1,12 @@
 package edu.wisc.ssec.mcidasv.util;
 
-import java.util.HashMap;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,13 +20,32 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class XPathUtils {
+import ucar.unidata.idv.IdvResourceManager;
+import ucar.unidata.idv.IntegratedDataViewer;
+import ucar.unidata.idv.IdvResourceManager.XmlIdvResource;
+import ucar.unidata.util.ResourceCollection.Resource;
+import ucar.unidata.xml.XmlResourceCollection;
 
-    private static final Map<String, XPathExpression> pathMap = new HashMap<String, XPathExpression>();
+import edu.wisc.ssec.mcidasv.McIDASV;
+import edu.wisc.ssec.mcidasv.util.Contract;
 
+/**
+ * Documentation is still forthcoming, but remember that <b>no methods accept 
+ * {@code null} parameters!</b>
+ */
+public final class XPathUtils {
+
+    /** Maps (and caches) the XPath {@link String} to its compiled {@link XPathExpression}. */
+    private static final Map<String, XPathExpression> pathMap = new ConcurrentHashMap<String, XPathExpression>();
+
+    /**
+     * Thou shalt not create an instantiation of this class!
+     */
     private XPathUtils() {}
 
     public static XPathExpression expr(String xPath) {
+        Contract.notNull(xPath, "Cannot compile a null string");
+
         XPathExpression expr = pathMap.get(xPath);
         if (expr == null) {
             try {
@@ -35,7 +58,44 @@ public class XPathUtils {
         return expr;
     }
 
+    public static List<Node> eval(final XmlResourceCollection collection, final String xPath) {
+        Contract.notNull(collection, "Cannot search a null resource collection");
+        Contract.notNull(xPath, "Cannot search using a null XPath query");
+
+        try {
+            List<Node> nodeList = new ArrayList<Node>();
+            XPathExpression expression = expr(xPath);
+
+            // Resources are the only things added to the list returned by 
+            // getResources().
+            @SuppressWarnings("unchecked")
+            List<Resource> files = collection.getResources();
+
+            for (int i = 0; i < files.size(); i++) {
+                if (!collection.isValid(i))
+                    continue;
+
+                File f = new File(XPathUtils.class.getResource(files.get(i).toString()).toURI());
+                if (!f.exists() || !f.canRead())
+                    continue;
+
+                NodeList tmpList = (NodeList)expression.evaluate(loadXml(f), XPathConstants.NODESET);
+                for (int j = 0; j < tmpList.getLength(); j++) {
+                    nodeList.add(tmpList.item(j));
+                }
+            }
+            return nodeList;
+        } catch (XPathExpressionException e) {
+            throw new RuntimeException("Error evaluating xpath", e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Error loading file", e);
+        }
+    }
+
     public static NodeList eval(final String xmlFile, final String xPath) {
+        Contract.notNull(xmlFile, "Null path to a XML file");
+        Contract.notNull(xPath, "Cannot search using a null XPath query");
+
         try {
             return (NodeList)expr(xPath).evaluate(loadXml(xmlFile), XPathConstants.NODESET);
         } catch (XPathExpressionException e) {
@@ -44,6 +104,9 @@ public class XPathUtils {
     }
 
     public static NodeList eval(final Node root, final String xPath) {
+        Contract.notNull(root, "Cannot search a null root node");
+        Contract.notNull(xPath, "Cannot search using a null XPath query");
+
         try {
             return (NodeList)expr(xPath).evaluate(root, XPathConstants.NODESET);
         } catch (XPathExpressionException e) {
@@ -51,23 +114,79 @@ public class XPathUtils {
         }
     }
 
+    public static List<Node> nodes(final XmlIdvResource collectionId, final String xPath) {
+        Contract.notNull(collectionId);
+        Contract.notNull(xPath);
+
+        McIDASV mcv = McIDASV.getStaticMcv();
+        if (mcv == null)
+            throw new AssertionError("Could not get a valid reference to McIDASV!");
+
+        XmlResourceCollection collection = mcv.getResourceManager().getXmlResources(collectionId);
+        return nodes(collection, xPath);
+    }
+    
+    public static List<Node> nodes(final XmlResourceCollection collection, final String xPath) {
+        Contract.notNull(collection);
+        Contract.notNull(xPath);
+        return eval(collection, xPath);
+    }
+
     public static NodeListIterator nodes(final String xmlFile, final String xPath) {
+        Contract.notNull(xmlFile);
+        Contract.notNull(xPath);
         return new NodeListIterator(eval(xmlFile, xPath));
     }
 
+    public static NodeListIterator nodes(final Node root, final String xPath) {
+        Contract.notNull(root);
+        Contract.notNull(xPath);
+        return new NodeListIterator(eval(root, xPath));
+    }
+
     public static NodeListIterator nodes(final Node root) {
-        return new NodeListIterator(eval(root, "//*"));
+        Contract.notNull(root);
+        return nodes(root, "//*");
+    }
+
+    public static List<Element> elements(final IntegratedDataViewer idv, final XmlIdvResource collectionId, final String xPath) {
+        Contract.notNull(idv);
+        Contract.notNull(collectionId);
+        Contract.notNull(xPath);
+
+        XmlResourceCollection collection = idv.getResourceManager().getXmlResources(collectionId);
+        return elements(collection, xPath);
+    }
+
+    public static List<Element> elements(final XmlResourceCollection collection, final String xPath) {
+        Contract.notNull(collection);
+        Contract.notNull(xPath);
+        List<Element> elements = new ArrayList<Element>();
+        for (Node n : eval(collection, xPath))
+            elements.add((Element)n);
+        return elements;
     }
 
     public static ElementListIterator elements(final String xmlFile, final String xPath) {
+        Contract.notNull(xmlFile);
+        Contract.notNull(xPath);
         return new ElementListIterator(eval(xmlFile, xPath));
     }
 
     public static ElementListIterator elements(final Node root) {
-        return new ElementListIterator(eval(root, "//*"));
+        Contract.notNull(root);
+        return elements(root, "//*");
+    }
+
+    public static ElementListIterator elements(final Node root, final String xPath) {
+        Contract.notNull(root);
+        Contract.notNull(xPath);
+        return new ElementListIterator(eval(root, xPath));
     }
 
     public static Document loadXml(final String xmlFile) {
+        Contract.notNull(xmlFile);
+
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
         try {
@@ -78,11 +197,25 @@ public class XPathUtils {
         }
     }
 
+    public static Document loadXml(final File xmlFile) {
+        Contract.notNull(xmlFile);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(true);
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(xmlFile);
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading XML file: "+e.getMessage(), e);
+        }
+    }
+
     public static class NodeListIterator implements Iterable<Node>, Iterator<Node> {
         private final NodeList nodeList;
         private int index = 0;
 
         public NodeListIterator(final NodeList nodeList) {
+            Contract.notNull(nodeList);
             this.nodeList = nodeList;
         }
 
@@ -108,6 +241,7 @@ public class XPathUtils {
         private int index = 0;
 
         public ElementListIterator(final NodeList nodeList) {
+            Contract.notNull(nodeList);
             this.nodeList = nodeList;
         }
 
@@ -127,5 +261,4 @@ public class XPathUtils {
             throw new UnsupportedOperationException("not implemented");
         }
     }
-
 }
