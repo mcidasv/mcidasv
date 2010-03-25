6a30112593e303ac9601cb015e42a0682e020ed8
Tom Rink <rink@ssec.wisc.edu>
2010-03-25 21:41:56 +0000

    take out a print

--- edu/wisc/ssec/mcidasv/resources/python/utilities/combineRGB.py
+++ edu/wisc/ssec/mcidasv/resources/python/utilities/combineRGB.py
@@ -8,5 +8,4 @@ def mycombineRGB(red, green, blue):
   red=GridUtil.setParamType(red,makeRealType("redimage%d" % uniqueID), 0)
   green=GridUtil.setParamType(green,makeRealType("greenimage%d" % uniqueID),0)
   blue=GridUtil.setParamType(blue,makeRealType("blueimage%d" % uniqueID),0)
-  print 'mycombineRGB'
   return FieldImpl.combine([red,green,blue], FieldImpl.NEAREST_NEIGHBOR, FieldImpl.NO_ERRORS, 0, 0)
