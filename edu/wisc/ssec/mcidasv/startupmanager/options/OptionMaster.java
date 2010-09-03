4af6227f53748d44cff5159d724d459c1f56dbf9
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2010-09-03 16:59:50 +0000

    image by reference defaults to enabled now [453]

--- edu/wisc/ssec/mcidasv/startupmanager/options/OptionMaster.java
+++ edu/wisc/ssec/mcidasv/startupmanager/options/OptionMaster.java
@@ -65,7 +65,7 @@ public enum OptionMaster {
         { "D3DREND", "  Enable Direct3D", "", Type.BOOLEAN, OptionPlatform.WINDOWS, Visibility.VISIBLE },
         // mcidasv enables this (the actual property is "visad.java3d.geometryByRef")
         // by default in mcidasv.properties.
-        { "USE_IMAGEBYREF", "Enable access to image data by reference", "0", Type.BOOLEAN, OptionPlatform.ALL, Visibility.VISIBLE },
+        { "USE_IMAGEBYREF", "Enable access to image data by reference", "1", Type.BOOLEAN, OptionPlatform.ALL, Visibility.VISIBLE },
         { "USE_NPOT", "Enable Non-Power of Two (NPOT) textures", "0", Type.BOOLEAN, OptionPlatform.ALL, Visibility.VISIBLE },
         { "USE_GEOBYREF", "Enable geometry by reference", "1", Type.BOOLEAN, OptionPlatform.ALL, Visibility.VISIBLE },
         // temp bandaid for people suffering from permgen problems.
