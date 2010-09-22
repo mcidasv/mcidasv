77545326219c32336a21fca891da30db975bf6be
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2010-09-22 22:19:34 +0000

    slight modifications for working outside of the expected jython "context" [126]

--- edu/wisc/ssec/mcidasv/resources/python/linearcombo/hydra.py
+++ edu/wisc/ssec/mcidasv/resources/python/linearcombo/hydra.py
@@ -7,7 +7,6 @@ import os
 _cwd = os.getcwd()
 _mcv_jar = os.path.join(_cwd, 'mcidasv.jar')
 _idv_jar = os.path.join(_cwd, 'idv.jar')
-_user_python = os.path.join(_idv.getStore().getUserDirectory().toString(), 'python')
 
 # however, if we're not inside a jar just prepend the current directory.
 if not os.path.exists(_mcv_jar):
@@ -18,13 +17,36 @@ if not os.path.exists(_idv_jar):
     _idv_jar = os.path.join(_cwd, '../idv')
 
 # NOTE: the paths appended to sys.path cannot have a trailing '/'!
-_linear_combo = _mcv_jar+'/edu/wisc/ssec/mcidasv/resources/python/linearcombo'
-_mcv_python = _mcv_jar+'/edu/wisc/ssec/mcidasv/resources/python'
-_idv_python = _idv_jar+'/ucar/unidata/idv/resources/python'
+sys.path.append(_mcv_jar+'/edu/wisc/ssec/mcidasv/resources/python/linearcombo')
+sys.path.append(_mcv_jar+'/edu/wisc/ssec/mcidasv/resources/python')
+sys.path.append(_idv_jar+'/ucar/unidata/idv/resources/python')
 
-sys.path.append(_linear_combo)
-sys.path.append(_mcv_python)
-sys.path.append(_idv_python)
+if '_idv' in dir():
+    _user_python = os.path.join(_idv.getStore().getUserDirectory().toString(), 'python')
+    if os.path.exists(_user_python):
+        sys.path.append(_user_python)
+        for mod in os.listdir(_user_python):
+            modname, ext = os.path.splitext(mod)
+            if ext == '.py':
+                globals()[modname] = __import__(modname, globals(), locals(), ['*'], -1)
+            del modname, ext
+    del _user_python
+
+del _cwd, _mcv_jar, _idv_jar
+
+if '_linearCombo' in dir():
+    # maps band names to wavenumbers (string -> float)
+    _namedBands = _linearCombo.getBandNameMappings()
+
+    # initial wavenumber changes depending upon the type of data
+    _initWavenumber = _linearCombo.getInitialWavenumber()
+else:
+    _namedBands = {}
+
+    # from edu.wisc.ssec.mcidasv.data.hydra.MultiSpectralData
+    _initWavenumber = 919.5
+
+# end of dirty tricks section
 
 import colorutils
 import imageFilters as filters
@@ -32,22 +54,6 @@ import shell as idvutils
 
 from edu.wisc.ssec.mcidasv.control.LinearCombo import Selector
 from edu.wisc.ssec.mcidasv.data.hydra import MultiSpectralData
-
-if os.path.exists(_user_python):
-    sys.path.append(_user_python)
-    for mod in os.listdir(_user_python):
-        modname, ext = os.path.splitext(mod)
-        if ext == '.py':
-            globals()[modname] = __import__(modname, globals(), locals(), ['*'], -1)
-        del modname, ext
-
-del _cwd, _mcv_jar, _idv_jar, _user_python, _linear_combo, _mcv_python, _idv_python
-
-# maps band names to wavenumbers (string -> float)
-_namedBands = _linearCombo.getBandNameMappings()
-
-# initial wavenumber changes depending upon the type of data
-_initWavenumber = _linearCombo.getInitialWavenumber()
 
 # maps keyword parameters to a list of valid aliases (string -> [string])
 # needs some work :(
