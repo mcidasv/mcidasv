9dc7833b21fd66b5628c863bdccd319626b36343
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2010-10-01 15:48:00 +0000

    noting all of the stuff that the idv shell *currently* places into the main namespace [0]

--- edu/wisc/ssec/mcidasv/resources/python/utilities/interactive.py
+++ edu/wisc/ssec/mcidasv/resources/python/utilities/interactive.py
@@ -1,3 +1,58 @@
 def dump_active_display():
     pass
 
+# all the junk that *currently* gets automatically placed into the namespace 
+# by the idv shell:
+# 
+# ArrayList ConstantMap Data DataChoice DataSelection DataUtil 
+# DerivedGridFactory Display EARTH_RADIUS EntryStore FFFffff FieldImpl 
+# FileOutputStream GRAVITY GeoLocationInfo GeoSelection GridMath GridUtil 
+# Integer JyVars MapMaker MultiSpectralData RemoteAddeEntry Selector 
+# StreamResult StreamSource StringUtil TWO_OMEGA Transformer 
+# TransformerFactory Util _KWARG_ALIASES __doc__ __name__ _cnames _color_cache 
+# _convertHexColor _convertNamedColor _convertRgbSeq _extract_kwarg _idv 
+# _initWavenumber _myip _namedBands _s abs abs_data acos acosDegrees add adv 
+# age annotate applyFunctionToValuesInField applyFunctionToValuesInRange 
+# applyToIndices applyToRange applyToRangeValues apply_colorbar array asin 
+# asinDegrees atan atan2 atan2Degrees atanDegrees atn2 averageFromMap 
+# averageFromMapAndClip averageOverTime avg avor bands ceil chol cleanFilter 
+# clear clearplot cloneArray cloudFilter collect_garbage colorutils combine 
+# combineFields combineRGB cond contrast convertColor coreFilter corl cos 
+# cosDegrees createDisplay cros datamanager ddx ddy default defr det 
+# differenceFromBaseTime dirn dirr disableAddeDebug discriminateFilter div 
+# domainDimension domainFactor domainType dot dumpType dumpTypes 
+# dump_active_display dvdx dvdy eig enableAddeDebug evaluate exp 
+# exportGridToNetcdf extract extractLatitudeFromNWPGrid 
+# extractPressureFromNWPGrid fft field filterMaps filters find findDataSource 
+# findDisplayControl floor frnt geo geolocate getAltitude getData 
+# getDataChoice getDomain getDomainDimension getDomainSet getDomainSizes 
+# getDomainType getImage getMapProperty getMapsWithProperty getMinMax 
+# getNetcdfData getNthTimeGrid getPointDataAdapter getRangeDimension 
+# getRangeType getRealType getScale getSliceAtAltitude getSliceAtLevel getType 
+# getValue getValues getValuesLengths grad gradientFilter gwfs gwfv 
+# highPass2DFilter hist holeFilter horizontalAdvection horizontalDivergence 
+# idv idveval idvutils ifft inad installmanager interpreter inverse 
+# islInterpreter java jcbn lap latr lav layerAverage layerDiff ldf listVars 
+# load loadBundle load_enhancement load_map log lowPass2DFilter lu mag make2D 
+# make3DMap makeDataSource makeDomain makeField makeFieldFromMapBounds 
+# makeFloatArray makeFlowField makeNavigatedImage makePairedLines makeRealType 
+# makeTimeSequence makeTopographyFromField makeTrueVector makeType makeUnit 
+# makeVector mapsAbsoluteValue mapsApplyToField mapsApplyToRange mapsAverage 
+# mapsMax mapsMin mapsSetValue mask maskGrid matrixMultiply matteImage 
+# maxOverTime max_data mergeFilter mergeTimeSequences minOverTime min_data 
+# mixr mod modify mul myCloudzz mycombineRGB newName newUnit noUnit norm1 
+# norm2 normF normInf oldaverageOverTime os passFilter pause plot 
+# printSounding printSoundings printType printValueDiff pvor qr quo qvcl qvec 
+# rangeDimension rangeType rank recenter relh replace replaceFilter 
+# replaceMissing resample resampleGrid rescale resizeImage rint round 
+# saveNetcdf saveplot savg savs sdiv selectData selectDataChoice selector 
+# setAxesScalesFont setAxesScalesLabel setDataChoices setDataSources setDebug 
+# setOffScreen setValues shell shotFilter showAxesScales showLib shr sin 
+# sinDegrees sm5s sm5v sm9s sm9v solve spotFilter sqrt strd sub subsetFromMap 
+# subsetRangeFromMap subsetRangeWithProperty subsetWithProperty substitute 
+# sumFromBaseTime sumOverTime svd sys tan tanDegrees testIndexToLatLon 
+# testIndexToLatLon2 thrm thta thte timeStepDifference timeStepSum tmpBundle 
+# tmpOut tmpXsl trace transform transpose un uniqueID ur vadd vecn vecr 
+# verifyServer vlav vldf vmul vn vor vquo vr vsub windShear windShearVector 
+# writeGridToXls writeImage writeImageToFile writeMovie write_image wshr xav 
+# xsum yav ysum
