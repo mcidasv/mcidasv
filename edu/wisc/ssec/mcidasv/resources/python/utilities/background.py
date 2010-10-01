2c4a27b3266a260c51bc763aa092055d363d82e8
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2010-10-01 15:30:42 +0000

    function stubs [845]

---
+++ edu/wisc/ssec/mcidasv/resources/python/utilities/background.py
@@ -0,0 +1,22 @@
+def load_enhancement(name=''):
+    pass
+
+def load_map(name=''):
+    pass
+
+def annotate(text=''):
+    pass
+
+def apply_colorbar(name=''):
+    pass
+
+def write_image(path=''):
+    pass
+
+def collect_garbage():
+    pass
+
+def recenter(*args, **kwargs):
+    pass
+
+
