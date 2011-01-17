f6655d6a5f7a45154b1846de4d7cb6817790704e
Tom Rink <rink@ssec.wisc.edu>
2011-01-17 21:21:57 +0000

    initial commit

---
+++ edu/wisc/ssec/mcidasv/control/NearCastTrajControl.java
@@ -0,0 +1,160 @@
+package edu.wisc.ssec.mcidasv.control;
+
+import ucar.unidata.data.DataChoice;
+import ucar.unidata.data.DataDataChoice;
+import ucar.unidata.data.DirectDataChoice;
+import ucar.unidata.data.DataSelection;
+import ucar.unidata.data.DataCategory;
+
+import ucar.unidata.data.grid.GridUtil;
+
+import ucar.unidata.idv.DisplayConventions;
+import ucar.unidata.idv.IntegratedDataViewer;
+import ucar.unidata.idv.control.DisplayControlImpl;
+import ucar.unidata.idv.DisplayControl;
+
+import ucar.visad.display.DisplayMaster;
+import ucar.visad.display.DisplayableData;
+import ucar.visad.display.Displayable;
+import ucar.visad.display.LineDrawing;
+import ucar.visad.display.RGBDisplayable;
+
+import edu.wisc.ssec.mcidasv.data.hydra.HydraRGBDisplayable;
+import edu.wisc.ssec.mcidasv.data.NearCastTrajDataSource;
+
+import visad.*;
+import visad.BaseColorControl;
+import visad.VisADException;
+import visad.RemoteVisADException;
+import visad.ScalarMapListener;
+import visad.ScalarMapControlEvent;
+import visad.ScalarMapEvent;
+import visad.ReferenceException;
+import visad.georef.MapProjection;
+import visad.georef.TrivialMapProjection;
+
+
+import ucar.unidata.idv.ViewManager;
+import ucar.unidata.idv.ViewDescriptor;
+
+import ucar.unidata.util.ColorTable;
+import ucar.unidata.util.Range;
+import ucar.unidata.util.Misc;
+import ucar.unidata.util.GuiUtils;
+
+import ucar.unidata.util.LogUtil;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;
+import java.awt.geom.Rectangle2D;
+
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
+import java.awt.Color;
+import java.awt.Font;
+
+
+import java.rmi.RemoteException;
+
+
+public class NearCastTrajControl extends DisplayControlImpl {
+
+   /** Displayable for the data */
+   LineDrawing trajDisplay;
+
+   private DisplayMaster displayMaster;
+
+   FieldImpl trajField = null;
+
+   RealType rgbRealType = null;
+   String paramName = null;
+
+   NearCastTrajDataSource dataSource;
+
+
+   public NearCastTrajControl() {
+     super();
+     setAttributeFlags(FLAG_COLORTABLE);
+   }
+
+   public boolean init(DataChoice dataChoice) throws VisADException, RemoteException {
+     displayMaster = getViewManager().getMaster();
+     dataSource = (NearCastTrajDataSource) ((DirectDataChoice) dataChoice).getDataSource();
+
+     DataSelection dataSelection = getDataSelection();
+     trajField = (FieldImpl) dataChoice.getData(dataSelection);
+
+     FlatField fltFld = null;
+     if (GridUtil.isSequence(trajField)) {
+       FieldImpl fld = (FieldImpl) trajField.getSample(0);
+       if (GridUtil.isSequence(fld)) {
+         fltFld = (FlatField)  fld.getSample(0);
+       }
+     }
+     else {
+       fltFld = (FlatField) trajField;
+     }
+     rgbRealType = (RealType) ((FunctionType)fltFld.getType()).getRange();
+     paramName = rgbRealType.getName();
+
+     RGBDisplayableImpl rgbDisp = new RGBDisplayableImpl("traj", rgbRealType, null, true);
+     rgbDisp.setData(trajField);
+
+     addDisplayable(rgbDisp, FLAG_COLORTABLE);
+
+     return true;
+   }
+
+   public void initDone() {
+   }
+
+   protected Range getInitialRange() throws VisADException, RemoteException {
+     return getDisplayConventions().getParamRange(paramName, null);
+   }
+
+   /**
+   protected ColorTable getInitialColorTable() {
+     return getDisplayConventions().getParamColorTable("image");
+   }
+   */
+
+    private class RGBDisplayableImpl extends RGBDisplayable {
+       RGBDisplayableImpl(String name, RealType rgbRealType, float[][] colorPalette, boolean alphaflag)
+           throws VisADException, RemoteException {
+         super(name, rgbRealType, colorPalette, alphaflag);
+       }
+    }
+
+    @Override public MapProjection getDataProjection() {
+        MapProjection mp = null;
+        Range lonRange = dataSource.getLonRange();
+        Range latRange = dataSource.getLatRange();
+        float delLon = (float) (lonRange.getMax() - lonRange.getMin());
+        float delLat = (float) (latRange.getMax() - latRange.getMin());
+
+        try {
+           mp = new TrivialMapProjection(RealTupleType.SpatialEarth2DTuple, 
+                   new Rectangle2D.Float((float)lonRange.getMin(), (float)latRange.getMin(), 
+                                         delLon, delLat));
+        } catch (Exception e) {
+            logException("MultiSpectralControl.getDataProjection", e);
+        }
+
+        return mp;
+    }
+
+
+}
---
+++ edu/wisc/ssec/mcidasv/data/NearCastTrajDataSource.java
@@ -0,0 +1,262 @@
+package edu.wisc.ssec.mcidasv.data;
+
+
+import edu.wisc.ssec.mcidasv.data.hydra.NetCDFFile;
+import visad.Data;
+import visad.Unit;
+import visad.Gridded3DSet;
+import visad.Gridded1DDoubleSet;
+import visad.Gridded1DSet;
+import visad.Set;
+import visad.UnionSet;
+import visad.RealTupleType;
+import visad.RealType;
+import visad.FlatField;
+import visad.FieldImpl;
+import visad.Integer1DSet;
+import visad.FunctionType;
+import visad.CoordinateSystem;
+import visad.VisADException;
+import visad.DateTime;
+import visad.data.units.Parser;
+import visad.data.units.ParseException;
+
+import java.rmi.RemoteException;
+import java.util.Hashtable;
+import java.util.List;
+
+import ucar.unidata.util.Misc;
+import ucar.unidata.util.Range;
+
+import ucar.unidata.data.DataUtil;
+import ucar.unidata.data.DataChoice;
+import ucar.unidata.data.DataDataChoice;
+import ucar.unidata.data.DirectDataChoice;
+import ucar.unidata.data.DataSelection;
+import ucar.unidata.data.DataCategory;
+import ucar.unidata.data.DataSourceImpl;
+import ucar.unidata.data.DataSourceDescriptor;
+
+
+
+public class NearCastTrajDataSource extends DataSourceImpl {
+
+    private static final String DATA_DESCRIPTION = "NearCastTrajectory";
+
+    public static String parcelDimName = "parcel";
+    public static String timeDimName = "times";
+    public static String lonName = "lon";
+    public static String latName = "lat";
+
+    NetCDFFile ncFile = null;
+    String pressName = "pres";
+    String timeName = "times";
+
+
+    int[] start = new int[2];
+    int[] count = new int[2];
+    int[] stride = new int[2];
+
+
+    int parcelDimIdx = 0;
+    int timeDimIdx = 1;
+
+    String[] paramList = null;
+    
+    String fileName = null;
+
+    CoordinateSystem presToHeightCS = null;
+
+    Unit timeUnit;
+    Set timeSet;
+
+    int numTimes;
+    int numParcels;
+
+    Range lonRange = new Range();
+    Range latRange = new Range();
+
+    public NearCastTrajDataSource() {
+    }
+
+    public NearCastTrajDataSource(String filename) throws VisADException {
+      this(null, Misc.newList(filename), null);
+    }
+
+    public NearCastTrajDataSource(DataSourceDescriptor descriptor,
+                                 String fileName, Hashtable properties) throws VisADException { 
+        this(descriptor, Misc.newList(fileName), properties);
+    }
+
+    public NearCastTrajDataSource(DataSourceDescriptor descriptor,
+                                 List newSources, Hashtable properties) throws VisADException {
+
+       super(descriptor, DATA_DESCRIPTION, DATA_DESCRIPTION, properties);
+
+       presToHeightCS = DataUtil.getPressureToHeightCS(DataUtil.STD_ATMOSPHERE);
+
+       fileName = (String) newSources.get(0);
+
+       try {
+         ncFile = new NetCDFFile(fileName);
+       }
+       catch (Exception e) {
+         e.printStackTrace();
+       }
+         
+       paramList = new String[] {"temp", "q", "the", "tp", "MS", "MQ", "MTe", "TP"};
+
+
+       String unitStr = ncFile.getArrayUnitString(timeName);
+       try {
+         timeUnit = Parser.parse(unitStr);
+       } 
+       catch (ParseException e) {
+         System.out.println(e);
+       }
+
+       numTimes = ncFile.getDimensionLength(timeDimName);
+       numParcels = ncFile.getDimensionLength(parcelDimName);
+
+       try {
+         Class type = ncFile.getArrayType(timeName);
+         if (type == Double.TYPE) {
+            double[] timeValues = ncFile.getDoubleArray(timeName, new int[] {0}, new int[] {numTimes}, new int[] {1});
+            timeSet = new Gridded1DDoubleSet(
+                       RealType.Time, new double[][] {timeValues}, numTimes, null, new Unit[] {timeUnit}, null);
+         }
+         else if (type == Float.TYPE) {
+            float[] timeValues = ncFile.getFloatArray(timeName, new int[] {0}, new int[] {numTimes}, new int[] {1});
+            timeSet = new Gridded1DSet(RealType.Time, new float[][] {timeValues}, numTimes, null, new Unit[] {timeUnit}, null);
+
+         }
+       } 
+       catch (Exception e) {
+         e.printStackTrace();
+       }
+    }
+
+
+    public FlatField[] createVisADData(String paramName) {
+      return null;
+    }
+
+    public FlatField singleTraj(String paramName, int parcelIndex, int timeStart, int timeCount, int timeStride) throws Exception {
+
+      start[parcelDimIdx] = parcelIndex;
+      start[timeDimIdx] = timeStart;
+
+      count[parcelDimIdx] = 1;
+      count[timeDimIdx] = timeCount;
+
+      stride[parcelDimIdx] = 1;
+      stride[timeDimIdx] = 1;
+
+      float[] lons = ncFile.getFloatArray(lonName, start, count, stride);
+      float[] lats = ncFile.getFloatArray(latName, start, count, stride);
+
+      float[] minmax = minmax(lons, (float)lonRange.getMin(), (float)lonRange.getMax());
+      lonRange.setMin(minmax[0]);
+      lonRange.setMax(minmax[1]);
+      minmax = minmax(lats, (float)latRange.getMin(), (float)latRange.getMax());
+      latRange.setMin(minmax[0]);
+      latRange.setMax(minmax[1]);
+
+      float[] pres = ncFile.getFloatArray(pressName, start, count, stride);
+      float[] param = ncFile.getFloatArray(paramName, start, count, stride);
+
+      float[] alt = (presToHeightCS.toReference(new float[][] {pres}))[0];
+ 
+      float[][] trajCoords = new float[][] {lons, lats, alt};
+
+      Gridded3DSet domain = new Gridded3DSet(RealTupleType.SpatialEarth3DTuple, trajCoords, trajCoords[0].length);
+
+      FunctionType fncType = new FunctionType(RealTupleType.SpatialEarth3DTuple, RealType.getRealType(paramName));
+      FlatField traj = new FlatField(fncType, domain);
+      traj.setSamples(new float[][] {param}, false);
+ 
+      return traj;
+    }
+
+    /**
+     * Make and insert the <code>DataChoice</code>-s for this
+     * <code>DataSource</code>.
+     */
+    public void doMakeDataChoices() {
+        try {
+          for (int k=0; k<paramList.length; k++) {
+            DataChoice choice = doMakeDataChoice(k);
+            if (choice != null) {
+	      addDataChoice(choice);
+	    }
+          }
+        }
+        catch(Exception e) {
+          e.printStackTrace();
+        }
+    }
+
+    private DataChoice doMakeDataChoice(int idx) throws Exception {
+        String name = paramList[idx];
+	DirectDataChoice ddc = null;
+	if (ncFile.hasArray(name)) {
+           ddc = new DirectDataChoice(this, new Integer(idx), name, name, null, new Hashtable());
+	}
+        return ddc;
+    }
+
+    protected Data getDataInner(DataChoice dataChoice, DataCategory category,
+                                DataSelection dataSelection,
+                                Hashtable requestProperties)
+            throws VisADException, RemoteException {
+
+        String paramName = dataChoice.getName();
+
+        FieldImpl trajField = new FieldImpl(
+                      new FunctionType(RealType.Generic, new FunctionType(RealTupleType.SpatialEarth3DTuple, RealType.getRealType(paramName))), new Integer1DSet(numParcels));
+
+        FieldImpl trajTimeField = new FieldImpl(new FunctionType(RealType.Time, trajField.getType()), timeSet); 
+
+        lonRange.setMin(Float.MAX_VALUE);
+        lonRange.setMax(-Float.MAX_VALUE);
+        latRange.setMin(Float.MAX_VALUE);
+        latRange.setMax(-Float.MAX_VALUE);
+
+        try {
+          for (int t=0; t<numTimes; t++) {
+             trajField = new FieldImpl(
+                      new FunctionType(RealType.Generic, new FunctionType(RealTupleType.SpatialEarth3DTuple, RealType.getRealType(paramName))), new Integer1DSet(numParcels));
+            for (int k=0; k<numParcels/4; k++) {
+               FlatField fld = singleTraj(paramName, k*4, 0, t+1, 1);
+               trajField.setSample(k, fld);
+            }
+            trajTimeField.setSample(t, trajField);
+          }
+          return trajTimeField;
+        }
+        catch (Exception e) {
+          e.printStackTrace();
+          return null;
+        }
+    }
+
+    public static float[] minmax(float[] values, float min, float max) {
+      for (int k = 0; k < values.length; k++) {
+        float val = values[k];
+        if ((val == val) && (val < Float.POSITIVE_INFINITY) && (val > Float.NEGATIVE_INFINITY)) {
+          if (val < min) min = val;
+          if (val > max) max = val;
+        }
+      }
+      return new float[] {min, max};
+    }
+
+   public Range getLonRange() {
+     return lonRange;
+   }
+
+   public Range getLatRange() {
+     return latRange;
+   }
+
+}
