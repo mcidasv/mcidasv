10c24bb77208fa00fc7157129da04c47aeaf49e7
Dave Parker <davep@ssec.wisc.edu>
2011-06-23 20:12:29 +0000

    Optionally make backup before getting new IDV libs [0]

--- tools/get_idv_latest.sh
+++ tools/get_idv_latest.sh
@@ -5,9 +5,21 @@
 #
 
 DEST_DIR=/home/mcidasv/idv/lib
+BACK_DIR=/home/mcidasv/idv/lib.old
 URL=ftp://ftp.unidata.ucar.edu/pub/idv/nightly
 FILE=`curl -ls ${URL}/ |grep idv_jars |xargs echo`
 
+if [ -d ${BACK_DIR} ]; then
+	echo "Old backup of IDV libraries exists."
+	echo -n "Overwrite with current copy? [y|N] "
+	read CONTYN
+	if [ "${CONTYN}" = "y" -o "${CONTYN}" = "Y" ]; then
+		echo "Making new backup of current IDV libraries..."
+		rm -Rf ${BACK_DIR} && mv ${DEST_DIR} ${BACK_DIR} && mkdir ${DEST_DIR}
+	fi
+fi
+
+echo "Getting new IDV nightly libraries..."
 cd $DEST_DIR
 wget ${URL}/${FILE} && unzip -o ${FILE} && rm -f ${FILE}
 cd -
