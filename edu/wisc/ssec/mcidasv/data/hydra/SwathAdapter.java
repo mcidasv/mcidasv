e471b8899f119805fdbe710d85c807dff2291f3d
Tom Rink <rink@ssec.wisc.edu>
2012-03-29 20:21:29 +0000

    add a metadata key to indicate which array dimensions matches the multiScale array

--- edu/wisc/ssec/mcidasv/data/hydra/SwathAdapter.java
+++ edu/wisc/ssec/mcidasv/data/hydra/SwathAdapter.java
@@ -72,6 +72,7 @@ public class SwathAdapter extends MultiDimensionAdapter {
       static String geo_scale_name = "geo_scale_name";
       static String geo_offset_name = "geo_scale_name";
       static String geo_fillValue_name = "geo_fillValue_name";
+      static String multiScaleDimensionIndex = "multiScaleDimensionIndex";
 
       String[] rangeName_s  = null;
       Class[] arrayType_s = null;
@@ -136,6 +137,7 @@ public class SwathAdapter extends MultiDimensionAdapter {
     	  metadata.put(geo_xtrack_offset_name, null);
     	  metadata.put(geo_track_skip_name, null);
     	  metadata.put(geo_xtrack_skip_name, null);
+          metadata.put(multiScaleDimensionIndex, null);
     	  return metadata;
       }
 
