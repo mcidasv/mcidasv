4750db8548013b22105b963708cce963b591414f
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2012-12-11 13:40:27 -0600

    Fixed paths. [0]

--- tools/pack_source.sh
+++ tools/pack_source.sh
@@ -15,6 +15,7 @@
 IDV_DIR="../../IDV"
 MCV_DIR="../../mcidasv"
 MEDIA_DIR="../../install4j/media"
+CUR_DIR="${PWD}"
 
 # Get the version
 VERSION=$(cat ${MCV_DIR}/release/mcidasv.install4j  |\
@@ -41,21 +42,22 @@ cp -R ${IDV_DIR}/lib ./IDV
 
 # Get McIDAS-V source
 echo "Copying McIDAS-V source..."
-mkdir ./mc-v
-cp ${MCV_DIR}/build.xml ./mcidasv
+mkdir ./mcidasv
+cp ${MCV_DIR}/build.xml ./mcidasv/
 for SUBDIR in docs edu lib mcidas release lib ucar; do
-	echo "  ${SUBDIR}" && cp -R ${MCV_DIR}/${SUBDIR} ./mc-v
+	echo "  ${SUBDIR}" && cp -R ${MCV_DIR}/${SUBDIR} ./mcidasv/
 done
 
 # Clean it up
 echo "Removing .class files"
 find . -name "*.class" |xargs rm -f
-echo "Removing CVS entries"
-find . -name CVS -type d |xargs rm -Rf
+
 echo "Removing javadoc"
 rm -Rf ./mcidasv/docs/javadoc
+
 echo "Removing webstart"
 rm -Rf ./mcidasv/release/webstart
+
 echo "Removing PDF guide"
 rm -f ./mcidasv/docs/userguide/processed/mcv_guide.pdf
 
@@ -65,6 +67,9 @@ cd ..
 rm -f ${NAME}.zip && zip -r ${NAME}.zip ${NAME}
 echo "Created $PWD/${NAME}.zip"
 
+echo "Returning to original directory: ${CUR_DIR}"
+cd ${CUR_DIR}
+
 # Copy it to the install4j/media directory
 echo "Copying to ${MEDIA_DIR}"
-cp ${NAME}.zip ${MEDIA_DIR}
+cp "../../source/${NAME}.zip" "${MEDIA_DIR}"
