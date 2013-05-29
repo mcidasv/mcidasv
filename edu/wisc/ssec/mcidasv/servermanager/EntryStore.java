2552c7afb44a5738e3ee5562af45b22badb5036a
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2013-05-29 13:57:14 -0500

    Force RESOLV.SRV to overwrite itself after init [1303]
    Migrating from 1.2 -> 1.3 introduced a problem with RESOLV.SRV not
    having descriptors calculated using the results of the "isTemporary"
    field. This commit will simply update RESOLV.SRV with the new
    descriptors immediately.

--- edu/wisc/ssec/mcidasv/servermanager/EntryStore.java
+++ edu/wisc/ssec/mcidasv/servermanager/EntryStore.java
@@ -192,6 +192,7 @@ public class EntryStore {
         putEntries(trie, prefEntries);
         putEntries(trie, userEntries);
         putEntries(trie, systemEntries);
+        saveEntries();
     }
 
     /**
