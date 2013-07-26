7808e505a6cbf454f010c3126537bd5011aa5c1b
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2013-07-26 09:56:53 -0500

    Back out of makeDataSource commit. [1303]
    It'll call the IDV's makeDataSource…and that needs (at least) its "idv"
    variable defined by the IDV's implementation of
    JythonManager.initBasicInterpreter.

--- edu/wisc/ssec/mcidasv/resources/python/background.py
+++ edu/wisc/ssec/mcidasv/resources/python/background.py
@@ -8,7 +8,6 @@ import ucar.unidata.util.Range
 
 from contextlib import contextmanager
 
-from shell import makeDataSource
 from decorators import deprecated
 from decorators import gui_invoke_later
 from decorators import gui_invoke_now
