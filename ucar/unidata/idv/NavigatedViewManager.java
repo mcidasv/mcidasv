5010ef7e9ad7edb8f39c28a8ace67260b0c075a2
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2013-08-01 16:01:40 -0500

    Refactor initReadout for component group compatibility.
    This reworks the logic within initReadout so that setReadoutFormat is
    always called, even if the IdvWindow passed to initReadout is null (as
    seems to be the case when dealing with component groups).
    
    Always calling setReadoutFormat like this is safe because it always
    does an internal check for whether or not the readout field is null.
    
    [1520]

--- ucar/unidata/idv/NavigatedViewManager.java
+++ ucar/unidata/idv/NavigatedViewManager.java
@@ -362,12 +362,10 @@ public abstract class NavigatedViewManager extends ViewManager {
      * @param w window for readout
      */
     private void initReadout(IdvWindow w) {
-        if ((w != null) && (readout != null)) {
-
-            // GuiUtils.setFixedWidthFont(w.getMsgLabel());
+        if (w != null) {
             readout.setValueDisplay(w.getMsgLabel());
-            setReadoutFormat();
         }
+        setReadoutFormat();
     }
 
     /**
