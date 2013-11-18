ac3506fc48b8d811df63102cfc7acb737e511470
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2013-11-18 10:45:03 -0600

    Fix mcv.userpath directory for other runMcV scripts.
    [1650]

--- release/stdout.bat
+++ release/stdout.bat
@@ -67,7 +67,7 @@ SET D3D_FLAG=
 
 REM Show the welcome window if needed
 if "%SHOW_WELCOME%"=="1" (
-jre\bin\javaw.exe -cp mcidasv.jar edu.wisc.ssec.mcidasv.util.WelcomeWindow
+jre\bin\javaw.exe -Dmcv.userpath="%MCV_USERPATH%" -cp mcidasv.jar edu.wisc.ssec.mcidasv.util.WelcomeWindow
 if ERRORLEVEL 1 GOTO end
 )
 
@@ -98,14 +98,14 @@ SET ALLOW_NPOT=false
 ) ELSE (
 SET ALLOW_NPOT=true
 )
-SET LOGBACK_CONFIG="%MCV_USERPATH%/logback.xml"
+SET LOGBACK_CONFIG="%MCV_USERPATH%\logback.xml"
 
 REM Get the amount of system memorys
 echo Reading system configuration...
 SET SYS_VER=Unknown
-FOR /F "tokens=*" %%i IN ('jre\bin\java.exe -cp mcidasv.jar edu.wisc.ssec.mcidasv.util.GetVer 2^>NUL') DO SET SYS_VER=%%i
+FOR /F "tokens=*" %%i IN ('jre\bin\java.exe -Dmcv.userpath="%MCV_USERPATH%" -cp mcidasv.jar edu.wisc.ssec.mcidasv.util.GetVer 2^>NUL') DO SET SYS_VER=%%i
 SET /a SYS_MEM=0
-FOR /F %%i IN ('jre\bin\java.exe -cp mcidasv.jar edu.wisc.ssec.mcidasv.util.GetMem 2^>NUL') DO SET SYS_MEM=%%i
+FOR /F %%i IN ('jre\bin\java.exe -Dmcv.userpath="%MCV_USERPATH%" -cp mcidasv.jar edu.wisc.ssec.mcidasv.util.GetMem 2^>NUL') DO SET SYS_MEM=%%i
 
 SET MCV_FLAGS=-Didv.3d=%ENABLE_3D% -Didv.sysmem=%SYS_MEM% -Dvisad.java3d.textureNpot=%ALLOW_NPOT% -Dvisad.java3d.imageByRef=%IMAGE_BY_REF% -Dvisad.java3d.geometryByRef=%GEOMETRY_BY_REF% -userpath "%MCV_USERPATH%"
 
