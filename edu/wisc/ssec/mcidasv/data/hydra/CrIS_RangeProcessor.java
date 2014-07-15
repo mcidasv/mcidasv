df4037f27da89dd984ca36bdc9f89dd1a714d306
Tommy Jasmin <tommy.jasmin@ssec.wisc.edu>
2014-07-15 10:43:00 -0500

    Lose a couple unneeded method calls [0]

--- edu/wisc/ssec/mcidasv/data/hydra/CrIS_RangeProcessor.java
+++ edu/wisc/ssec/mcidasv/data/hydra/CrIS_RangeProcessor.java
@@ -39,11 +39,7 @@ public class CrIS_RangeProcessor extends RangeProcessor {
 	public float[] processRange(float[] values, HashMap subset) {
 
 		double[] track_coords = (double[]) subset.get(SwathAdapter.track_name);
-		double[] xtrack_coords = (double[]) subset.get(SwathAdapter.xtrack_name);
-
-		int numElems = ((int) (xtrack_coords[1] - xtrack_coords[0]) + 1);
 		int numLines = ((int) (track_coords[1] - track_coords[0]) + 1);
-
 		values = CrIS_SDR_Utility.psuedoScanReorder(values, 90, numLines * 3);
 
 		// - subset here, if necessary
--- edu/wisc/ssec/mcidasv/data/hydra/IASI_RangeProcessor.java
+++ edu/wisc/ssec/mcidasv/data/hydra/IASI_RangeProcessor.java
@@ -40,13 +40,8 @@ public class IASI_RangeProcessor extends RangeProcessor {
 		int channelIndex = (int) ((double[]) subset.get(SpectrumAdapter.channelIndex_name))[0];
 
 		float[] new_values = IASI_L1C_Utility.getDecodedIASIImage(values, null, channelIndex);
-
 		double[] track_coords = (double[]) subset.get(SwathAdapter.track_name);
-		double[] xtrack_coords = (double[]) subset.get(SwathAdapter.xtrack_name);
-
-		int numElems = ((int) (xtrack_coords[1] - xtrack_coords[0]) + 1);
 		int numLines = ((int) (track_coords[1] - track_coords[0]) + 1);
-
 		new_values = IASI_L1C_Utility.psuedoScanReorder2(new_values, 60, numLines * 2);
 
 		// - subset here, if necessary
