9ddbe43bc256225bd93fe5207be77eb607adb0fc
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2014-10-24 23:34:46 -0500

    Quick javadoc warning fixes. [0]
    This is mostly just to keep my streak alive...but hey--an improvement is
    an improvement!
    
    Another perspective:
    
    https://lkml.org/lkml/2004/12/20/255

--- edu/wisc/ssec/mcidasv/probes/ReadoutProbe.java
+++ edu/wisc/ssec/mcidasv/probes/ReadoutProbe.java
@@ -28,7 +28,6 @@
 package edu.wisc.ssec.mcidasv.probes;
 
 import static java.util.Objects.requireNonNull;
-import static edu.wisc.ssec.mcidasv.util.Contract.*;
 
 import java.awt.Color;
 import java.beans.PropertyChangeEvent;
@@ -40,7 +39,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import ucar.unidata.collab.SharableImpl;
 import ucar.unidata.util.LogUtil;
 import ucar.unidata.view.geoloc.NavigatedDisplay;
-import ucar.visad.ShapeUtility;
 import ucar.visad.display.DisplayMaster;
 import ucar.visad.display.LineProbe;
 import ucar.visad.display.SelectorDisplayable;
@@ -394,9 +392,9 @@ public class ReadoutProbe extends SharableImpl implements PropertyChangeListener
     }
 
     /**
+     * Returns the number format string current being used.
      *
-     *
-     * @return
+     * @return Location format pattern string.
      */
     public String getFormatPattern() {
         return numFmt.toPattern();
