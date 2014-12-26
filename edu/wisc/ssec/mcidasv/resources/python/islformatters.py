7195a4b45ff0f2c9bf235eb2a8a4e98fb12b591e
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2014-12-26 15:03:38 -0600

    Fix PEP257 warnings in islformatters.py. [1780]

--- edu/wisc/ssec/mcidasv/resources/python/islformatters.py
+++ edu/wisc/ssec/mcidasv/resources/python/islformatters.py
@@ -1,9 +1,30 @@
+"""Classes for 'Pythonic' usage of ISL formatting capabilities."""
+
 class ImageFormatting(object):
+    
+    """Base class for ISL formatting objects."""
+    
     def __init__(self):
+        """Override this stubbed out method."""
         pass
         
 class Matte(ImageFormatting):
+    
+    """The Matte formatter allows you to add a space around any of the sides of an image."""
+    
     def __init__(self, background=None, top=None, bottom=None, left=None, right=None, space=None, hspace=None, vspace=None):
+        """Create a new Matte ISL formatting object.
+        
+        Optional Args:
+            background: Color name or red,green,blue values.
+            top: Pixel spacing.
+            bottom: Pixel spacing.
+            left: Pixel spacing.
+            right: Pixel spacing.
+            space: Pixel spacing.
+            hspace: Pixel spacing.
+            vspace: Pixel spacing.
+        """
         if background:
             self.background = 'background=%s' % (background)
         else:
@@ -45,11 +66,24 @@ class Matte(ImageFormatting):
             self.vspace = ''
             
     def toIsl(self):
+        """Return the ISL string representing this Matte instance."""
         islString = "matte %s %s %s %s %s %s %s %s" % (self.background, self.top, self.bottom, self.left, self.right, self.space, self.hspace, self.vspace)
         return islString.strip() + '; '
         
 class ImageOverlay(ImageFormatting):
+    
+    """The ImageOverlay formatter allows you to add an icon as an image overlay."""
+    
     def __init__(self, image=None, place=None, anchor=None, transparency=None, scale=None):
+        """Create a new ImageOverlay ISL formatting object.
+        
+        Optional Args:
+            image: Filesystem path or URL to image.
+            place: Rectangle point on base image.
+            anchor: Rectangle point on overlay
+            transparency: Transparency percentage. Values should be 0 - 1.0.
+            scale: Not currently functional.
+        """
         if image:
             self.image = 'image=%s' % (image)
         else:
@@ -76,11 +110,28 @@ class ImageOverlay(ImageFormatting):
             self.scale = ''
             
     def toIsl(self):
+        """Return the ISL string representing this ImageOverlay instance."""
         islString = "overlay %s %s %s %s" % (self.image, self.place, self.anchor, self.transparency)
         return islString.strip() + '; '
         
 class TextOverlay(ImageFormatting):
+    
+    """The TextOverlay formatter allows you to add text as an overlay. """
+    
     def __init__(self, text=None, place=None, anchor=None, fontSize=None, fontFace=None, color=None, background=None, transparency=None, scale=None):
+        """Create a new TextOverlay ISL formatting object.
+        
+        Optional Args:
+            text: Text to draw.
+            place: Rectangle point on base image.
+            anchor: Rectangle point on overlay.
+            fontSize: Font size for text.
+            fontFace: Font face for text
+            color: Color for text.
+            background: Color for background.
+            transparency: Transparency percentage. Values should be 0-1.0.
+            scale: Not currently functional.
+        """
         if text:
             self.text = 'text=%s' % (text)
         else:
@@ -127,11 +178,35 @@ class TextOverlay(ImageFormatting):
             self.scale = ''
             
     def toIsl(self):
+        """Return the ISL string representing this TextOverlay instance."""
         islString = "overlay %s %s %s %s %s %s %s %s" % (self.text, self.place, self.anchor, self.fontSize, self.fontFace, self.color, self.background, self.transparency)
         return islString.strip() + '; '
         
 class Clip(ImageFormatting):
+    
+    """The Clip formatter allows you to clip the image."""
+    
     def __init__(self, north=None, south=None, east=None, west=None, top=None, bottom=None, left=None, right=None, display=None, space=None, hspace=None, vspace=None, spaceLeft=None, spaceRight=None, spaceTop=None, spaceBottom=None):
+        """Create a new Clip ISL formatting object.
+        
+        Optional Args:
+            north: Latitude.
+            south: Latitude.
+            east: Longitude.
+            west: Longitude.
+            top: Pixels or percentage.
+            bottom: Pixels or percentage.
+            left: Pixels or percentage.
+            right: Pixels or percentage.
+            display: ID of a display which we use its data's map projection to clip with.
+            space: When clipping at the box pad outwards this number of pixels.
+            hspace: When clipping at the box pad horiz.
+            vspace: When clipping at the box pad vertically.
+            spaceLeft: Padding.
+            spaceRight: Padding.
+            spaceTop: Padding.
+            spaceBottom: Padding.
+        """
         if north:
             self.north = 'north=%s' % (north)
         else:
@@ -213,11 +288,47 @@ class Clip(ImageFormatting):
             self.spaceBottom = ''
             
     def toIsl(self):
+        """Return the ISL string representing this Clip instance."""
         islString = "clip %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s" % (self.north, self.south, self.east, self.west, self.top, self.bottom, self.left, self.right, self.display, self.space, self.hspace, self.vspace, self.spaceLeft, self.spaceRight, self.spaceTop, self.spaceBottom)
         return islString.strip() + '; '
         
 class Colorbar(ImageFormatting):
+    
+    """The Colorbar formatter allows you to add a color bar from the color tables in the display controls."""
+    
     def __init__(self, display=None, width=None, height=None, orientation=None, tickMarks=None, interval=None, values=None, place=None, anchor=None, showLines=None, suffix=None, suffixFrequency=None, showUnit=None, transparency=None, color=None, lineColor=None):
+        """Create a new Colorbar ISL formatting object.
+        
+        Optional Args:
+        
+            display: Optional id of display to use. If not defined then this 
+                     will use all colorbars found.
+            width: Bar width.
+            height: Bar height.
+            orientation: Tick mark location.
+                         Accepted values: "right", "left", "bottom", "top".
+            tickMarks: Number of tick marks.
+            interval: Interval value.
+            values: Comma-separated list of values.
+            place: Rectangle location on image.
+            anchor: Rectangle location on colorbar.
+            showLines: Draw tick lines. Accepted values: True, False 
+                       "true" or "false" are also accepted, but are considered bad 
+                       practice.
+            suffix: Text to add to tick labels
+            suffixFrequency: Frequency of suffix plotting. 
+                             Accepted values: "all", "first", "last".
+            showUnit: Use unit as suffix. Accepted values: True, False
+                      "true" or "false" are also accepted, but are considered 
+                      bad practice.
+            transparency: Include the transparency when drawing the colorbar 
+                          (defaults to True). 
+                          Accepted values: True, False
+                          "true" or "false" are also accepted, but are 
+                          considered bad practice.
+            color: Label color.
+            lineColor: Line color.
+        """
         if display:
             self.displayObj = display
             display._ensureIslId()
@@ -304,12 +415,21 @@ class Colorbar(ImageFormatting):
             self.lineColor = ''
             
     def toIsl(self):
+        """Return the ISL string representing this Colorbar instance."""
         islString = "colorbar %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s" % (self.display, self.width, self.height, self.orientation, self.tickMarks, self.interval, self.values, self.place, self.anchor, self.showLines, self.suffix, self.suffixFrequency, self.showUnit, self.transparency, self.color, self.lineColor)
         return islString.strip() + '; '
         
 class TransparentColor(ImageFormatting):
+    
+    """The TransparentColor formatter marks a color in the display as transparent."""
+    
     # def __init__(self, color, rgbTuple):
     def __init__(self, color=None):
+        """Create a new TransparentColor ISL formatting object.
+        
+        Optional Arg:
+            color: Color to set to transparent.
+        """
         if color:
             self.color = 'color=%s' % (color)
         else:
@@ -319,18 +439,33 @@ class TransparentColor(ImageFormatting):
         # self.blue = blue
         
     def toIsl(self):
+        """Return the ISL string representing this TransparentColor instance."""
         islString = "transparent %s" % (self.color)
         return islString.strip() + '; '
         
 class TransparentBackground(ImageFormatting):
+    
+    """The TransparentBackground formatter marks the display background color as transparent."""
+    
     def __init__(self):
+        """Create a new TransparentBackground ISL formatting object."""
         pass
         
     def toIsl(self):
+        """Return the ISL string representing this TransparentBackground instance."""
         return "backgroundtransparent ; "
         
 class Resize(ImageFormatting):
+    
+    """The Resize formatter allows you to resize the captured image."""
+    
     def __init__(self, dimensions):
+        """Create a new Resize ISL formatting object.
+        
+        Optional Arg:
+            dimensions: Tuple representing the desired dimensions (width and 
+                        height). Values may either be in pixels or percentages.
+        """
         if dimensions and len(dimensions) == 2:
             self.width = 'width=%s' % (dimensions[0])
             self.height = 'height=%s' % (dimensions[1])
@@ -338,5 +473,6 @@ class Resize(ImageFormatting):
             raise ValueError()
             
     def toIsl(self):
+        """Return the ISL string representing this Resize instance."""
         islString = "resize %s %s" % (self.width, self.height)
         return islString.strip() + '; '
