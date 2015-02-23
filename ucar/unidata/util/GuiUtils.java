d6ec9d4a9c625de8a2d4a0ea888ccef9dce0e0e5
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2015-02-22 21:13:15 -0600

    Remove some of the trace output. [1866]

--- ucar/unidata/util/GuiUtils.java
+++ ucar/unidata/util/GuiUtils.java
@@ -1218,9 +1218,7 @@ public class GuiUtils extends LayoutUtil {
      */
     public static Image getImage(String file, Class c, boolean cache,
                                  boolean returnNullIfNotFound) {
-        logger.trace("file='{}' class='{}' cache param={} returnNullIfNotFound={}", file, c.getCanonicalName(), cache, returnNullIfNotFound);
         if ( !CacheManager.getDoCache()) {
-            logger.trace("CacheManager is disabled; disabling cache param");
             cache = false;
         }
 
@@ -1236,10 +1234,8 @@ public class GuiUtils extends LayoutUtil {
         if (cache) {
             image = (Image) imageCache.get(key);
             if (image != null) {
-                logger.trace("cache key='{}' found a cached image={}", key, image);
                 return image;
             } else {
-                logger.trace("cache key='{}' did not find a cached image", key);
             }
         }
 
@@ -1247,39 +1243,27 @@ public class GuiUtils extends LayoutUtil {
             InputStream is = IOUtil.getInputStream(file, c);
             if (is != null) {
                 byte[] bytes = IOUtil.readBytes(is);
-                logger.trace("read {} bytes", bytes.length);
                 image = Toolkit.getDefaultToolkit().createImage(bytes);
                 if (cache) {
-                    logger.trace("caching image using key='{}'", key);
                     imageCache.put(key, image);
-                } else {
-                    logger.trace("not caching image");
                 }
                 return image;
-            } else {
-                logger.trace("could not create input stream from '{}'", file);
             }
             if (returnNullIfNotFound) {
                 return null;
             }
         } catch (Exception exc) {
-            logger.error("Problem occurred when getting image", exc);
             if (returnNullIfNotFound) {
                 return null;
             }
-//            System.err.println(exc + " getting image ");
         }
 
-//        System.err.println("Unable to find image:" + file);
         logger.warn("Unable to find image '{}'", file);
         URL url = Misc.getURL(MISSING_IMAGE, GuiUtils.class);
         if (url == null) {
             logger.error("Could not load missing image '"+MISSING_IMAGE+'\'');
-//            System.err.println("Whoah, could not load missing image:"
-//                               + MISSING_IMAGE);
             return null;
         }
-        logger.trace("creating image from url '{}'", url);
         return Toolkit.getDefaultToolkit().createImage(url);
     }
 
@@ -4079,7 +4063,6 @@ public class GuiUtils extends LayoutUtil {
      * @throws Exception
      */
     public static Image getImage(Component component) throws Exception {
-        logger.trace("component='{}'", component);
         RepaintManager manager = RepaintManager.currentManager(component);
         //        Image image = manager.getOffscreenBuffer(component, component.getWidth (), component.getHeight ());
         double w = component.getWidth();
@@ -4088,7 +4071,6 @@ public class GuiUtils extends LayoutUtil {
                                   BufferedImage.TYPE_INT_RGB);
         Graphics2D g = (Graphics2D) image.getGraphics();
         component.paint(g);
-        logger.trace("returning image={}", image);
         return image;
     }
 
@@ -5924,7 +5906,7 @@ public class GuiUtils extends LayoutUtil {
             final JTabbedPane tab) {
         tab.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
-                logger.trace("tab changed: e='{}' isViz={}", e, tab.isVisible());
+//                logger.trace("tab changed: e='{}' isViz={}", e, tab.isVisible());
                 checkHeavyWeightComponents(tab);
             }
         });
