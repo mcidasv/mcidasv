d25ec98da725d573dc2213db6e5c94cc11fd7c36
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2015-06-22 14:52:41 -0500

    Some UrlChooser cleanup. [0]

--- edu/wisc/ssec/mcidasv/chooser/UrlChooser.java
+++ edu/wisc/ssec/mcidasv/chooser/UrlChooser.java
@@ -52,9 +52,8 @@ import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Element;
 
 import edu.wisc.ssec.mcidasv.Constants;
@@ -64,73 +63,87 @@ import edu.wisc.ssec.mcidasv.util.McVGuiUtils.TextColor;
 import edu.wisc.ssec.mcidasv.util.McVGuiUtils.Width;
 
 import ucar.unidata.data.DataManager;
-import ucar.unidata.idv.IntegratedDataViewer;
 import ucar.unidata.idv.chooser.IdvChooserManager;
 import ucar.unidata.util.GuiUtils;
+import ucar.unidata.util.LayoutUtil;
 import ucar.unidata.util.PreferenceList;
 import ucar.unidata.util.StringUtil;
 
 /**
- * Allows the user to select a url as a data source
- *
- * @author IDV development team
- * @version $Revision$Date: 2011/03/24 16:06:31 $
+ * Allows the user to select a url as a data source.
  */
 public class UrlChooser extends ucar.unidata.idv.chooser.UrlChooser implements Constants {
 
-    /** Manages the pull down list of urls */
+    /** Property ID for the contents of {@link #textArea}. */
+    public static final String PROP_MULTI_URLS =
+        "mcidasv.chooser.url.multiurlcontents";
+
+    /**
+     * Property ID for the {@literal "URL type"} selection.
+     * Values will be either {@link #MULTIPLE_URL_VALUE} or
+     * {@link #SINGLE_URL_VALUE}.
+     */
+    public static final String PROP_URL_TYPE = "mcidasv.chooser.url.urltype";
+
+    /** Multiple URL selection value for {@link #PROP_URL_TYPE}. */
+    public static final String MULTIPLE_URL_VALUE = "multiple";
+
+    /** Single URL selection value for {@link #PROP_URL_TYPE}. */
+    public static final String SINGLE_URL_VALUE = "single";
+
+    /** Manages the pull down list of URLs. */
     private PreferenceList prefList;
 
-    /** The list of urls */
+    /** List of URLs. */
     private JComboBox box;
+
     private JTextField boxEditor;
 
-    /** The text area for multi-line urls */
+    /** Text area for multi-line URLs. */
     private JTextArea textArea;
 
-    /** text scroller */
+    /** Text scroller. */
     private JScrollPane textScroller;
 
-    /** Holds the combo box */
+    /** Holds the combo box. */
     private JPanel urlPanel;
 
-    /** Holds the text area */
+    /** Holds the text area. */
     private JPanel textPanel;
 
-    /** Are we showing the combo box */
+    /** Are we showing {@link #textPanel}? */
     private boolean showBox = true;
 
-    /** Swtich */
-    private JButton switchBtn;
-
-    /** panel */
-    private GuiUtils.CardLayoutPanel cardLayoutPanel;
-    
     /**
-     * Get a handle on the IDV
+     * Panel that allows switching between {@link #urlPanel} and
+     * {@link #textPanel}.
      */
-    protected IntegratedDataViewer idv = getIdv();
+    private GuiUtils.CardLayoutPanel cardLayoutPanel;
+
+    private JLabel statusLabel = new JLabel("Status");
 
     /**
      * Create the {@code UrlChooser}.
      *
      * @param mgr {@code IdvChooserManager}.
      * @param root XML root that defines this chooser.
-     *
      */
     public UrlChooser(IdvChooserManager mgr, Element root) {
         super(mgr, root);
-        
-        loadButton = McVGuiUtils.makeImageTextButton(ICON_ACCEPT_SMALL, getLoadCommandName());
+
+        String loadCommand = getLoadCommandName();
+        loadButton =
+            McVGuiUtils.makeImageTextButton(ICON_ACCEPT_SMALL, loadCommand);
         loadButton.setActionCommand(getLoadCommandName());
         loadButton.addActionListener(this);
 
     }
 
     /**
-     * Toggle the combobox with the text area.
+     * Show {@link #urlPanel} or {@link #textPanel}, depending on the value of
+     * {@link #showBox}.
      */
-    public void switchFields() {
+    @Override public void switchFields() {
         if (showBox) {
             cardLayoutPanel.show(urlPanel);
         } else {
@@ -143,23 +156,21 @@ public class UrlChooser extends ucar.unidata.idv.chooser.UrlChooser implements C
      * Disable/enable any components that depend on the server.
      * Try to update the status label with what we know here.
      */
-    protected void updateStatus() {
-        if (boxEditor==null || textArea==null) return;
-        if (showBox) {
-            if (!boxEditor.getText().trim().equals(""))
-                setHaveData(true);
-            else
-                setHaveData(false);
-        } else {
-            if (!textArea.getText().trim().equals(""))
-                setHaveData(true);
-            else
-                setHaveData(false);
-        }
-        super.updateStatus();
-        if (!getHaveData()) {
-            if (showBox) setStatus("Enter a URL");
-            else setStatus("Enter one or more URLs");
+    @Override protected void updateStatus() {
+        if ((boxEditor != null) && (textArea != null)) {
+            if (showBox) {
+                setHaveData(!boxEditor.getText().trim().isEmpty());
+            } else {
+                setHaveData(!textArea.getText().trim().isEmpty());
+            }
+            super.updateStatus();
+            if (!getHaveData()) {
+                if (showBox) {
+                    setStatus("Enter a URL.");
+                } else {
+                    setStatus("Enter one or more URLs.");
+                }
+            }
         }
     }
 
@@ -185,121 +196,117 @@ public class UrlChooser extends ucar.unidata.idv.chooser.UrlChooser implements C
     private void loadURLInner() {
 
         String url = "";
-        String    dataSourceId = getDataSourceId();
+        String dataSourceId = getDataSourceId();
         if (showBox) {
             Object selectedItem = box.getSelectedItem();
             if (selectedItem != null) {
                 url = selectedItem.toString().trim();
             }
-            if (url.length() == 0 && dataSourceId == null) {
-                userMessage("Please specify a url");
+            if (url.isEmpty() && (dataSourceId == null)) {
+                userMessage("Please specify a URL.");
                 return;
             }
         }
 
-        Hashtable properties   = new Hashtable();
+        Hashtable props = new Hashtable();
         if (dataSourceId != null) {
-            properties.put(DataManager.DATATYPE_ID, dataSourceId);
+            props.put(DataManager.DATATYPE_ID, dataSourceId);
         }
 
         if (showBox) {
-            if (idv.handleAction(url, properties)) {
+            if (getIdv().handleAction(url, props)) {
                 closeChooser();
                 prefList.saveState(box);
             }
         } else {
-            List urls = StringUtil.split(textArea.getText(), "\n", true,
-                                         true);
+            List<String> urls =
+                StringUtil.split(textArea.getText(), "\n", true, true);
 
-            if ((urls.size() > 0)
-                    && makeDataSource(urls, dataSourceId, properties)) {
+            if (!urls.isEmpty() && makeDataSource(urls, dataSourceId, props)) {
                 closeChooser();
-                idv.getStore().put("mcidasv.chooser.url.multiurlcontents", textArea.getText());
+                getIdv().getStore().put(PROP_MULTI_URLS, textArea.getText());
             }
         }
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(UrlChooser.class);
-
     /**
-     * Make the contents
+     * Make the contents.
      *
      * @return  the contents
      */
     protected JPanel doMakeInnerPanel() {
 
-        String urlType = idv.getStore().get("mcidasv.chooser.url.urltype", "single");
-        if ("multiple".equals(urlType)) {
+        String urlType =
+            getIdv().getStore().get(PROP_URL_TYPE, SINGLE_URL_VALUE);
+        if (MULTIPLE_URL_VALUE.equals(urlType)) {
             showBox = false;
         }
 
-        final JRadioButton singleBtn = new JRadioButton("Single", showBox);
+        final JToggleButton singleBtn = new JRadioButton("Single", showBox);
         singleBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                logger.trace("singleBtn: selected={}", singleBtn.isSelected());
-                idv.getStore().put("mcidasv.chooser.url.urltype", "single");
+            @Override public void actionPerformed(ActionEvent e) {
+                getIdv().getStore().put(PROP_URL_TYPE, SINGLE_URL_VALUE);
                 showBox = true;
                 switchFields();
             }
         });
 
-        final JRadioButton multipleBtn = new JRadioButton("Multiple", !showBox);
+        final JToggleButton multipleBtn =
+            new JRadioButton("Multiple", !showBox);
         multipleBtn.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                logger.trace("multipleBtn: selected={}", multipleBtn.isSelected());
-                idv.getStore().put("mcidasv.chooser.url.urltype", "multiple");
+            @Override public void actionPerformed(ActionEvent e) {
+                getIdv().getStore().put(PROP_URL_TYPE, MULTIPLE_URL_VALUE);
                 showBox = false;
                 switchFields();
             }
         });
         GuiUtils.buttonGroup(singleBtn, multipleBtn);
-        JPanel radioPanel = GuiUtils.hbox(singleBtn, multipleBtn);
+        JPanel radioPanel = LayoutUtil.hbox(singleBtn, multipleBtn);
         
         prefList = getPreferenceList(PREF_URLLIST);
         box = prefList.createComboBox(CMD_LOAD, this);
         boxEditor = (JTextField)box.getEditor().getEditorComponent();
         boxEditor.addKeyListener(new KeyListener() {
-            public void keyPressed(KeyEvent e) {}
-            public void keyReleased(KeyEvent e) {
+            @Override public void keyPressed(KeyEvent e) {}
+            @Override public void keyReleased(KeyEvent e) {
                 updateStatus();
             }
-            public void keyTyped(KeyEvent e) {}
+            @Override public void keyTyped(KeyEvent e) {}
         });
         
         textArea = new JTextArea(5, 30);
         textScroller = new JScrollPane(textArea);
         textArea.addKeyListener(new KeyListener() {
-            public void keyPressed(KeyEvent e) {}
-            public void keyReleased(KeyEvent e) {
+            @Override public void keyPressed(KeyEvent e) {}
+            @Override public void keyReleased(KeyEvent e) {
                 updateStatus();
             }
-            public void keyTyped(KeyEvent e) {}
+            @Override public void keyTyped(KeyEvent e) {}
         });
         
-        urlPanel = GuiUtils.top(box);
-        textPanel = GuiUtils.top(textScroller);
+        urlPanel = LayoutUtil.top(box);
+        textPanel = LayoutUtil.top(textScroller);
         
         cardLayoutPanel = new GuiUtils.CardLayoutPanel();
         cardLayoutPanel.addCard(urlPanel);
         cardLayoutPanel.addCard(textPanel);
         
-        JPanel showPanel = McVGuiUtils.topBottom(radioPanel, cardLayoutPanel, null);
+        JPanel showPanel =
+            McVGuiUtils.topBottom(radioPanel, cardLayoutPanel, null);
         
         setHaveData(false);
         updateStatus();
         if (!showBox) {
-            textArea.setText(idv.getStore().get("mcidasv.chooser.url.multiurlcontents", ""));
+            textArea.setText(getIdv().getStore().get(PROP_MULTI_URLS, ""));
             switchFields();
         }
         return McVGuiUtils.makeLabeledComponent("URL:", showPanel);
     }
 
-    private JLabel statusLabel = new JLabel("Status");
-
-    @Override
-    public void setStatus(String statusString, String foo) {
-        if (statusString == null)
+    @Override public void setStatus(String statusString, String foo) {
+        if (statusString == null) {
             statusString = "";
+        }
         statusLabel.setText(statusString);
     }
         
@@ -308,7 +315,9 @@ public class UrlChooser extends ucar.unidata.idv.chooser.UrlChooser implements C
      */
     protected JComponent doMakeContents() {
         JComponent typeComponent = getDataSourcesComponent();
-        if (typeComponent==null) typeComponent=new JLabel("No data type specified");
+        if (typeComponent == null) {
+            typeComponent = new JLabel("No data type specified");
+        }
 
         JPanel innerPanel = doMakeInnerPanel();
         
@@ -323,11 +332,13 @@ public class UrlChooser extends ucar.unidata.idv.chooser.UrlChooser implements C
         McVGuiUtils.setLabelPosition(statusLabel, Position.RIGHT);
         McVGuiUtils.setComponentColor(statusLabel, TextColor.STATUS);
         
-        JButton helpButton = McVGuiUtils.makeImageButton(ICON_HELP, "Show help");
+        JButton helpButton =
+            McVGuiUtils.makeImageButton(ICON_HELP, "Show help");
         helpButton.setActionCommand(GuiUtils.CMD_HELP);
         helpButton.addActionListener(this);
         
-        JButton refreshButton = McVGuiUtils.makeImageButton(ICON_REFRESH, "Refresh");
+        JButton refreshButton =
+            McVGuiUtils.makeImageButton(ICON_REFRESH, "Refresh");
         refreshButton.setActionCommand(GuiUtils.CMD_UPDATE);
         refreshButton.addActionListener(this);
         
@@ -380,10 +391,7 @@ public class UrlChooser extends ucar.unidata.idv.chooser.UrlChooser implements C
                     .addComponent(helpButton))
                 .addContainerGap())
         );
-    
         return outerPanel;
-
     }
-
 }
 
