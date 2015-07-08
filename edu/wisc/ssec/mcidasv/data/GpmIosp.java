ff668f18d8f7bb9a5427a9e6689e23bff8fda0dc
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2015-07-08 06:45:34 -0500

    Code is finally looking more reasonable. [0]

--- edu/wisc/ssec/mcidasv/data/GpmIosp.java
+++ edu/wisc/ssec/mcidasv/data/GpmIosp.java
@@ -56,128 +56,20 @@ public class GpmIosp extends AbstractIOServiceProvider {
 
     private NetcdfFile hdfFile;
 
-    private static int[] getLineElementCounts(Group group) throws IOException {
-        Variable lat = group.findVariable("Latitude");
-        return new int[] { lat.getDimension(0).getLength(), lat.getDimension(1).getLength() };
+    private static int[] getDimensionLengths(NetcdfFile hdf, String groupName) throws IOException {
+        Group group = hdf.findGroup(groupName);
+        Variable tc = group.findVariable("Tc");
+        return new int[] {
+            tc.getDimension(0).getLength(),
+            tc.getDimension(1).getLength(),
+            tc.getDimension(2).getLength()
+        };
     }
 
-    @Override public boolean isValidFile(RandomAccessFile raf)
-        throws IOException
-    {
-        // this isn't exactly a suitable test--but maybe it'll work for now?
-        // the better check is to probably try opening the location as a netcdf
-        // file and looking for S1/S2 groups and possibly S1/Tc S2/Tc vars?
-        String location = raf.getLocation();
-        return location.contains("1C-R-CS") && location.endsWith(".HDF");
-    }
-
-    public void open(RandomAccessFile raf, NetcdfFile ncfile,
-                     CancelTask cancelTask) throws IOException
-    {
-        try {
-            hdfFile = NetcdfFile.open(raf.getLocation(), "ucar.nc2.iosp.hdf5.H5iosp", -1, (CancelTask)null, (Object)null);
-
-            Group hdfGroup1 = hdfFile.findGroup("S1");
-            int[] counts = getLineElementCounts(hdfGroup1);
-            Group s1 = new Group(ncfile, null, "S1");
-            Dimension s1Lines = new Dimension("line", counts[0]);
-            Dimension s1Elements = new Dimension("ele", counts[1]);
-            s1.addDimension(s1Lines);
-            s1.addDimension(s1Elements);
-
-            Group hdfGroup2 = hdfFile.findGroup("S2");
-            counts = getLineElementCounts(hdfGroup2);
-            Group s2 = new Group(ncfile, null, "S2");
-            Dimension s2Lines = new Dimension("line", counts[0]);
-            Dimension s2Elements = new Dimension("ele", counts[1]);
-            s2.addDimension(s2Lines);
-            s2.addDimension(s2Elements);
-
-            Variable s1Lat = new Variable(ncfile, s1, null, "latitude", DataType.FLOAT, "line ele");
-            s1Lat.addAttribute(new Attribute(_Coordinate.AxisType, AxisType.Lat.toString()));
-            Variable s1Lon = new Variable(ncfile, s1, null, "longitude", DataType.FLOAT, "line ele");
-            s1Lon.addAttribute(new Attribute(_Coordinate.AxisType, AxisType.Lon.toString()));
-
-            Variable s2Lat = new Variable(ncfile, s2, null, "latitude", DataType.FLOAT, "line ele");
-            s2Lat.addAttribute(new Attribute(_Coordinate.AxisType, AxisType.Lat.toString()));
-            Variable s2Lon = new Variable(ncfile, s2, null, "longitude", DataType.FLOAT, "line ele");
-            s2Lon.addAttribute(new Attribute(_Coordinate.AxisType, AxisType.Lon.toString()));
-
-            Attribute attr = new Attribute("coordinates", "latitude longitude");
-
-            Variable s1Var0 = new Variable(ncfile, s1, null, "Tc_0", DataType.FLOAT, "line ele");
-            s1Var0.addAttribute(attr);
-            Variable s1Var1 = new Variable(ncfile, s1, null, "Tc_1", DataType.FLOAT, "line ele");
-            s1Var1.addAttribute(attr);
-            Variable s1Var2 = new Variable(ncfile, s1, null, "Tc_2", DataType.FLOAT, "line ele");
-            s1Var2.addAttribute(attr);
-            Variable s1Var3 = new Variable(ncfile, s1, null, "Tc_3", DataType.FLOAT, "line ele");
-            s1Var3.addAttribute(attr);
-            Variable s1Var4 = new Variable(ncfile, s1, null, "Tc_4", DataType.FLOAT, "line ele");
-            s1Var4.addAttribute(attr);
-            Variable s1Var5 = new Variable(ncfile, s1, null, "Tc_5", DataType.FLOAT, "line ele");
-            s1Var5.addAttribute(attr);
-            Variable s1Var6 = new Variable(ncfile, s1, null, "Tc_6", DataType.FLOAT, "line ele");
-            s1Var6.addAttribute(attr);
-            Variable s1Var7 = new Variable(ncfile, s1, null, "Tc_7", DataType.FLOAT, "line ele");
-            s1Var7.addAttribute(attr);
-            Variable s1Var8 = new Variable(ncfile, s1, null, "Tc_8", DataType.FLOAT, "line ele");
-            s1Var8.addAttribute(attr);
-
-            Variable s2Var0 = new Variable(ncfile, s2, null, "Tc_0", DataType.FLOAT, "line ele");
-            s2Var0.addAttribute(attr);
-            Variable s2Var1 = new Variable(ncfile, s2, null, "Tc_1", DataType.FLOAT, "line ele");
-            s2Var1.addAttribute(attr);
-            Variable s2Var2 = new Variable(ncfile, s2, null, "Tc_2", DataType.FLOAT, "line ele");
-            s2Var2.addAttribute(attr);
-            Variable s2Var3 = new Variable(ncfile, s2, null, "Tc_3", DataType.FLOAT, "line ele");
-            s2Var3.addAttribute(attr);
-            Variable s2Var4 = new Variable(ncfile, s2, null, "Tc_4", DataType.FLOAT, "line ele");
-            s2Var4.addAttribute(attr);
-            Variable s2Var5 = new Variable(ncfile, s2, null, "Tc_5", DataType.FLOAT, "line ele");
-            s2Var5.addAttribute(attr);
-            Variable s2Var6 = new Variable(ncfile, s2, null, "Tc_6", DataType.FLOAT, "line ele");
-            s2Var6.addAttribute(attr);
-            Variable s2Var7 = new Variable(ncfile, s2, null, "Tc_7", DataType.FLOAT, "line ele");
-            s2Var7.addAttribute(attr);
-            Variable s2Var8 = new Variable(ncfile, s2, null, "Tc_8", DataType.FLOAT, "line ele");
-            s2Var8.addAttribute(attr);
-
-            ncfile.addGroup(null, s1);
-            ncfile.addGroup(null, s2);
-
-            s1.addVariable(s1Lat);
-            s1.addVariable(s1Lon);
-            s1.addVariable(s1Var0);
-            s1.addVariable(s1Var1);
-            s1.addVariable(s1Var2);
-            s1.addVariable(s1Var3);
-            s1.addVariable(s1Var4);
-            s1.addVariable(s1Var5);
-            s1.addVariable(s1Var6);
-            s1.addVariable(s1Var7);
-            s1.addVariable(s1Var8);
-
-            s2.addVariable(s2Lat);
-            s2.addVariable(s2Lon);
-            s2.addVariable(s2Var0);
-            s2.addVariable(s2Var1);
-            s2.addVariable(s2Var2);
-            s2.addVariable(s2Var3);
-            s2.addVariable(s2Var4);
-            s2.addVariable(s2Var5);
-            s2.addVariable(s2Var6);
-            s2.addVariable(s2Var7);
-            s2.addVariable(s2Var8);
-
-            ncfile.finish();
-        } catch (ClassNotFoundException e) {
-            logger.error("error loading HDF5 IOSP", e);
-        } catch (IllegalAccessException e) {
-            logger.error("java reflection error", e);
-        } catch (InstantiationException e) {
-            logger.error("error instantiating", e);
-        }
+    private static void addVar(NetcdfFile nc, Group g, int channel) {
+        Variable v = new Variable(nc, g, null, "Tc_"+channel, DataType.FLOAT, "line ele");
+        v.addAttribute(new Attribute("coordinates", "latitude longitude"));
+        g.addVariable(v);
     }
 
     private static int variableToChannel(String variableName) {
@@ -189,40 +81,96 @@ public class GpmIosp extends AbstractIOServiceProvider {
         return result;
     }
 
+    private static void addLatitude(NetcdfFile nc, Group g) {
+        Variable lat = new Variable(nc, g, null, "Latitude", DataType.FLOAT, "line ele");
+        lat.addAttribute(new Attribute(_Coordinate.AxisType, AxisType.Lat.toString()));
+        g.addVariable(lat);
+    }
+
+    private static void addLongitude(NetcdfFile nc, Group g) {
+        Variable lon = new Variable(nc, g, null, "Longitude", DataType.FLOAT, "line ele");
+        lon.addAttribute(new Attribute(_Coordinate.AxisType, AxisType.Lon.toString()));
+        g.addVariable(lon);
+    }
+
+
+    @Override public boolean isValidFile(RandomAccessFile raf)
+        throws IOException
+    {
+        // this isn't exactly a suitable test--but maybe it'll work for now?
+        // the better check is to probably try opening the location as a netcdf
+        // file and looking for S1/S2 groups and possibly S1/Tc S2/Tc vars?
+        String location = raf.getLocation();
+        return location.contains("1C-R-CS") && location.endsWith(".HDF");
+    }
+
+    @Override public void open(RandomAccessFile raf, NetcdfFile ncfile,
+                     CancelTask cancelTask) throws IOException
+    {
+        try {
+            hdfFile = NetcdfFile.open(raf.getLocation(), "ucar.nc2.iosp.hdf5.H5iosp", -1, (CancelTask)null, (Object)null);
+            createGroupFromHdf(ncfile, "S1");
+            createGroupFromHdf(ncfile, "S2");
+            ncfile.finish();
+        } catch (ClassNotFoundException e) {
+            logger.error("error loading HDF5 IOSP", e);
+        } catch (IllegalAccessException e) {
+            logger.error("java reflection error", e);
+        } catch (InstantiationException e) {
+            logger.error("error instantiating", e);
+        }
+    }
+
+    private void createGroupFromHdf(NetcdfFile ncOut, String groupName)
+        throws IOException
+    {
+        Group s1 = new Group(ncOut, null, groupName);
+        int[] dimLen = getDimensionLengths(hdfFile, groupName);
+        ncOut.addGroup(null, s1);
+        s1.addDimension(new Dimension("line", dimLen[0]));
+        s1.addDimension(new Dimension("ele", dimLen[1]));
+        addLatitude(ncOut, s1);
+        addLongitude(ncOut, s1);
+        for (int i = 0; i < dimLen[2]; i++) {
+            addVar(ncOut, s1, i);
+        }
+    }
+
     @Override public Array readData(Variable variable, Section section)
         throws IOException, InvalidRangeException
     {
         logger.trace("variable='{}' group='{}' section='{}'", variable.getShortName(), variable.getParentGroup().getShortName(), section);
-//        String groupName = variable.getGroup().getShortName();
+
         String variableName = variable.getShortName();
+        String groupName = variable.getParentGroup().getFullName();
+        Group hdfGroup = hdfFile.findGroup(groupName);
+        Array result;
 
-        Group hdfGroup = hdfFile.findGroup(variable.getParentGroup().getFullName());
-        if (variableName.equals("latitude")) {
-
-            // case matters
-            Variable hdfVariable = hdfFile.findVariable(hdfGroup, "Latitude");
-            return hdfVariable.read();
-        }
-        if (variableName.equals("longitude")) {
-            // case matters
-            Variable hdfVariable = hdfFile.findVariable(hdfGroup, "Longitude");
-            return hdfVariable.read();
-        }
-        if (variableName.startsWith("Tc_")) {
+        if (variableName.equals("Latitude") || variableName.equals("Longitude")) {
+            Variable hdfVariable = hdfFile.findVariable(hdfGroup, variableName);
+            result = hdfVariable.read();
+        } else if (variableName.startsWith("Tc_")) {
             int channel = variableToChannel(variableName);
             logger.trace("trying to read '{}' channel={}", variableName, channel);
             Variable hdfVariable = hdfFile.findVariable(hdfGroup, "Tc");
-            int[] counts = getLineElementCounts(hdfGroup);
-            return readChannel(hdfVariable, counts[0], counts[1], channel);
+//            int[] counts = getLineElementCounts(hdfGroup);
+            int[] counts = getDimensionLengths(hdfFile, groupName);
+            result = readChannel(hdfVariable, counts[0], counts[1], channel);
+        } else {
+            logger.trace("return null :(");
+            result = null;
         }
-
-        logger.trace("return null :(");
-        return null;
+        return result;
     }
 
     private Array readChannel(Variable v, int lines, int elements, int channel)
         throws IOException, InvalidRangeException
     {
+        // "S1/Tc" and "S2/Tc" (aka "v") is laid out like "line, ele, chan"
+        // this origin/size business is the notation required for per-channel
+        // access
+        // see "Reading data from a Variable" from
+        // http://www.unidata.ucar.edu/software/thredds/current/netcdf-java/tutorial/NetcdfFile.html
         int[] origin = { 0, 0, channel };
         int[] size = { lines, elements, 1};
         return v.read(origin, size).reduce();
