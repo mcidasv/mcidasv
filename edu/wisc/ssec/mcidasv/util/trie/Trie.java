8f1b67f259f1731d6c9d7cc03bbfc5199acc4f17
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2015-10-03 20:04:02 -0500

    More javadoc warning fixes. [0]

--- edu/wisc/ssec/mcidasv/util/trie/Trie.java
+++ edu/wisc/ssec/mcidasv/util/trie/Trie.java
@@ -48,8 +48,13 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * For example, if the Trie contains 'Lime', 'LimeWire', 
      * 'LimeRadio', 'Lax', 'Later', 'Lake', and 'Lovely', then
      * a lookup of 'Lime' would return 'Lime', 'LimeRadio', and 'LimeWire'.
+     *
+     * @param key Key to search for within the trie.
+     *
+     * @return {@code Map} containing keys matching the given prefix and their
+     * values.
      */
-    public SortedMap<K, V> getPrefixedBy(K key);
+    SortedMap<K, V> getPrefixedBy(K key);
     
     /**
      * Returns a view of this Trie of all elements that are
@@ -62,8 +67,15 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * 'LimeRadio', 'Lax', 'Later', 'Lake', and 'Lovely', then
      * a lookup of 'LimePlastics' with a length of 4 would
      * return 'Lime', 'LimeRadio', and 'LimeWire'.
+     *
+     * @param key Key to search for within the trie.
+     * @param length Length of {@code key}. Ignored for tries with a fixed key
+     *               size.
+     *
+     * @return {@code Map} containing keys matching the given prefix and their
+     * values.
      */
-    public SortedMap<K, V> getPrefixedBy(K key, int length);
+    SortedMap<K, V> getPrefixedBy(K key, int length);
     
     /**
      * Returns a view of this Trie of all elements that are prefixed
@@ -76,8 +88,16 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * 'LimeRadio', 'Lax', 'Later', 'Lake', and 'Lovely', then
      * a lookup of 'The Lime Plastics' with an offset of 4 and a 
      * length of 4 would return 'Lime', 'LimeRadio', and 'LimeWire'.
+     *
+     * @param key Key to search for within the trie.
+     * @param offset Offset to begin search at.
+     * @param length Length of {@code key}. Ignored for tries with a fixed key
+     *               size.
+     *
+     * @return {@code Map} containing keys matching the given prefix and their
+     * values.
      */
-    public SortedMap<K, V> getPrefixedBy(K key, int offset, int length);
+    SortedMap<K, V> getPrefixedBy(K key, int offset, int length);
     
     /**
      * Returns a view of this Trie of all elements that are prefixed
@@ -88,8 +108,14 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * addresses, you can lookup all addresses that begin with
      * '192.168' by providing the key '192.168.X.X' and a length of 16
      * would return all addresses that begin with '192.168'.
+     *
+     * @param key Key to search for within the trie.
+     * @param bitLength Length of {@code key} in bits.
+     *
+     * @return {@code Map} containing keys matching the given prefix and their
+     * values.
      */
-    public SortedMap<K, V> getPrefixedByBits(K key, int bitLength);
+    SortedMap<K, V> getPrefixedByBits(K key, int bitLength);
     
     /**
      * Returns the value for the entry whose key is closest in a bitwise
@@ -102,8 +128,12 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * If the Trie contained 'H' and 'L', a lookup of 'D' would return 'L',
      * because the XOR distance between D and L is smaller than the XOR distance
      * between D and H.
+     *
+     * @param key Key to search for within the trie.
+     *
+     * @return Value associated with {@code key}.
      */
-    public V select(K key);
+    V select(K key);
     
     /**
      * Iterates through the Trie, starting with the entry whose bitwise
@@ -118,11 +148,14 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * and stop traversing.
      * <p>
      * Note: The {@link Cursor.SelectStatus#REMOVE} operation is not supported.
-     * 
+     *
+     * @param key Key to match.
+     * @param cursor Cursor being used to traverse the trie.
+     *
      * @return The entry the cursor returned EXIT on, or null if it continued
      *         till the end.
      */
-    public Map.Entry<K,V> select(K key, Cursor<? super K, ? super V> cursor);
+    Map.Entry<K,V> select(K key, Cursor<? super K, ? super V> cursor);
     
     /**
      * Traverses the Trie in lexicographical order. {@code Cursor.select}
@@ -133,11 +166,13 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * selected and continue traversing.<br>
      * {@code Cursor.SelectStatus.REMOVE_AND_EXIT} is used to remove the current element
      * and stop traversing.
+     *
+     * @param cursor Cursor being used to traverse the trie.
      *   
      * @return The entry the cursor returned EXIT on, or null if it continued
      *         till the end.
      */
-    public Map.Entry<K,V> traverse(Cursor<? super K, ? super V> cursor);
+    Map.Entry<K,V> traverse(Cursor<? super K, ? super V> cursor);
     
     /**
      * An interface used by a {@link Trie}. A {@link Trie} selects items by 
@@ -161,7 +196,7 @@ public interface Trie<K, V> extends SortedMap<K, V> {
      * @param <K> Key Type
      * @param <V> Key Value
      */
-    public static interface Cursor<K, V> {
+    interface Cursor<K, V> {
         
         /**
          * Notification that the Trie is currently looking at the given entry.
@@ -170,14 +205,15 @@ public interface Trie<K, V> extends SortedMap<K, V> {
          * to remove the entry and continue iterating, or
          * {@code REMOVE_AND_EXIT} to remove the entry and stop iterating.
          * Not all operations support {@code REMOVE}.
-         * 
+         *
+         * @param entry Entry that will be operated upon.
+         *
+         * @return Status of cursor after trie operation.
          */
-        public SelectStatus select(Map.Entry<? extends K, ? extends V> entry);
+        SelectStatus select(Map.Entry<? extends K, ? extends V> entry);
      
-        /** The mode during selection.      */
-        public static enum SelectStatus {
-            EXIT, CONTINUE, REMOVE, REMOVE_AND_EXIT;
-        }
+        /** The mode during selection. */
+        enum SelectStatus { EXIT, CONTINUE, REMOVE, REMOVE_AND_EXIT }
     }
 }
 
