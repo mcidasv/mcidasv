ca42638b09e456fcf5bee8d7bd710ae9e2944ecc
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2015-11-23 20:24:27 -0600

    Formatting pass. [0]

--- edu/wisc/ssec/mcidasv/data/McIdasFrame.java
+++ edu/wisc/ssec/mcidasv/data/McIdasFrame.java
@@ -28,9 +28,6 @@
 
 package edu.wisc.ssec.mcidasv.data;
 
-import edu.wisc.ssec.mcidasv.data.McIdasXInfo;
-import edu.wisc.ssec.mcidasv.data.McIdasXFrameInfo;
-
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -39,22 +36,25 @@ import java.awt.Image;
 import ucar.unidata.util.ColorTable;
 
 /**
- * Class to hold McIdas-X frame datasets
+ * Class to hold McIDAS-X frame data sets.
  */
 public class McIdasFrame {
-    
+
     /** frame data */
     private int	myFrameNumber = 0;
     private McIdasXFrameInfo myXFrameInfo;
-    
-    /** Keep local copies of everything so we dont have to go back over the bridge unless asked to refresh */
+
+    /**
+     * Keep local copies of everything so we don't have to go back over the
+     * bridge unless asked to refresh
+     */
     private int myLineSize = -1;
     private int myElementSize = -1;
     private FrameDirectory myFrameDirectory;
     private ColorTable myColorTable;
     private byte[] myImage;
     private byte[] myGraphics;
-    
+
     /**
      *  Empty constructor for XML encoding
      */
@@ -63,91 +63,107 @@ public class McIdasFrame {
     /**
      * Construct a new McIdasFrame from the given frame number
      *
-     * @param frameNumber       frame number
+     * @param frameNumber Frame number.
      */
     public McIdasFrame(int frameNumber, McIdasXInfo xInfo) {
 //    	System.out.println("McIdasFrame constructor for frame: " + frameNumber);
         this.myFrameNumber = frameNumber;
         this.myXFrameInfo = new McIdasXFrameInfo(frameNumber, xInfo);
     }
-    
-    /** Get frame number */
+
+    /**
+     * Get frame number
+     */
     public int getFrameNumber() {
 //    	System.out.println("McIdasFrame getFrameNumber: " + this.myFrameNumber);
         return this.myFrameNumber;
     }
-    
-    /** Tell the XFrameInfo to refresh the cached data */
+
+    /**
+     * Tell the XFrameInfo to refresh the cached data
+     */
     public void setRefreshData(boolean refresh) {
 //		System.out.println("McIdasFrame setRefreshData(" + refresh + ")");
-    	this.myXFrameInfo.setRefreshData(refresh);
+        this.myXFrameInfo.setRefreshData(refresh);
     }
 
-    /** Get frame data */
+    /**
+     * Get frame data
+     */
     public int getLineSize(boolean refresh) {
 //		System.out.println("McIdasFrame getLineSize(" + refresh + ")");
-    	if (this.myLineSize <0 || refresh) {
-    		this.myLineSize = this.myXFrameInfo.getLineSize();
-    	}
-    	return this.myLineSize;
+        if (this.myLineSize <0 || refresh) {
+            this.myLineSize = this.myXFrameInfo.getLineSize();
+        }
+        return this.myLineSize;
     }
 
-    /** Get frame data */
+    /**
+     * Get frame data
+     */
     public int getElementSize(boolean refresh) {
 //		System.out.println("McIdasFrame getElementSize(" + refresh + ")");
-    	if (this.myElementSize <0 || refresh) {
-    		this.myElementSize = this.myXFrameInfo.getElementSize();
-    	}
-    	return this.myElementSize;
+        if (this.myElementSize <0 || refresh) {
+            this.myElementSize = this.myXFrameInfo.getElementSize();
+        }
+        return this.myElementSize;
     }
 
-    /** Get Frame Directory */
-	public FrameDirectory getFrameDirectory(boolean refresh) {
+    /**
+     * Get Frame Directory
+     */
+    public FrameDirectory getFrameDirectory(boolean refresh) {
 //		System.out.println("McIdasFrame getFrameDirectory(" + refresh + ")");
-		if (this.myFrameDirectory == null || refresh) {
-			this.myFrameDirectory = new FrameDirectory(this.myXFrameInfo.getFrameDirectory());
-		}
-    	return this.myFrameDirectory;
+        if (this.myFrameDirectory == null || refresh) {
+            this.myFrameDirectory = new FrameDirectory(this.myXFrameInfo.getFrameDirectory());
+        }
+        return this.myFrameDirectory;
     }
 
-	/** Get Color Table */
-	public ColorTable getColorTable(boolean refresh) {
+    /**
+     * Get Color Table
+     */
+    public ColorTable getColorTable(boolean refresh) {
 //		System.out.println("McIdasFrame getColorTable(" + refresh + ")");
-		if (this.myColorTable == null || refresh) {
-			this.myColorTable = new ColorTable("McIDAS-X",ColorTable.CATEGORY_BASIC,
-					this.myXFrameInfo.getEnhancementTable());
-		}
-    	return this.myColorTable;
+        if (this.myColorTable == null || refresh) {
+            this.myColorTable = new ColorTable("McIDAS-X",ColorTable.CATEGORY_BASIC,
+                this.myXFrameInfo.getEnhancementTable());
+        }
+        return this.myColorTable;
     }
-	
-    /** Get image data */
+
+    /**
+     * Get image data
+     */
     public byte[] getImageData(boolean refresh) {
 //		System.out.println("McIdasFrame getImageData(" + refresh + ")");
-    	if (this.myImage == null || refresh) {
-	        byte[] image = this.myXFrameInfo.getImage();
-	        int height = this.myLineSize;
-	        int width = this.myElementSize;
+        if (this.myImage == null || refresh) {
+            byte[] image = this.myXFrameInfo.getImage();
+            int height = this.myLineSize;
+            int width = this.myElementSize;
             this.myImage = new byte[height*width];
-	        for (int i=0; i<height; i++) {
-	        	for (int j=0; j<width; j++) {
-	        		this.myImage[i*width + j] = image[(height-i-1)*width + j];
-	        	}
-	        }
-    	}
-    	return this.myImage;
+            for (int i=0; i<height; i++) {
+                for (int j=0; j<width; j++) {
+                    this.myImage[i*width + j] = image[(height-i-1)*width + j];
+                }
+            }
+        }
+        return this.myImage;
     }
 
-    /** Get graphics data */
-	public byte[] getGraphicsData(boolean refresh) {
+    /**
+     * Get graphics data
+     */
+    public byte[] getGraphicsData(boolean refresh) {
 //		System.out.println("McIdasFrame getGraphicsData(" + refresh + ")");
-    	if (this.myGraphics == null || refresh) {
+        if (this.myGraphics == null || refresh) {
             List graphics = this.myXFrameInfo.getGraphics();
-	        int height = this.myLineSize;
-	        int width = this.myElementSize;
+            int height = this.myLineSize;
+            int width = this.myElementSize;
             this.myGraphics = new byte[height*width];
-	        for (int i=0; i<this.myGraphics.length; i++) {
-	        	this.myGraphics[i] = (byte)255;
-	        }
+            for (int i=0; i<this.myGraphics.length; i++) {
+                this.myGraphics[i] = (byte)255;
+            }
             String line;
             StringTokenizer tok;
             int[] graphicsPt = new int[3];
@@ -164,21 +180,22 @@ public class McIdasFrame {
                     this.myGraphics[y*width + x] = (byte)color;
                 }
             }
-    	}
-    	return this.myGraphics;
-	}
-	
-    /** Get image data */
-    public Image getGIF() {
-    	return this.myXFrameInfo.getGIF();
+        }
+        return this.myGraphics;
     }
-	
+
+    /**
+     * Get image data
+     */
+    public Image getGIF() {
+        return this.myXFrameInfo.getGIF();
+    }
+
     /**
      * See if this McIdasFrame is equal to the object in question
      *
-     * @param o   object in question
-     * @return  true if {@code o} is a McIdasFrame and
-     *          they area equivalent
+     * @param o Object in question.
+     * @return true if {@code o} is a McIdasFrame and they area equivalent.
      */
     public boolean equals(Object o) {
         if ( !(o instanceof McIdasFrame)) {
@@ -191,16 +208,15 @@ public class McIdasFrame {
 
     /**
      * Get a String representation of this object
-     * 
+     *
      * @return a string representation
      */
     public String toString() {
         StringBuffer buf = new StringBuffer();
         if (this.myFrameNumber > 0) {
-          buf.append("Frame " + this.myFrameNumber);
+            buf.append("Frame " + this.myFrameNumber);
         }
 //      System.out.println("McIdasFrame toString: " + buf);
         return buf.toString();
     }
-  
 }
