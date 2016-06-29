1f7a235ac3434e7c3bf60d608210486a09fee385
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2016-06-29 15:40:04 -0500

    Add some trace logging to RGBCompControl. [2109]
    I haven't had a lot of success replicating Bob's situation from
    yesterday, but that's likely because things get crashy for me
    with too many displays being created/destroyed...and I'm
    reluctant to go in and start making "fixes" for problems
    that may not exist.
    
    With that in mind, this commit allows us to start gathering
    at least some data about what may be happening--but it's
    just counting iterations and time spent in initDone's RGB
    while loops.

--- edu/wisc/ssec/mcidasv/control/RGBCompositeControl.java
+++ edu/wisc/ssec/mcidasv/control/RGBCompositeControl.java
@@ -39,6 +39,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -204,24 +205,47 @@ public class RGBCompositeControl extends DisplayControlImpl {
     }
 
     public void initDone() {
+        // why not keep track of iteration count?
+        DateTime elapsed = new DateTime();
+
+        DateTime redStart = new DateTime();
+        long redCount = 0;
         while (true) {
             if (null != redMap.getControl()) {
                 redTable = ((BaseColorControl) redMap.getControl()).getTable();
                 break;
             }
+            redCount++;
         }
+        long redDuration = new DateTime().minus(redStart.getMillis()).getMillis();
+        logger.trace("RED: iterations={}, duration={} (ms)", redCount, redDuration);
+
+        DateTime greenStart = new DateTime();
+        long greenCount = 0;
         while (true) {
             if (null != grnMap.getControl()) {
                 grnTable = ((BaseColorControl) grnMap.getControl()).getTable();
                 break;
             }
+            greenCount++;
         }
+        long greenDuration = new DateTime().minus(greenStart.getMillis()).getMillis();
+        logger.trace("GREEN: iterations={}, duration={} (ms)", greenCount, greenDuration);
+
+        DateTime blueStart = new DateTime();
+        long blueCount = 0;
         while (true) {
             if (null != bluMap.getControl()) {
                 bluTable = ((BaseColorControl) bluMap.getControl()).getTable();
                 break;
             }
+            blueCount++;
         }
+        long blueDuration = new DateTime().minus(blueStart.getMillis()).getMillis();
+        logger.trace("BLUE: iterations={}, duration={} (ms)", blueCount, blueDuration);
+
+        long finished = new DateTime().minus(elapsed.getMillis()).getMillis();
+        logger.trace("total iterations: {}, elapsed time: {} (ms)", redCount+greenCount+blueCount, finished);
 
         float[][] newRedTbl = getZeroOutArray(redTable);
         float[][] newGrnTbl = getZeroOutArray(grnTable);
