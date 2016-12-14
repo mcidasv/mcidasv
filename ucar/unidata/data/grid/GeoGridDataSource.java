c64cc532a48d276c0d84d5feb6ea1ec56d4aa18a
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2016-12-14 14:47:59 -0600

    Write local grids dialog sizing work. [2211]
    The most significant change is to GuiUtil's "showOkCancelDialog" method.
    
    I added a parameter named "resizable" that controls whether or not the
    resulting JDialog can be resized, and also made sure to set the
    JDialog's preferred size to the preferred size of the contents
    parameter.
    
    GuiUtils also gained another "showOkCancelDialog" overload that ensures
    all existing calls to showOkCancelDialog will behave the same as they
    always have.
    
    The actual dialog created in this case will be 1000x500, non-resizable,
    and have the "Select Fields" as the title.

--- ucar/unidata/data/grid/GeoGridDataSource.java
+++ ucar/unidata/data/grid/GeoGridDataSource.java
@@ -29,6 +29,8 @@
 package ucar.unidata.data.grid;
 
 
+import static ucar.unidata.util.GuiUtils.CMD_OK;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -36,6 +38,8 @@ import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
@@ -879,7 +883,8 @@ public class GeoGridDataSource extends GridDataSource {
         JLabel label = new JLabel(getNameForDataSource(this, 50, true));
         contents = GuiUtils.topCenter(label, contents);
         contents = GuiUtils.inset(contents, 5);
-        if ( !GuiUtils.showOkCancelDialog(null, "", contents, null)) {
+        contents.setPreferredSize(new Dimension(1000, 500));
+        if ( !GuiUtils.showOkCancelDialog(null, "Select Fields", contents, null, null, CMD_OK, false)) {
             return null;
         }
 
--- ucar/unidata/util/GuiUtils.java
+++ ucar/unidata/util/GuiUtils.java
@@ -2331,8 +2331,8 @@ public class GuiUtils extends LayoutUtil {
         }
         return dialog;
     }
-
-
+    
+    
     /**
      * Show a modeful  Ok/Cancel dialog.
      *
@@ -2352,6 +2352,32 @@ public class GuiUtils extends LayoutUtil {
                                              Component src,
                                              List actionComponents,
                                              String okLabel) {
+        return showOkCancelDialog(f, title, contents, src, actionComponents, okLabel, true);
+    }
+    
+
+    /**
+     * Show a modal Ok/Cancel dialog.
+     *
+     * @param f                The frame to attach to
+     * @param title            The window title
+     * @param contents         The gui contents to show
+     * @param src              Where should the window popup
+     * @param actionComponents If non-null then these are components
+     *                         in the contents (e.g., JTextField) that an
+     *                         action listener is added to to do the Ok
+     *                         on an action event
+     * @param okLabel          text for the OK button
+     * @param resizable        Whether or not the dialog can be resized.
+     * @return True if Ok was pressed, false otherwise
+     */
+    public static boolean showOkCancelDialog(Window f, String title,
+                                             Component contents,
+                                             Component src,
+                                             List actionComponents,
+                                             String okLabel,
+                                             boolean resizable
+    ) {
         if ( !LogUtil.getInteractiveMode()) {
             throw new IllegalStateException(
                 "Cannot show dialog in non-interactive mode");
@@ -2377,8 +2403,11 @@ public class GuiUtils extends LayoutUtil {
 
             }
         }
+        
+        dialog.setPreferredSize(contents.getPreferredSize());
         packDialog(dialog, centerBottom(contents, buttons));
         setDefaultButton(buttons, dialog.getRootPane());
+        dialog.setResizable(resizable);
         if (src != null) {
             dialog.setLocation(getLocation(src));
             dialog.setVisible(true);
