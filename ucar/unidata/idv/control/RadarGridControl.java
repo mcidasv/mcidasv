9536c8d0f40d1a47200a769ee9c68f31b5e15a50
Bob Carp <rcarp@ssec.wisc.edu>
2017-05-01 09:46:43 -0500

    Adding menu item back to Range Rings [2465]

--- ucar/unidata/idv/control/RadarGridControl.java
+++ ucar/unidata/idv/control/RadarGridControl.java
@@ -537,25 +537,23 @@ public class RadarGridControl extends DisplayControlImpl implements ActionListen
      * @param forMenuBar Is this for the menu in the window's menu bar or
      * for a popup menu in the legend
      */
-    /**
-     * BC: Commenting out section below as part of Inquiry 2465
-    * protected void getEditMenuItems(List items, boolean forMenuBar) {
-    *     ObjectListener listener = new ObjectListener("") {
-    *         public void actionPerformed(ActionEvent ae, Object obj) {
-    *             stationTableName = ((NamedStationTable) obj).getFullName();
-    *             stationIdx       = 0;
-    *             setStations();
-    *         }
-    *     };
-    *     List stationTables = getControlContext().getLocationList();
-    *     items.add(
-    *         GuiUtils.makeMenu(
-    *             "Set Locations",
-    *             NamedStationTable.makeMenuItems(stationTables, listener)));
-    * 
-    *     super.getEditMenuItems(items, forMenuBar);
-    * }
-    */
+    protected void getEditMenuItems(List items, boolean forMenuBar) {
+        ObjectListener listener = new ObjectListener("") {
+            public void actionPerformed(ActionEvent ae, Object obj) {
+               stationTableName = ((NamedStationTable) obj).getFullName();
+                stationIdx       = 0;
+                setStations();
+            }
+        };
+        List stationTables = getControlContext().getLocationList();
+        items.add(
+            GuiUtils.makeMenu(
+                "Set Locations",
+                NamedStationTable.makeMenuItems(stationTables, listener)));
+    
+        super.getEditMenuItems(items, forMenuBar);
+    }
+    
 
     /**
      * Make a selector for selecting the spacing
