ff8424820f3a2897e764a8ef0afde518bb39da89
Tommy Jasmin <tommy.jasmin@ssec.wisc.edu>
2018-02-27 08:54:08 -0600

    Just cleaning up the imports [0]

--- edu/wisc/ssec/mcidasv/data/hydra/CrIS_SDR_MultiSpectralData.java
+++ edu/wisc/ssec/mcidasv/data/hydra/CrIS_SDR_MultiSpectralData.java
@@ -28,25 +28,12 @@
 
 package edu.wisc.ssec.mcidasv.data.hydra;
 
-import java.awt.geom.Rectangle2D;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 
-import visad.CoordinateSystem;
 import visad.FlatField;
-import visad.FunctionType;
 import visad.Gridded2DSet;
-import visad.Linear1DSet;
-import visad.Linear2DSet;
-import visad.Real;
 import visad.RealTuple;
 import visad.RealTupleType;
-import visad.RealType;
-import visad.SampledSet;
-import visad.Set;
-import visad.SetType;
 import visad.VisADException;
 
 public class CrIS_SDR_MultiSpectralData extends MultiSpectralData {
