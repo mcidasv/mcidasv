64171c38a7fd926343eb0d08f8e9b377cf8687cc
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2018-05-30 12:59:26 -0500

    Forgot to include update runMcV-Config.bat.
    [2661]

--- release/runMcV-Config.bat
+++ release/runMcV-Config.bat
@@ -3,22 +3,18 @@
 SET MCV_USERPATH=%USERPROFILE%\McIDAS-V
 SET MCV_PARAMS=%*
 
+SET MCV_DIR=%~dp0%
+
+SET CURRENT_DIR=%cd%
+
+CD %MCV_DIR%\lib
+
 SET MCV_JAR=
 FOR /F %%a IN ('DIR /b mcidasv-*.jar 2^>nul') DO SET MCV_JAR=%%a
 IF DEFINED MCV_JAR (
-    GOTO finduserguide
-) ELSE (
-    ECHO "*** ERROR: Could not find McIDAS-V JAR file"
-    GOTO end
-)
-
-:finduserguide
-SET USERGUIDE_JAR=
-FOR /F %%b IN ('DIR /b mcv_userguide-*.jar 2^>nul') DO SET USERGUIDE_JAR=%%b
-IF DEFINED USERGUIDE_JAR (
     GOTO donefindingjars
 ) ELSE (
-    ECHO "*** ERROR: Could not find McIDAS-V User's Guide JAR file"
+    ECHO "*** ERROR: Could not find McIDAS-V JAR file"
     GOTO end
 )
 
@@ -52,11 +48,14 @@ IF EXIST "%USERPROFILE%\.mcidasv" (
 
 SET LOGBACK_CONFIG="%MCV_USERPATH%\logback.xml"
 
-set MCV_CLASSPATH=%CD%\%MCV_JAR%;%CD%\%USERGUIDE_JAR%
+set MCV_CLASSPATH=%MCV_JAR%;%USERGUIDE_JAR%
 
 REM Get the amount of system memory
 echo Reading system configuration...
 SET SYS_MEM=0
-FOR /F %%i IN ('jre\bin\java.exe -cp %MCV_JAR% edu.wisc.ssec.mcidasv.util.GetMem 2^>NUL') DO SET SYS_MEM=%%i
+FOR /F %%i IN ('%MCV_DIR%\jre\bin\java.exe -cp %MCV_JAR% edu.wisc.ssec.mcidasv.util.GetMem 2^>NUL') DO SET SYS_MEM=%%i
 
-jre\bin\java -Dmcv.userpath="%MCV_USERPATH%" -Dlogback.configurationFile=%LOGBACK_CONFIG% -classpath "%MCV_CLASSPATH%" -da edu.wisc.ssec.mcidasv.startupmanager.StartupManager -Didv.sysmem=%SYS_MEM% -userpath "%MCV_USERPATH%" %MCV_PARAMS%
+%MCV_DIR%\jre\bin\java -Dmcv.userpath="%MCV_USERPATH%" -Dlogback.configurationFile=%LOGBACK_CONFIG% -classpath "%MCV_CLASSPATH%" -da edu.wisc.ssec.mcidasv.startupmanager.StartupManager -Didv.sysmem=%SYS_MEM% -userpath "%MCV_USERPATH%" %MCV_PARAMS%
+:end
+
+CD %CURRENT_DIR%
