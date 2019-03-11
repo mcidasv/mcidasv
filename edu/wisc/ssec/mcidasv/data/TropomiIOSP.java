ce5596965719375e94d9e632a1bb3be02dada0f0
Tommy Jasmin <tommy.jasmin@ssec.wisc.edu>
2019-03-11 10:57:53 -0500

    Make available all products in sub-groups that match geolocation bounds [2574]

--- edu/wisc/ssec/mcidasv/data/TropomiIOSP.java
+++ edu/wisc/ssec/mcidasv/data/TropomiIOSP.java
@@ -29,7 +29,11 @@
 package edu.wisc.ssec.mcidasv.data;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,6 +53,11 @@ import ucar.nc2.constants._Coordinate;
 import ucar.nc2.iosp.AbstractIOServiceProvider;
 import ucar.nc2.util.CancelTask;
 
+/**
+ * @author tommyj
+ *
+ */
+
 public class TropomiIOSP extends AbstractIOServiceProvider {
 
     private static final String LAT = "latitude";
@@ -58,6 +67,8 @@ public class TropomiIOSP extends AbstractIOServiceProvider {
     private static final Logger logger = LoggerFactory.getLogger(TropomiIOSP.class);
     
     private static final String BASE_GROUP = "PRODUCT";
+
+    private static HashMap<String, String> groupMap = new HashMap<String, String>();
 
     // Dimensions of a product we can work with, init this early
     private static int[] dimLen = null;
@@ -83,7 +94,20 @@ public class TropomiIOSP extends AbstractIOServiceProvider {
                raf.getLocation(), "ucar.nc2.iosp.hdf5.H5iosp", -1, (CancelTask) null, (Object) null
             );
             
-            createGroupFromHdf(ncfile, BASE_GROUP);
+            // Get the dimension lengths for product data if we haven't yet
+            dimLen = getDataShape(hdfFile);
+            // Just logging the dimensions here for debugging purposes
+            for (int i = 0; i < dimLen.length; i++) {
+                logger.trace("Product dimension[" + i + "]: " + dimLen[i]);
+            }
+
+            // Populate map pairing group name to any products we deem suitable for visualization
+            Map<String, List<Variable>> newGroups = getDataVars(hdfFile, dimLen);
+
+            ncfile.addDimension(null, new Dimension("line", dimLen[1]));
+            ncfile.addDimension(null, new Dimension("ele", dimLen[2]));
+            populateDataTree(ncfile, newGroups);
+
             ncfile.finish();
         } catch (ClassNotFoundException e) {
             logger.error("error loading HDF5 IOSP", e);
@@ -94,33 +118,60 @@ public class TropomiIOSP extends AbstractIOServiceProvider {
         }
     }
 
-    private void createGroupFromHdf(NetcdfFile ncOut, String groupName)
-        throws IOException
-    {
-        Group productGroup = new Group(ncOut, null, groupName);
-        if (dimLen == null) dimLen = getDimensionLengths(hdfFile, groupName);
-        for (int i = 0; i < dimLen.length; i++) {
-            logger.trace("group Dim: " + dimLen[i]);
-        }
-        
-        ncOut.addGroup(null, productGroup);
-        productGroup.addDimension(new Dimension("line", dimLen[1]));
-        productGroup.addDimension(new Dimension("ele", dimLen[2]));
+    /*
+     * Loop over all data looking for the products we can display
+     */
 
-        Group hdfGroup = hdfFile.findGroup(groupName);
-        List<Variable> varList = hdfGroup.getVariables();
-        for (Variable v : varList) {
-            addVar(ncOut, productGroup, v);
+    private static Map<String, List<Variable>> getDataVars(NetcdfFile hdf, int[] dataShape) {
+        List<Variable> variables = hdf.getVariables();
+        Map<String, List<Variable>> groupsToDataVars = new HashMap<>(variables.size());
+        for (Variable v : variables) {
+            if (Arrays.equals(dataShape, v.getShape())) {
+                String groupName = v.getGroup().getFullNameEscaped();
+                if (! groupsToDataVars.containsKey(groupName)) {
+                    groupsToDataVars.put(groupName, new ArrayList<Variable>(variables.size()));
+                }
+                groupsToDataVars.get(groupName).add(v);
+            }
         }
-       
+        return groupsToDataVars;
     }
+
+    /*
+     * Create the group structure and data products for our McV output
+     */
+
+    private static void populateDataTree(NetcdfFile ncOut, Map<String, List<Variable>> groupsToVars)
+    {
+        for (Map.Entry<String, List<Variable>> e : groupsToVars.entrySet()) {
+            Group g = new Group(ncOut, null, e.getKey());
+
+            logger.trace("Adding Group: " + g.getFullName());
+            // Newly created groups will have path separators converted to underscores
+            // We'll need to map back to the original group name for file access
+            groupMap.put(g.getFullName(), e.getKey());
+
+            ncOut.addGroup(null, g);
+
+            for (Variable v : e.getValue()) {
+                logger.trace("Adding Variable: " + v.getFullNameEscaped());
+                addVar(ncOut, g, v);
+            }
+
+        }
+    }
+
+    /**
+     * Fulfill data requests
+     * @return Array - an array with the requested data subset
+     */
 
     @Override public Array readData(Variable variable, Section section)
         throws IOException, InvalidRangeException
     {
         String variableName = variable.getShortName();
         
-        String groupName = "PRODUCT";
+        String groupName = groupMap.get(variable.getGroup().getFullName());
         logger.trace("looking for Group: " + groupName);
         Group hdfGroup = hdfFile.findGroup(groupName);
         Array result;
@@ -138,6 +189,13 @@ public class TropomiIOSP extends AbstractIOServiceProvider {
         return result;
     }
 
+    /*
+     * Test whether file in question is a valid product for this IOSP
+     * This method MUST BE LIGHTNING FAST, since it's part of the system
+     * process of attempting to infer the proper handler when the user
+     * is not certain what the best way to handle the data might be.
+     */
+
     private static boolean validProduct(Variable variable) {
         int[] varShape = variable.getShape();
         if (varShape.length != dimLen.length) return false;
@@ -148,16 +206,25 @@ public class TropomiIOSP extends AbstractIOServiceProvider {
         return true;
     }
 
-    private static int[] getDimensionLengths(NetcdfFile hdf, String groupName) throws IOException {
-        Group group = hdf.findGroup(groupName);
-        Variable geoVar = group.findVariable("latitude");
-        
+    /*
+     * Get the shape of valid data products. We consider anything that matches
+     * the geolocation bounds to be valid.
+     */
+
+    private static int[] getDataShape(NetcdfFile hdf) {
+        Group productGroup = hdf.findGroup(BASE_GROUP);
+        // Shape of valid data products will match that of geolocation, so base on LAT or LON
+        Variable geoVar = productGroup.findVariable(LAT);
         return new int[] {
             geoVar.getDimension(0).getLength(),
             geoVar.getDimension(1).getLength(),
             geoVar.getDimension(2).getLength()
         };
     }
+
+    /*
+     * Add a variable to the set of available products.
+     */
 
     private static void addVar(NetcdfFile nc, Group g, Variable vIn) {
 
@@ -180,7 +247,7 @@ public class TropomiIOSP extends AbstractIOServiceProvider {
             for (Attribute a : attList) {
                 v.addAttribute(a);
             }
-            logger.trace("adding vname: " + vIn.getFullName());
+            logger.trace("adding vname: " + vIn.getFullName() + " to group: " + g.getFullName());
             
             g.addVariable(v);
         }
