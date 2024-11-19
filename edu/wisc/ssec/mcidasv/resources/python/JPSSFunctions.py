689111a77c7a76c7ba242436f6f72320c3130e40
Bob Carp <robert.carp@ssec.wisc.edu>
2024-11-19 14:58:59 -0600

    JPSS Cloud Phase RGB functions [3175]
    Also fixed some incorrect central wavelengths in comments

--- edu/wisc/ssec/mcidasv/resources/python/JPSSFunctions.py
+++ edu/wisc/ssec/mcidasv/resources/python/JPSSFunctions.py
@@ -66,9 +66,9 @@ def makeGrid(fltField, res):
 # VIIRS SDR True Color RGB
 def VIIRSTrueColorRGB(M5, M4, M3):
     # https://gina.alaska.edu/wp-content/uploads/2023/02/QuickGuide_True_Color_Final.pdf
-    # red = M5 (0.672um) Reflectance - 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
-    # grn = M4 (0.555um) Reflectance - 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M3 (0.488um) Reflectance - 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # red = M5 (0.672um) Reflectance; 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # grn = M4 (0.555um) Reflectance; 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M3 (0.488um) Reflectance; 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
 
     inM5 = M5
     M5 = unpackage(M5)
@@ -116,7 +116,7 @@ def VIIRSNaturalColorRGB(M10, M7, M5):
 def VIIRSDustRGB(M14, M15, M16):
     # https://rammb2.cira.colostate.edu/wp-content/uploads/2020/01/Dust_RGB_Quick_Guide-1.pdf
     # red = M16 - M15 (12.013um - 10.763um); -6.7C to 2.6C rescaled to 0 to 255; gamma 1.0
-    # grn = M15 - M14 (10.763um - 8.4um);    -0.5C to 20C rescaled to 0 to 255; gamma 2.5
+    # grn = M15 - M14 (10.763um - 8.55um);   -0.5C to 20C rescaled to 0 to 255; gamma 2.5
     # blu = M15 (10.763um);                  261.2K to 288.7K rescaled to 0 to 255; gamma 1.0
 
     inM14 = M14
@@ -163,9 +163,9 @@ def VIIRSNightMicrophysicsRGB(M12, M15, M16):
 # VIIRS SDR Day Fire RGB
 def VIIRSDayFireRGB(I4, I2, I1):
     # http://rammb.cira.colostate.edu/training/visit/quick_guides/VIIRS_Day_Land_Cloud_Fire_RGB_Quick_Guide_10182018.pdf
-    # red = I4 (3.7um)  - 0C to 60C rescaled to 0 to 255; gamma 0.4
-    # grn = I2 (0.86um) - 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = I1 (0.64um) - 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # red = I4 (3.74um);  0C to 60C rescaled to 0 to 255; gamma 0.4
+    # grn = I2 (0.865um); 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = I1 (0.64um);  0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
 
     inI4 = I4
     I4 = unpackage(I4)
@@ -187,9 +187,9 @@ def VIIRSDayFireRGB(I4, I2, I1):
 # VIIRS SDR Fire Temperature RGB
 def VIIRSFireTemperatureRGB(M12, M11, M10):
     # http://rammb.cira.colostate.edu/training/visit/quick_guides/VIIRS_Fire_Temperature_RGB_Quick_Guide_10182018.pdf
-    # red = M12 (3.7um)  - 0C to 70C rescaled to 0 to 255; gamma 0.4
-    # grn = M11 (0.86um) - 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M10 (0.64um) - 0% to 75% reflectance rescaled to 0 to 255; gamma 1.0
+    # red = M12 (3.7um);  0C to 70C rescaled to 0 to 255; gamma 0.4
+    # grn = M11 (2.25um); 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M10 (1.61um); 0% to 75% reflectance rescaled to 0 to 255; gamma 1.0
 
     inM12 = M12
     M12 = unpackage(M12)
@@ -210,9 +210,9 @@ def VIIRSFireTemperatureRGB(M12, M11, M10):
 
 # VIIRS SDR Natural Color RGB (I-band)
 def VIIRSNaturalColorIRGB(I3, I2, I1):
-    # red = I3 (1.61um); 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
-    # grn = I2 (0.86um); 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = I1 (0.64um); 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # red = I3 (1.61um);  0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # grn = I2 (0.865um); 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = I1 (0.64um);  0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
 
     inI1 = I1
     I1 = unpackage(I1)
@@ -236,7 +236,7 @@ def VIIRSDayCloudPhaseMRGB(M5, M10, M15):
     # https://rammb.cira.colostate.edu/training/visit/quick_guides/QuickGuide_DayCloudPhaseDistinction_final_v2.pdf
     # red = M15 (10.763um); 7.5C to -53.5C (280.65K to 219.65) rescaled to 0 to 255; gamma 1.0
     # grn = M5 (0.672um);   0 to 78% reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M10 (1.612um);  1 to 59% reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M10 (1.61um);   1 to 59% reflectance rescaled to 0 to 255; gamma 1.0
 
     inM5 = M5
     M5 = unpackage(M5)
@@ -283,9 +283,9 @@ def VIIRSDayCloudPhaseIRGB(I1, I3, I5):
 
 # VIIRS SDR Cloud Type RGB
 def VIIRSCloudTypeRGB(M9, M5, M10):
-    # red = M9 (1.378um);  0 - 10 reflectance rescaled to 0 to 255; gamma 0.66
-    # grn = M5 (0.672um);  0 - 78 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M10 (1.612um); 0 - 59 reflectance rescaled to 0 to 255; gamma 1.0
+    # red = M9 (1.378um); 0 - 10 reflectance rescaled to 0 to 255; gamma 0.66
+    # grn = M5 (0.672um); 0 - 78 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M10 (1.61um); 0 - 59 reflectance rescaled to 0 to 255; gamma 1.0
 
     inM9 = M9
     M9 = unpackage(M9)
@@ -310,7 +310,7 @@ def VIIRSSnowmeltRGB(M10, M8, M5):
     # https://rammb2.cira.colostate.edu/wp-content/uploads/2021/03/VIIRS_Snowmelt_RGB_Quick_Guide_v3.pdf
     # red = M10 (1.61um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
     # grn = M8 (1.24um);  0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M5 (0.67um);  0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M5 (0.672um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
 
     inM10 = M10
     M10 = unpackage(M10)
@@ -333,9 +333,9 @@ def VIIRSSnowmeltRGB(M10, M8, M5):
 # VIIRS SDR Sea Spray RGB
 def VIIRSSeaSprayRGB(I1, I2, I4, I5):
     # https://rammb.cira.colostate.edu/training/visit/quick_guides/VIIRS_Sea_Spray_RGB_Quick_Guide_v2.pdf
-    # red = I4 (3.7um) - I5 (11.45)  - 0C to 10C rescaled to 0 to 255; gamma 1.0
-    # grn = I2 (0.86um) - 1% to 20% reflectance rescaled to 0 to 255; gamma 0.6
-    # blu = I1 (0.64um) - 2% to 25% reflectance rescaled to 0 to 255; gamma 0.6
+    # red = I4 (3.74um) - I5 (11.45); 0C to 10C rescaled to 0 to 255; gamma 1.0
+    # grn = I2 (0.865um);             1% to 20% reflectance rescaled to 0 to 255; gamma 0.6
+    # blu = I1 (0.64um);              2% to 25% reflectance rescaled to 0 to 255; gamma 0.6
 
     inI1 = I1
     I1 = unpackage(I1)
@@ -360,7 +360,7 @@ def VIIRSSeaSprayRGB(I1, I2, I4, I5):
 def VIIRSAshRGB(M14, M15, M16):
     # https://rammb.cira.colostate.edu/training/visit/quick_guides/GOES_Ash_RGB.pdf
     # red = M16 - M15 (12.013um - 10.763um); -6.7C to 2.6C rescaled to 0 to 255
-    # grn = M15 - M14 (10.763um - 8.4um);    -6C to 6.3C rescaled to 0 to 255
+    # grn = M15 - M14 (10.763um - 8.55um);   -6C to 6.3C rescaled to 0 to 255
     # blu = M15 (10.763um);                  243.6K to 302.4K rescaled to 0 to 255
     
     inM14 = M14
@@ -435,10 +435,35 @@ def VIIRSDaySnowFogIRGB(I2, I3, I4, I5):
     return package(inI2, rgb)
 
 
+# VIIRS SDR Cloud Phase RGB
+def VIIRSCloudPhaseRGB(M5, M10, M11):
+    # https://resources.eumetrain.org/data/7/726/navmenu.php?tab=3&page=1.0.0
+    # red = M5  (0.672um) Reflectance; 0% to 50% reflectance rescaled to 0 to 255; gamma 1.0
+    # grn = M10 (1.61um)  Reflectance; 0% to 50% reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M11 (2.25um)  Reflectance; 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+
+    inM5 = M5
+    M5 = unpackage(M5)
+    inM10 = M10
+    M10 = unpackage(M10)
+    inM11 = M11
+    M11 = unpackage(M11)
+  
+    grd750 = makeGrid(M5, 750)
+    
+    red = rescale(M10, 0, 0.5, 0, 255)
+    grn = rescale(M11, 0, 0.5, 0, 255)
+    blu = rescale(M5, 0, 1, 0, 255)
+
+    rgb = MultiSpectralDataSource.swathToGrid(grd750, [red, grn, blu], 1.0)
+
+    return package(inM5, rgb)
+
+
 # VIIRS SDR NDVI
 def VIIRSNDVI(I1, I2):
-    # I1 = 0.64um - visible Reflectance
-    # I2 = 0.865um - near IR Reflectance
+    # I1 = 0.64um;  visible Reflectance
+    # I2 = 0.865um; near IR Reflectance
 
     inI1 = I1
     I1 = unpackage(I1)
@@ -456,8 +481,8 @@ def VIIRSNDVI(I1, I2):
 
 # VIIRS SDR NDSI
 def VIIRSNDSI(I1, I3):
-    # I1 = 0.64um - visible Reflectance
-    # I3 = 1.61um - shortwave IR Reflectance
+    # I1 = 0.64um; visible Reflectance
+    # I3 = 1.61um; shortwave IR Reflectance
 
     inI1 = I1
     I1 = unpackage(I1)
@@ -475,8 +500,8 @@ def VIIRSNDSI(I1, I3):
 
 # VIIRS SDR Burn Area Index
 def VIIRSBAI(I1, I2):
-    # I1 = 0.64um - visible Reflectance
-    # I2 = 0.865um - near IR Reflectance
+    # I1 = 0.64um;  visible Reflectance
+    # I2 = 0.865um; near IR Reflectance
     
     inI1 = I1
     I1 = unpackage(I1)
@@ -494,8 +519,8 @@ def VIIRSBAI(I1, I2):
 
 # VIIRS SDR Normalized Burn Ratio
 def VIIRSNBR(I2, I3):
-    # I2 = 0.865um - near IR Reflectance
-    # I3 = 1.61um - shortwave IR Reflectance
+    # I2 = 0.865um; near IR Reflectance
+    # I3 = 1.61um;  shortwave IR Reflectance
     inI2 = I2
     
     I2 = unpackage(I2)
@@ -514,9 +539,9 @@ def VIIRSNBR(I2, I3):
 def VIIRSVARI(M5, M4, M3):
     # VARI = Visible Atmospherically Resistant Index
     # Makes vegetation stand out from surrounding areas
-    # M5 = 0.672um reflectance = Red
-    # M4 = 0.555um reflectance = Grn
-    # M3 = 0.488um reflectance = Blu
+    # red = M5 (0.672um); visible reflectance
+    # grn = M4 (0.555um); visible reflectance
+    # blu = M3 (0.488um); visible reflectance
     
     inM5 = M5
     M5 = unpackage(M5)
@@ -537,8 +562,8 @@ def VIIRSVARI(M5, M4, M3):
 
 # VIIRS SDR Normalized Difference Built-up Index
 def VIIRSNDBI(I2, I3):
-    # I2 = 0.865um - near IR Reflectance
-    # I3 = 1.61um - shortwave IR Reflectance
+    # I2 = 0.865um; near IR Reflectance
+    # I3 = 1.61um;  shortwave IR Reflectance
     
     inI2 = I2
     I2 = unpackage(I2)
@@ -585,7 +610,7 @@ def VIIRSEdrNaturalColorRGB(M10, M7, M5):
 def VIIRSEdrDustRGB(M14, M15, M16):
     # https://rammb2.cira.colostate.edu/wp-content/uploads/2020/01/Dust_RGB_Quick_Guide-1.pdf
     # red = M16 - M15 (12.013um - 10.763um); -6.7C to 2.6C rescaled to 0 to 255; gamma 1.0
-    # grn = M15 - M14 (10.763um - 8.4um);    -0.5C to 20C rescaled to 0 to 255; gamma 2.5
+    # grn = M15 - M14 (10.763um - 8.55um);   -0.5C to 20C rescaled to 0 to 255; gamma 2.5
     # blu = M15 (10.763um);                  261.2K to 288.7K rescaled to 0 to 255; gamma 1.0
     red = rescale(M16-M15,  -6.7, 2.6, 0, 255)
     grn = 255*(rescale(M15-M14, -0.5, 20, 0, 1)**(1/2.5))
@@ -608,9 +633,9 @@ def VIIRSEdrNightMicrophysicsRGB(M12, M15, M16):
 # VIIRS Edr Day Fire RGB
 def VIIRSEdrDayFireRGB(I4, I2, I1):
     # http://rammb.cira.colostate.edu/training/visit/quick_guides/VIIRS_Day_Land_Cloud_Fire_RGB_Quick_Guide_10182018.pdf
-    # red = I4 (3.7um);  0C to 60C rescaled to 0 to 255; gamma 0.4
-    # grn = I2 (0.86um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = I1 (0.64um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # red = I4 (3.74um);  0C to 60C rescaled to 0 to 255; gamma 0.4
+    # grn = I2 (0.865um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = I1 (0.64um);  0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
     red = 255*(rescale(I4, 273.15, 333.15, 0, 1)**(1/.4))
     grn = rescale(I2, 0, 1, 0, 255)
     blu = rescale(I1, 0, 1, 0, 255)
@@ -621,8 +646,8 @@ def VIIRSEdrDayFireRGB(I4, I2, I1):
 def VIIRSEdrFireTemperatureRGB(M12, M11, M10):
     # http://rammb.cira.colostate.edu/training/visit/quick_guides/VIIRS_Fire_Temperature_RGB_Quick_Guide_10182018.pdf
     # red = M12 (3.7um);  0C to 70C rescaled to 0 to 255; gamma 0.4
-    # grn = M11 (0.86um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M10 (0.64um); 0 to 75 reflectance rescaled to 0 to 255; gamma 1.0
+    # grn = M11 (2.25um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M10 (1.61um); 0 to 75 reflectance rescaled to 0 to 255; gamma 1.0
     red = 255*(rescale(M12, 273.15, 343.15, 0, 1)**(1/.4))
     grn = rescale(M11, 0, 1, 0, 255)
     blu = rescale(M10, 0, 0.75, 0, 255)
@@ -631,9 +656,9 @@ def VIIRSEdrFireTemperatureRGB(M12, M11, M10):
 
 # VIIRS EDR Natural Color RGB (I-band)
 def VIIRSEdrNaturalColorIRGB(I3, I2, I1):
-    # red = I3 (1.61um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
-    # grn = I2 (0.86um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = I1 (0.64um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # red = I3 (1.61um);  0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # grn = I2 (0.865um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = I1 (0.64um);  0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
     red = rescale(I3, 0, 1, 0, 255)
     grn = rescale(I2, 0, 1, 0, 255)
     blu = rescale(I1, 0, 1, 0, 255)
@@ -642,9 +667,9 @@ def VIIRSEdrNaturalColorIRGB(I3, I2, I1):
 
 # VIIRS EDR Day Cloud Type RGB
 def VIIRSEdrCloudTypeRGB(M9, M5, M10):
-    # red = M9 (1.378um);  0 to 10 reflectance rescaled to 0 to 255; gamma 0.66
-    # grn = M5 (0.672um);  0 to 78 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M10 (1.612um); 0 to 59 reflectance rescaled to 0 to 255; gamma 1.0
+    # red = M9 (1.378um); 0 to 10 reflectance rescaled to 0 to 255; gamma 0.66
+    # grn = M5 (0.672um); 0 to 78 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M10 (1.61um); 0 to 59 reflectance rescaled to 0 to 255; gamma 1.0
     red = 255*(rescale(M9, 0, .10, 0, 1)**(1/0.66))
     grn = rescale(M5, 0, 0.78, 0, 255)
     blu = rescale(M10, 0, 0.59, 0, 255)
@@ -656,7 +681,7 @@ def VIIRSEdrSnowmeltRGB(M10, M8, M5):
     # https://rammb2.cira.colostate.edu/wp-content/uploads/2021/03/VIIRS_Snowmelt_RGB_Quick_Guide_v3.pdf
     # red = M10 (1.61um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
     # grn = M8 (1.24um);  0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M5 (0.67um);  0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M5 (0.672um); 0 to 100 reflectance rescaled to 0 to 255; gamma 1.0
     red = rescale(M10, 0, 1, 0, 255)
     grn = rescale(M8, 0, 1, 0, 255)
     blu = rescale(M5, 0, 1, 0, 255)
@@ -666,9 +691,9 @@ def VIIRSEdrSnowmeltRGB(M10, M8, M5):
 # VIIRS EDR Sea Spray RGB
 def VIIRSEdrSeaSprayRGB(I1, I2, I4, I5):
     # https://rammb.cira.colostate.edu/training/visit/quick_guides/VIIRS_Sea_Spray_RGB_Quick_Guide_v2.pdf
-    # red = I4 (3.7um) - I5 (11.45); 0C to 10C rescaled to 0 to 255; gamma 1.0
-    # grn = I2 (0.86um);             1 to 20 reflectance rescaled to 0 to 255; gamma 0.6
-    # blu = I1 (0.64um);             2 to 25 reflectance rescaled to 0 to 255; gamma 0.6
+    # red = I4 (3.74um) - I5 (11.45); 0C to 10C rescaled to 0 to 255; gamma 1.0
+    # grn = I2 (0.865um);             1 to 20 reflectance rescaled to 0 to 255; gamma 0.6
+    # blu = I1 (0.64um);              2 to 25 reflectance rescaled to 0 to 255; gamma 0.6
     red = rescale(sub(I4, I5), 0, 10, 0, 255)
     grn = 255*(rescale(I2, 0.01, 0.20, 0, 1)**(1/.6))
     blu = 255*(rescale(I1, 0.02, 0.25, 0, 1)**(1/.6))
@@ -679,7 +704,7 @@ def VIIRSEdrSeaSprayRGB(I1, I2, I4, I5):
 def VIIRSEdrAshRGB(M14, M15, M16):
     # https://rammb.cira.colostate.edu/training/visit/quick_guides/GOES_Ash_RGB.pdf
     # red = M16 - M15 (12.013um - 10.763um); -6.7C to 2.6C rescaled to 0 to 255
-    # grn = M15 - M14 (10.763um - 8.4um);    -6C to 6.3C rescaled to 0 to 255
+    # grn = M15 - M14 (10.763um - 8.55um);   -6C to 6.3C rescaled to 0 to 255
     # blu = M15 (10.763um);                  243.6K to 302.4K rescaled to 0 to 255
     red = rescale(M16-M15, -6.7, 2.6, 0, 255)
     grn = rescale(M15-M14, -6.0, 6.3, 0, 255)
@@ -692,7 +717,7 @@ def VIIRSEdrDayCloudPhaseMRGB(M5, M10, M15):
     # https://rammb.cira.colostate.edu/training/visit/quick_guides/QuickGuide_DayCloudPhaseDistinction_final_v2.pdf
     # red = M15 (10.763um); 7.5C to -53.5C (280.65K to 219.65) rescaled to 0 to 255; gamma 1.0
     # grn = M5 (0.672um);   0 to 78 reflectance rescaled to 0 to 255; gamma 1.0
-    # blu = M10 (1.612um);  1 to 59 reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M10 (1.61um);   1 to 59 reflectance rescaled to 0 to 255; gamma 1.0
     red = rescale(M15, 280.65, 219.65, 0, 255)
     grn = rescale(M5, 0, 0.78, 0, 255)
     blu = rescale(M10, 0.01, 0.59, 0, 255)
@@ -734,32 +759,43 @@ def VIIRSEdrDaySnowFogIRGB(I2, I3, I4, I5):
     blu = 255*(rescale(I4-I5, 0, 30, 0, 1)**(1/1.7))
     return mycombineRGB(red, grn, blu)
 
+# VIIRS EDR Cloud Phase RGB
+def VIIRSEdrCloudPhaseRGB(M5, M10, M11):
+    # https://resources.eumetrain.org/data/7/726/navmenu.php?tab=3&page=1.0.0
+    # red = M5  (0.672um) Reflectance; 0% to 50% reflectance rescaled to 0 to 255; gamma 1.0
+    # grn = M10 (1.61um)  Reflectance; 0% to 50% reflectance rescaled to 0 to 255; gamma 1.0
+    # blu = M11 (2.25um)  Reflectance; 0% to 100% reflectance rescaled to 0 to 255; gamma 1.0
+    red = rescale(M10, 0, 0.5, 0, 255)
+    grn = rescale(M11, 0, 0.5, 0, 255)
+    blu = rescale(M5, 0, 1, 0, 255)
+    return mycombineRGB(red, grn, blu)
+
 
 # VIIRS EDR NDVI
 def VIIRSEdrNDVI(I1, I2):
-    # I1 = 0.64um - visible Reflectance
-    # I2 = 0.865um - near IR Reflectance
+    # I1 = 0.64um;  visible Reflectance
+    # I2 = 0.865um; near IR Reflectance
     return (I2-I1)/(I1+I2)
 
 
 # VIIRS EDR NDSI
 def VIIRSEdrNDSI(I1, I3):
-    # I1 = 0.64um - visible Reflectance
-    # I3 = 1.61um - shortwave IR Reflectance
+    # I1 = 0.64um; visible Reflectance
+    # I3 = 1.61um; shortwave IR Reflectance
     return (I1-I3)/(I1+I3)
 
 
 # VIIRS EDR Burn Area Index
 def VIIRSEdrBAI(I1, I2):
-    # I1 = 0.64um - visible Reflectance
-    # I2 = 0.865um - near IR Reflectance
+    # I1 = 0.64um;  visible Reflectance
+    # I2 = 0.865um; near IR Reflectance
     return 1/((0.1 - I2)**2 + (0.06 - I1)**2)
 
 
 # VIIRS EDR Normalized Burn Ratio
 def VIIRSEdrNBR(I2, I3):
-    # I2 = 0.865um - near IR Reflectance
-    # I3 = 1.61um - shortwave IR Reflectance
+    # I2 = 0.865um; near IR Reflectance
+    # I3 = 1.61um;  shortwave IR Reflectance
     return (I2-I3) / (I2+I3)
 
 
@@ -767,14 +803,14 @@ def VIIRSEdrNBR(I2, I3):
 def VIIRSEdrVARI(M5, M4, M3):
     # VARI = Visible Atmospherically Resistant Index
     # Makes vegetation stand out from surrounding areas
-    # M5 = 0.672um reflectance = Red
-    # M4 = 0.555um reflectance = Grn
-    # M3 = 0.488um reflectance = Blu
+    # red = M5 (0.672um) reflectance
+    # grn = M4 (0.555um) reflectance
+    # blu = M3 (0.488um) reflectance
     return (M4 - M5) / (M4 + M5 - M3)
 
 
 # VIIRS EDR Normalized Difference Built-up Index
 def VIIRSEdrNDBI(I2, I3):
-    # I2 = 0.865um - near IR Reflectance
-    # I3 = 1.61um - shortwave IR Reflectance
+    # I2 = 0.865um; near IR Reflectance
+    # I3 = 1.61um;  shortwave IR Reflectance
     return (I3-I2) / (I3+I2)
