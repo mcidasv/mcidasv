9c2d73c0ac90a54d631ba908fb8e9ee795d2b6e5
Jonathan Beavers <jbeavers@ssec.wisc.edu>
2025-01-02 13:19:13 -0600

    Previous commit missed *a few* files [3180]
    Time to investigate set_copyright.sh.

--- ucar/unidata/idv/VMManager.java
+++ ucar/unidata/idv/VMManager.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of McIDAS-V
  *
- * Copyright 2007-2024
+ * Copyright 2007-2025
  * Space Science and Engineering Center (SSEC)
  * University of Wisconsin - Madison
  * 1225 W. Dayton Street, Madison, WI 53706, USA
@@ -25,9 +25,8 @@
  * You should have received a copy of the GNU Lesser Public License
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
+
 package ucar.unidata.idv;
-
-
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
