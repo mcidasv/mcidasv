4fede91fe3bd20dcf902de582d6fd76c6c9d978c
mug mcidasv <mcidasv@mug.ssec.wisc.edu>
2025-01-02 18:46:55 +0000

    Update source code to 2025 [3180]
    Thanks belong to Bob for getting the ball rolling on this.

--- edu/wisc/ssec/adapter/AggregationRangeProcessor.java
+++ edu/wisc/ssec/adapter/AggregationRangeProcessor.java
@@ -6,27 +6,25 @@
  * University of Wisconsin - Madison
  * 1225 W. Dayton Street, Madison, WI 53706, USA
  * https://www.ssec.wisc.edu/mcidas/
- *
+ * 
  * All Rights Reserved
- *
- * McIDAS-V is built on Unidata's IDV, as well as SSEC's VisAD and HYDRA
- * projects. Parts of McIDAS-V source code are based on IDV, VisAD, and
- * HYDRA source code.
- *
+ * 
+ * McIDAS-V is built on Unidata's IDV and SSEC's VisAD libraries, and
+ * some McIDAS-V source code is based on IDV and VisAD source code.  
+ * 
  * McIDAS-V is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * McIDAS-V is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser Public License
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
-
 package edu.wisc.ssec.adapter;
 
 import java.util.ArrayList;
