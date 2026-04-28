e454846984a4928190b79006476fbd7ea6a30faf
Bob Carp <robert.carp@ssec.wisc.edu>
2026-04-28 19:50:48 +0000

    [0] Dreamweaver updates synced from web server.

--- docs/userguide/processed/mcv-top.php
+++ docs/userguide/processed/mcv-top.php
@@ -1,6 +1,6 @@
 <table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
   <tr>
-    <td width=300 align=center valign=middle><img src="../../../images/mcidas-logos/mcidasv_200x100.png"></td>
+    <td width=300 align=center valign=middle><img src="../../../images/mcidas-logos/mcidasv_200x100.png" alt="McIDAS-V logo"></td>
     <td align=left valign=middle><h1>McIDAS-V User's Guide</h1></td>
   </tr>
 </table>
